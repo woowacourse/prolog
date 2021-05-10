11cb74998c1929a4f6ebca646bb0fdf0c0da54e6
gracefulBrown <brown@woowahan.com>
2021-05-11 00:19:42 +0900

    feat(study-log): sample domain class

---
+++ backend/src/main/java/wooteco/studylog/StudylogApplication.java
@@ -0,0 +1,13 @@
+package wooteco.studylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudylogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudylogApplication.class, args);
+	}
+
+}
---
+++ backend/src/main/java/wooteco/studylog/log/StudyLog.java
@@ -0,0 +1,4 @@
+package wooteco.studylog.log;
+
+public class StudyLog {
+}
