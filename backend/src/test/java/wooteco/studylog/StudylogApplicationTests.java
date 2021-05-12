b92a8378328c45181cd40ee16e27d8f003acb16c
류성현 <brown@woowahan.com>
2021-05-12 16:46:36 +0900

    feat(study-log): sample domain class (#15)
    * feat(study-log): sample domain class
    
    * feat(gradle): add dependency and sample code for acceptance test & documentation
    
    * feat(rest-doce): add rest docs sample

---
+++ backend/src/test/java/wooteco/studylog/StudylogApplicationTests.java
@@ -0,0 +1,13 @@
+package wooteco.studylog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudylogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
