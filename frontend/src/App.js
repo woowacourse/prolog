804e01044139fc028d0f655376a1b8167fca5fc0
sunhpark42 <sunhpark42@gmail.com>
2021-05-14 20:54:38 +0900

    chore: 디렉토리 구조 및 라우터 설정
    - react-router-dom 라이브러리 설치

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { PATH } from './constants';
+import { MainPage, NewPostPage, PostPage } from './pages';
+
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route exact path={PATH.ROOT} component={MainPage} />
+          <Route exact path={PATH.POST} component={PostPage} />
+          <Route exact path={PATH.NEW_POST} component={NewPostPage} />
+          <Redirect to={PATH.ROOT} />
+        </Switch>
+      </Router>
+    </>
+  );
 };
 
 export default App;
