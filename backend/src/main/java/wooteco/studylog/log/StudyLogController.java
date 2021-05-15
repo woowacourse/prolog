8d4e55b6a5e25ac847ba552f356af402e5753bf3
gracefulBrown <brown@woowahan.com>
2021-05-15 10:36:20 +0900

    feat: add sample request for server

--- backend/src/main/java/wooteco/studylog/log/StudyLogController.java
+++ backend/src/main/java/wooteco/studylog/log/StudyLogController.java
@@ -1,10 +1,13 @@
 package wooteco.studylog.log;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class StudyLogController {
@@ -12,5 +15,14 @@ public class StudyLogController {
     public ResponseEntity<StudyLogResponse> create() {
         StudyLogResponse studyLogResponse = new StudyLogResponse(1L, "제목", "본문", "작성자이름");
         return ResponseEntity.created(URI.create("/study-logs/" + studyLogResponse.getId())).body(studyLogResponse);
+    }
+
+    @GetMapping("/study-logs")
+    public ResponseEntity<List<StudyLogResponse>> list() {
+        List<StudyLogResponse> studyLogResponses = Arrays.asList(
+                new StudyLogResponse(1L, "제목1", "본문1", "작성자이름1"),
+                new StudyLogResponse(2L, "제목2", "본문2", "작성자이름2")
+        );
+        return ResponseEntity.ok().body(studyLogResponses);
     }
 }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,33 @@
+import {useEffect, useState} from "react";
+import axios from 'axios';
+
 const App = () => {
-  return <div></div>;
+  const [studyLogs, setStudyLogs] = useState([]);
+
+  const fetchStudyLogs = async () => {
+    try {
+      const response = await axios.get('/study-logs');
+      setStudyLogs(response.data);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  useEffect(() => {
+    fetchStudyLogs();
+  }, []);
+
+  return (
+      <div>
+        {studyLogs.map((studyLog, index) => {
+          return (
+              <div key={index}>
+                {studyLog.title}
+              </div>
+          );
+        })}
+      </div>
+  );
 };
 
 export default App;
