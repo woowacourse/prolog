d333c3f4a07833201893345077a2b440e7b815b0
pomo <bosl95@naver.com>
2021-05-16 21:25:52 +0900

    feat: 고정된 결과가 반환되도록 API 수정

--- backend/src/main/java/wooteco/studylog/log/web/controller/StudyLogController.java
+++ backend/src/main/java/wooteco/studylog/log/web/controller/StudyLogController.java
@@ -3,12 +3,13 @@ package wooteco.studylog.log.web.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import wooteco.studylog.log.service.StudyLogService;
-import wooteco.studylog.log.web.controller.dto.CategoryResponses;
-import wooteco.studylog.log.web.controller.dto.LogRequest;
-import wooteco.studylog.log.web.controller.dto.LogResponse;
-import wooteco.studylog.log.web.controller.dto.LogResponses;
+import wooteco.studylog.log.web.controller.dto.*;
 
 import java.net.URI;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 @RequestMapping("/logs")
@@ -21,28 +22,66 @@ public class StudyLogController {
 
     @GetMapping
     public ResponseEntity<LogResponses> showAll() {
-        return ResponseEntity.ok(studyLogService.findAll());
+        List<LogResponse> rowLogResponses = Collections.singletonList(
+                new LogResponse(1L,
+                        new AuthorResponse(1L, "뽀모", "image"),
+                        LocalDateTime.now(),
+                        new CategoryResponse(1L, "미션1"),
+                        "제목",
+                        "내용",
+                        Arrays.asList("자바", "쟈스")
+                )
+        );
+        LogResponses studyLogResponses = new LogResponses(rowLogResponses);
+        return ResponseEntity.ok(studyLogResponses);
+//        return ResponseEntity.ok(studyLogService.findAll());
     }
 
     @PostMapping
-    public ResponseEntity<LogResponse> createLog(@RequestBody LogRequest logRequest) {
-        LogResponse logResponse = studyLogService.insertLog(
-                logRequest.getCategoryId(),
-                logRequest.getTitle(),
-                logRequest.getTags(),
-                logRequest.getContent());
+    public ResponseEntity<LogResponse> createLog(@RequestBody List<LogRequest> logRequest) {
+        LogResponse logResponse = new LogResponse(1L,
+                new AuthorResponse(1L, "뽀모", "image"),
+                LocalDateTime.now(),
+                new CategoryResponse(1L, "미션1"),
+                "제목",
+                "내용",
+                Arrays.asList("자바", "쟈스"));
+        studyLogService.insertLogs(logRequest);
         return ResponseEntity.created(URI.create("/logs/" + logResponse.getLogId())).body(logResponse);
+//        LogResponse logResponse = studyLogService.insertLog(
+//                logRequest.getCategoryId(),
+//                logRequest.getTitle(),
+//                logRequest.getTags(),
+//                logRequest.getContent());
+//        return ResponseEntity.created(URI.create("/logs/" + logResponse.getLogId())).body(logResponse);
     }
 
     @GetMapping("/{logId}")
     public ResponseEntity<LogResponse> showLog(@PathVariable Long logId){
-        LogResponse logResponse = studyLogService.findById(logId);
+//        LogResponse logResponse = studyLogService.findById(logId);
+        LogResponse logResponse =
+                new LogResponse(1L,
+                        new AuthorResponse(1L, "웨지", "image"),
+                        LocalDateTime.now(),
+                        new CategoryResponse(1L, "미션1"),
+                        "매감",
+                        "매서운감자",
+                        Arrays.asList("자바", "자스")
+
+                );
         return ResponseEntity.ok(logResponse);
     }
 
     @GetMapping("/categories")
     public ResponseEntity<CategoryResponses> showCategories() {
-        CategoryResponses allCategories = studyLogService.findAllCategories();
-        return ResponseEntity.ok(allCategories);
+//        CategoryResponses allCategories = studyLogService.findAllCategories();
+        List<CategoryResponse> categoryFixture = Arrays.asList(
+                new CategoryResponse(1L, "빈지모"),
+                new CategoryResponse(2L, "빈포모"),
+                new CategoryResponse(3L, "웨지노")
+        );
+
+        CategoryResponses categoryResponses = new CategoryResponses(categoryFixture);
+        return ResponseEntity.ok(categoryResponses);
     }
 }
