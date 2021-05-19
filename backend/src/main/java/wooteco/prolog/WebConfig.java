b55ebefea4cbf57d0f82c9dfc32025a7e1a831b4
gracefulBrown <brown@woowahan.com>
2021-05-19 14:10:15 +0900

    feat(cors)

---
+++ backend/src/main/java/wooteco/prolog/WebConfig.java
@@ -0,0 +1,22 @@
+package wooteco.prolog;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        String[] allowedOriginPatterns = Arrays.asList(
+                "http://localhost:3000"
+        ).toArray(new String[0]);
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOriginPatterns(allowedOriginPatterns)
+                .exposedHeaders("*")
+        ;
+    }
+}
