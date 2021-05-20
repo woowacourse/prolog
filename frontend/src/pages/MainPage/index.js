4865182e3b71d2ff9d24a05e9a56a87d3b1572e8
pkeugine <pkeugine@gmail.com>
2021-05-20 17:32:48 +0900

    refactor: 프론트 메인페이지 index.js 원상태 복구
    - login 부분 README.md 제거

--- frontend/src/pages/MainPage/index.js
+++ frontend/src/pages/MainPage/index.js
@@ -1,28 +1,7 @@
 import React from 'react';
-import axios from 'axios';
-import GitHubLogin from 'react-github-login';
 
 const MainPage = () => {
-  const onSuccess = async (res) => {
-    const {code} = res;
-
-    const {access_token} = await axios.post(`http://localhost:8080/login/token`, {
-      code,
-    });
-
-    console.log(access_token);
-  };
-  const onFailure = (response) => console.error(response);
-  return (
-    <div>
-      <GitHubLogin
-        clientId="f91b56445e08d44adb76"
-        onSuccess={onSuccess}
-        onFailure={onFailure}
-        redirectUri={'http://localhost:3000'}
-      />
-    </div>
-  );
+  return <div>Main Page</div>;
 };
 
 export default MainPage;
