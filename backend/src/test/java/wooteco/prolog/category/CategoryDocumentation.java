a0597ba5a2321c377ce21a4d001cef635476c468
gracefulBrown <brown@woowahan.com>
2021-05-23 00:56:11 +0900

    fix(login): documentation

--- backend/src/test/java/wooteco/prolog/category/CategoryDocumentation.java
+++ backend/src/test/java/wooteco/prolog/category/CategoryDocumentation.java
@@ -2,15 +2,24 @@ package wooteco.prolog.category;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import wooteco.prolog.Documentation;
+import wooteco.prolog.login.GithubLoginService;
 
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 public class CategoryDocumentation extends Documentation {
+    @MockBean
+    private GithubLoginService githubLoginService;
+
     @Test
     void post() {
+        when(githubLoginService.createToken(anyString())).thenReturn("asdf.asdf.asdf");
+
         RestAssured
                 .given(spec).log().all()
                 .filter(document("category/list",
