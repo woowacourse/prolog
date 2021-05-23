d1283f75823ac21047a9708a9d20eb4c05c6acda
SungSiHyung <51393021+sihyung92@users.noreply.github.com>
2021-05-23 17:49:59 +0900

    fix(backend) : h2 및 mysql 모두에서 범용적인 DB 스키마 파일로 수정 (#52)
    1. post테이블 content칼럼의 타입 수정
    2. drop table문 순서 조정
    3. INNO_DB 설정 제거

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -1,62 +1,61 @@
 -- -----------------------------------------------------
 -- Table mydb.post
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS postTag ;
 DROP TABLE IF EXISTS post ;
+DROP TABLE IF EXISTS tag ;
+DROP TABLE IF EXISTS category ;
+drop table if exists MEMBER;
 
-CREATE TABLE IF NOT EXISTS post (
-                                    id BIGINT NOT NULL AUTO_INCREMENT,
-                                    member_id BIGINT NOT NULL,
-                                    createdAt DATETIME NOT NULL,
-                                    updatedAt DATETIME NULL,
-                                    title VARCHAR(65535) NOT NULL,
+CREATE TABLE IF NOT EXISTS post
+(
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    member_id BIGINT NOT NULL,
+    createdAt DATETIME NOT NULL,
+    updatedAt DATETIME NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    content TEXT NOT NULL,
     category_id BIGINT NOT NULL,
     PRIMARY KEY (id)
-    ) ENGINE = InnoDB;
+);
 -- -----------------------------------------------------
 -- Table category
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS category ;
 
-CREATE TABLE IF NOT EXISTS category (
+CREATE TABLE IF NOT EXISTS category
+(
     id BIGINT NOT NULL AUTO_INCREMENT,
     name VARCHAR(45) NOT NULL,
     PRIMARY KEY (id)
-)ENGINE = InnoDB;
-
-
+);
 -- -----------------------------------------------------
 -- Table tag
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS tag ;
 
-CREATE TABLE IF NOT EXISTS tag (
+CREATE TABLE IF NOT EXISTS tag
+(
     id BIGINT NOT NULL AUTO_INCREMENT,
     name VARCHAR(20) NOT NULL,
     PRIMARY KEY (id)
-)ENGINE = InnoDB;
-
+);
 
 -- -----------------------------------------------------
 -- Table postTag
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS postTag ;
 
-CREATE TABLE IF NOT EXISTS postTag (
+CREATE TABLE IF NOT EXISTS postTag
+(
     id BIGINT NOT NULL AUTO_INCREMENT,
     post_id BIGINT NOT NULL,
     tag_id BIGINT NOT NULL,
-    PRIMARY KEY (id),
-    INDEX post_id_idx (post_id ASC) VISIBLE,
-    INDEX tag_id_idx (tag_id ASC) VISIBLE,
-    CONSTRAINT post_id
+    PRIMARY KEY (id),CONSTRAINT post_id
     FOREIGN KEY (post_id)
     REFERENCES post (id),
     CONSTRAINT tag_id
     FOREIGN KEY (tag_id)
     REFERENCES tag (id)
-) ENGINE = InnoDB;
+);
 
-drop table if exists MEMBER;
 
 create table if not exists MEMBER
 (
