0da4f9249c4cf087b80bb42b65a9e5d7417afee1
pomo <bosl95@naver.com>
2021-05-30 22:21:38 +0900

    feat(카테고리, 태그 조회) : tag Dao, Controller 추가

---
+++ backend/src/main/java/wooteco/prolog/tag/ui/TagController.java
@@ -0,0 +1,29 @@
+package wooteco.prolog.tag.ui;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import wooteco.prolog.tag.application.TagService;
+import wooteco.prolog.tag.application.dto.TagRequest;
+import wooteco.prolog.tag.application.dto.TagResponse;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tags")
+public class TagController {
+
+    private TagService tagService;
+
+    public TagController(TagService tagService) {
+        this.tagService = tagService;
+    }
+
+    @PostMapping
+    public ResponseEntity<List<TagResponse>> createTag(@RequestBody List<TagRequest> tagRequests) {
+        List<TagResponse> tagResponses = tagService.create(tagRequests);
+        return ResponseEntity.ok(tagResponses);
+    }
+}
