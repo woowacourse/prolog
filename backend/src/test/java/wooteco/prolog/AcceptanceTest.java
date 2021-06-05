aef6f20421fb6398d06b94bcf48bdf0053d7aaf1
gracefulBrown <brown@woowahan.com>
2021-06-05 14:22:28 +0900

    refactor: fake test

--- backend/src/test/java/wooteco/prolog/AcceptanceTest.java
+++ backend/src/test/java/wooteco/prolog/AcceptanceTest.java
@@ -3,15 +3,14 @@ package wooteco.prolog;
 import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-@ExtendWith(MockitoExtension.class)
 public class AcceptanceTest {
     @LocalServerPort
     int port;
