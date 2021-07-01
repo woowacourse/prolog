6b20dd92090d83d39f23c9b1c95da3cbd28d7031
gracefulBrown <brown@woowahan.com>
2021-07-01 09:57:02 +0900

    feat(member): roll back /members/me api

--- backend/src/main/java/wooteco/prolog/member/ui/MemberController.java
+++ backend/src/main/java/wooteco/prolog/member/ui/MemberController.java
@@ -3,9 +3,9 @@ package wooteco.prolog.member.ui;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import wooteco.prolog.login.domain.AuthMemberPrincipal;
 import wooteco.prolog.member.application.MemberService;
 import wooteco.prolog.member.application.dto.MemberResponse;
-import wooteco.prolog.login.domain.AuthMemberPrincipal;
 import wooteco.prolog.member.application.dto.MemberUpdateRequest;
 import wooteco.prolog.member.domain.Member;
 
@@ -16,6 +16,11 @@ public class MemberController {
 
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping(value = "/me", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<MemberResponse> findMemberInfoOfMine(@AuthMemberPrincipal Member member) {
+        return ResponseEntity.ok().body(MemberResponse.of(member));
     }
 
     @GetMapping(value = "/{username}", produces = MediaType.APPLICATION_JSON_VALUE)
