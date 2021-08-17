09101496ffc132d4257258d17b7333b2982bd51d
Hyeon9mak <jinha3507@gmail.com>
2021-08-17 17:40:44 +0900

    test: 멤버 Repository 테스트 코드 추가

---
+++ backend/src/test/java/wooteco/prolog/member/domain/repository/MemberRepositoryTest.java
@@ -0,0 +1,74 @@
+package wooteco.prolog.member.domain.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import wooteco.prolog.member.domain.Member;
+import wooteco.prolog.member.domain.Role;
+
+@DataJpaTest
+class MemberRepositoryTest {
+
+    private static final Member 웨지 = new Member("sihyung92", "웨지", Role.CREW, 2222L, "https://avatars.githubusercontent.com/u/51393021?v=4");
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @DisplayName("githubId로 Member 검색 - 성공")
+    @Test
+    void findByGithubId() {
+        // given
+        Member member = 멤버를_생성한다(웨지);
+
+        // when
+        Optional<Member> foundMember = memberRepository.findByGithubId(웨지.getGithubId());
+
+        // then
+        assertThat(foundMember).isPresent();
+        assertThat(foundMember.get()).usingRecursiveComparison()
+            .isEqualTo(member);
+    }
+
+    @DisplayName("githubId로 Member 검색 - 실패")
+    @Test
+    void findByGithubIdFailed() {
+        // when
+        Optional<Member> foundMember = memberRepository.findByGithubId(웨지.getGithubId());
+
+        // then
+        assertThat(foundMember).isNotPresent();
+    }
+
+    @DisplayName("username으로 Member 검색 - 성공")
+    @Test
+    void findByUsername() {
+        // given
+        Member member = 멤버를_생성한다(웨지);
+
+        // when
+        Optional<Member> foundMember = memberRepository.findByUsername(웨지.getUsername());
+
+        // then
+        assertThat(foundMember).isPresent();
+        assertThat(foundMember.get()).usingRecursiveComparison()
+            .isEqualTo(member);
+    }
+
+    @DisplayName("username으로 Member 검색 - 실패")
+    @Test
+    void findByUsernameFailed() {
+        // when
+        Optional<Member> foundMember = memberRepository.findByUsername(웨지.getUsername());
+
+        // then
+        assertThat(foundMember).isNotPresent();
+    }
+
+    private Member 멤버를_생성한다(Member member) {
+        return memberRepository.save(member);
+    }
+}
