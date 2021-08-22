fde77e614224b39da2b71da4c668028b574c9b71
Hyeon9mak <jinha3507@gmail.com>
2021-08-22 09:22:48 +0900

    feat: 엔티티 중복 필드 추상화

--- backend/src/main/java/wooteco/prolog/PrologApplication.java
+++ backend/src/main/java/wooteco/prolog/PrologApplication.java
@@ -2,12 +2,13 @@ package wooteco.prolog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class PrologApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PrologApplication.class, args);
     }
-
 }
--- backend/src/main/java/wooteco/prolog/mission/domain/Mission.java
+++ backend/src/main/java/wooteco/prolog/mission/domain/Mission.java
@@ -1,26 +1,16 @@
 package wooteco.prolog.mission.domain;
 
-import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import wooteco.prolog.BaseEntity;
 
-@Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-public class Mission {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+@Entity
+public class Mission extends BaseEntity {
 
     @Column
     private String name;
@@ -29,20 +19,8 @@ public class Mission {
         this(null, name);
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Mission)) {
-            return false;
-        }
-        Mission mission = (Mission) o;
-        return Objects.equals(id, mission.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
+    public Mission(Long id, String name) {
+        super(id);
+        this.name = name;
     }
 }
