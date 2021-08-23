5a0b6b008bc9b2a145fcd43299aa15fc6084b785
sihyung92 <fjzjqhdl@gmail.com>
2021-08-23 20:19:36 +0900

    feat : jpa 테이블 생성 방식 및 타임존 설정 변경

--- backend/src/main/java/wooteco/prolog/PrologApplication.java
+++ backend/src/main/java/wooteco/prolog/PrologApplication.java
@@ -1,5 +1,7 @@
 package wooteco.prolog;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -7,6 +9,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class PrologApplication {
+
+    @PostConstruct
+    private void setTimeZone(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PrologApplication.class, args);
