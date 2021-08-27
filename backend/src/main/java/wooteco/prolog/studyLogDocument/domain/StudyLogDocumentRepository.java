161bc1fc8d1a59390d191e28df54be0b58302d34
seovalue <seominjeong.dev@gmail.com>
2021-08-27 23:36:05 +0900

    feat: StudyLogDocument 및 Repository 생성

---
+++ backend/src/main/java/wooteco/prolog/studyLogDocument/domain/StudyLogDocumentRepository.java
@@ -0,0 +1,12 @@
+package wooteco.prolog.studyLogDocument.domain;
+
+import java.util.List;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.annotations.Query;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+public interface StudyLogDocumentRepository extends ElasticsearchRepository<StudyLogDocument, Long> {
+
+    @Query("{\"query_string\": {\"fields\": [\"title\",\"content\"], \"query\": \"*?0*\"}}")
+    List<StudyLogDocument> findByKeyword(String keyword, Pageable pageable);
+}
