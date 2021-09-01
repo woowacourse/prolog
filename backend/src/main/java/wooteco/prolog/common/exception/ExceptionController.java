aee052dec4ba8ba95ffb4f6e45496487f90b41a1
Minsung Son <sns2831@gmail.com>
2021-09-02 08:55:34 +0900

    백엔드 로깅을 도입한다. (#346)

--- backend/src/main/java/wooteco/prolog/common/exception/ExceptionController.java
+++ backend/src/main/java/wooteco/prolog/common/exception/ExceptionController.java
@@ -1,15 +1,26 @@
 package wooteco.prolog.common.exception;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class ExceptionController {
 
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<BadRequestExceptionDto> loginExceptionHandler(BadRequestException e) {
+    public ResponseEntity<ExceptionDto> loginExceptionHandler(BadRequestException e) {
+        log.warn(e.getMessage());
         return ResponseEntity.badRequest()
-                .body(new BadRequestExceptionDto(e.getCode(), e.getMessage()));
+                .body(new ExceptionDto(e.getCode(), e.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionDto> runtimeExceptionHandler(Exception e) {
+        log.error(e.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(new ExceptionDto(500, "알 수 없는 에러"));
     }
 }
