4fa31bdfb35369b20b8b511b9a8e4070ac8de22d
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-09-23 00:58:59 +0900

    feat: 로그인이 안된 사용자는 리포트 작성 페이지에 접근할 수 없도록 조건 추가

--- frontend/src/pages/ProfilePageNewReport/index.js
+++ frontend/src/pages/ProfilePageNewReport/index.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory, useParams } from 'react-router-dom';
+import localStorage from 'local-storage';
+
 import { Button } from '../../components';
-import { COLOR } from '../../constants';
+import { API, COLOR } from '../../constants';
 import StudyLogModal from './StudyLogModal';
 import ReportInfoInput from './ReportInfoInput';
 import ReportStudyLogTable from './ReportStudyLogTable';
@@ -11,7 +13,6 @@ import { Checkbox, Form, FormButtonWrapper } from './style';
 
 // TODO 1. 페이지 나갈 떄, 확인하는 기능 넣기
 // TODO 3. 학습로그 한 개 이상 선택하도록 하기
-// TODO 4. 로그인이 안된 사용자는 접근이 불가하도록 처리하기
 // TODO 5. 로그인이 풀렸을 경우에 어떻게 처리할 것인지 알아보기 -> 글 쓰던게 날아갈 수 있음.
 
 const ProfilePageNewReport = () => {
@@ -25,6 +26,11 @@ const ProfilePageNewReport = () => {
     if (isLoggedIn) {
       if (username !== user.data.username) {
         alert('본인의 리포트만 작성할 수 있습니다.');
+        history.push(`/${username}/reports`);
+      }
+    } else {
+      if (!localStorage.get(API.ACCESS_TOKEN)) {
+        alert('로그인한 사용자만 이용할 수 있습니다.');
         history.push(`/${username}/reports`);
       }
     }
