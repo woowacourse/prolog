42cb1bc273b5bb07d6dbacb0129ddddfafa09115
Minsung Son <sns2831@gmail.com>
2021-09-22 18:27:50 +0900

    [리포트] 선택된 학습로그들을 불러오는 API를 구현한다. (#417)
    * feat: id로 여러개의 studylog를 가지고 오는 api 구현
    
    * refactor: 사용하지 않는 메서드 제거

--- backend/src/main/java/wooteco/prolog/studylog/ui/StudylogController.java
+++ backend/src/main/java/wooteco/prolog/studylog/ui/StudylogController.java
@@ -2,6 +2,9 @@ package wooteco.prolog.studylog.ui;
 
 import java.net.URI;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import wooteco.prolog.login.domain.AuthMemberPrincipal;
 import wooteco.prolog.member.domain.Member;
