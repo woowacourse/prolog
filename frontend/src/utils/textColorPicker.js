cd395f530e70b7bda310e7fc49be99a528570e73
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-09-28 02:42:45 +0900

    feat: 텍스트 컬러 대비 계산하는 함수 구현

---
+++ frontend/src/utils/textColorPicker.js
@@ -0,0 +1,15 @@
+import { COLOR } from '../constants';
+
+function getTextColor(hexColor) {
+  const rgb = parseInt(hexColor.substring(1), 16);
+
+  const r = (rgb >> 16) & 0xff;
+  const g = (rgb >> 8) & 0xff;
+  const b = (rgb >> 0) & 0xff;
+
+  const luma = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+
+  return luma < 127.5 ? `${COLOR.WHITE}` : `${COLOR.BLACK_900}`;
+}
+
+export { getTextColor };
