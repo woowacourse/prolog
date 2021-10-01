a29ed915c40f18451972297d23f47c193b176ff8
EUNHYUN CHO <59258239+devhyun637@users.noreply.github.com>
2021-10-01 20:23:53 +0900

    fix: 삭제된 닉네임 수정 기능 코드 삽입 (#445)
    * fix: 삭제된 닉네임 수정 기능 코드 삽입
    
    * refactor: 닉네임 수정에 변경된 API 적용

--- frontend/src/components/ProfilePageSideBar/ProfilePageSideBar.js
+++ frontend/src/components/ProfilePageSideBar/ProfilePageSideBar.js
@@ -51,10 +51,6 @@ const ProfilePageSideBar = ({ menu }) => {
     history.push(`/${username}/reports`);
   };
 
-  // const goProfilePageAccount = () => {
-  //   history.push(`/${username}/account`);
-  // };
-
   const getProfile = async () => {
     try {
       const response = await requestGetProfile(username);
@@ -63,7 +59,10 @@ const ProfilePageSideBar = ({ menu }) => {
         throw new Error(await response.text());
       }
 
-      setUser(await response.json());
+      const user = await response.json();
+      console.log(user);
+      setUser(user);
+      setNickname(user.nickname);
       setNotFound(false);
     } catch (error) {
       const errorResponse = JSON.parse(error.message);
@@ -86,7 +85,10 @@ const ProfilePageSideBar = ({ menu }) => {
     }
 
     try {
-      const response = await requestEditProfile({ nickname, imageUrl: user.imageUrl }, accessToken);
+      const response = await requestEditProfile(
+        { username, nickname, imageUrl: user.imageUrl },
+        accessToken
+      );
 
       if (!response.ok) {
         throw new Error(await response.text());
@@ -104,65 +106,59 @@ const ProfilePageSideBar = ({ menu }) => {
   useEffect(() => {
     getProfile();
     setSelectedMenu(menu);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [username]);
 
   return (
-      <Container>
-        <Profile>
-          <Image src={user?.imageUrl} alt="프로필 이미지" /> <Role>{user?.role}</Role>
-          <NicknameWrapper>
-            {isProfileEditing ? (
-                <NicknameInput
-                    autoFocus
-                    value={nickname}
-                    onChange={({ target }) => setNickname(target.value)}
-                />
-            ) : (
-                <Nickname>{user?.nickname}</Nickname>
-            )}
-            {myName === username && (
-                <Button
-                    size={BUTTON_SIZE.X_SMALL}
-                    type="button"
-                    css={EditButtonStyle}
-                    alt={isProfileEditing ? '수정 완료 버튼' : '수정 버튼'}
-                    onClick={() => {
-                      isProfileEditing ? editProfile() : setIsProfileEditing(true);
-                    }}
-                >
-                  {isProfileEditing ? '완료' : '수정'}
-                </Button>
-            )}
-          </NicknameWrapper>
-        </Profile>
-        <MenuList>
-          <MenuItem isSelectedMenu={selectedMenu === PROFILE_PAGE_MENU.OVERVIEW}>
-            <MenuButton value={PROFILE_PAGE_MENU.OVERVIEW} type="button" onClick={goProfilePage}>
-              {/* <MenuIcon src={overviewIcon} alt="overview icon" /> */}
-              <OverviewIcon width="16" height="16" />
-              오버뷰
-            </MenuButton>
-          </MenuItem>
-          <MenuItem isSelectedMenu={selectedMenu === PROFILE_PAGE_MENU.POSTS}>
-            <MenuButton value={PROFILE_PAGE_MENU.POSTS} type="button" onClick={goProfilePagePosts}>
-              <PostIcon width="16" height="16" />
-              학습로그
-            </MenuButton>
-          </MenuItem>
-          <MenuItem isSelectedMenu={selectedMenu === PROFILE_PAGE_MENU.REPORTS}>
-            <MenuButton value={PROFILE_PAGE_MENU.REPORTS} type="button" onClick={goProfilePageReport}>
-              <PostIcon width="16" height="16" />
-              리포트
-            </MenuButton>
-          </MenuItem>
-          {/* <MenuItem>
-            <button type="button" onClick={goProfilePageAccount}>
-              내 정보 수정
-            </button>
-          </MenuItem> */}
-        </MenuList>
-      </Container>
+    <Container>
+      <Profile>
+        <Image src={user?.imageUrl} alt="프로필 이미지" /> <Role>{user?.role}</Role>
+        <NicknameWrapper>
+          {isProfileEditing ? (
+            <NicknameInput
+              autoFocus
+              value={nickname}
+              onChange={({ target }) => setNickname(target.value)}
+            />
+          ) : (
+            <Nickname>{user?.nickname}</Nickname>
+          )}
+          {myName === username && (
+            <Button
+              size={BUTTON_SIZE.X_SMALL}
+              type="button"
+              css={EditButtonStyle}
+              alt={isProfileEditing ? '수정 완료 버튼' : '수정 버튼'}
+              onClick={() => {
+                isProfileEditing ? editProfile() : setIsProfileEditing(true);
+              }}
+            >
+              {isProfileEditing ? '완료' : '수정'}
+            </Button>
+          )}
+        </NicknameWrapper>
+      </Profile>
+      <MenuList>
+        <MenuItem isSelectedMenu={selectedMenu === PROFILE_PAGE_MENU.OVERVIEW}>
+          <MenuButton value={PROFILE_PAGE_MENU.OVERVIEW} type="button" onClick={goProfilePage}>
+            {/* <MenuIcon src={overviewIcon} alt="overview icon" /> */}
+            <OverviewIcon width="16" height="16" />
+            오버뷰
+          </MenuButton>
+        </MenuItem>
+        <MenuItem isSelectedMenu={selectedMenu === PROFILE_PAGE_MENU.POSTS}>
+          <MenuButton value={PROFILE_PAGE_MENU.POSTS} type="button" onClick={goProfilePagePosts}>
+            <PostIcon width="16" height="16" />
+            학습로그
+          </MenuButton>
+        </MenuItem>
+        <MenuItem isSelectedMenu={selectedMenu === PROFILE_PAGE_MENU.REPORTS}>
+          <MenuButton value={PROFILE_PAGE_MENU.REPORTS} type="button" onClick={goProfilePageReport}>
+            <PostIcon width="16" height="16" />
+            리포트
+          </MenuButton>
+        </MenuItem>
+      </MenuList>
+    </Container>
   );
 };
 
