8b1776dc120dcd311a3a15778678fb4f3127708c
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-10-03 11:18:03 +0900

    refactor: console.log 삭제

--- frontend/src/components/ProfilePageSideBar/ProfilePageSideBar.js
+++ frontend/src/components/ProfilePageSideBar/ProfilePageSideBar.js
@@ -60,7 +60,6 @@ const ProfilePageSideBar = ({ menu }) => {
       }
 
       const user = await response.json();
-      console.log(user);
       setUser(user);
       setNickname(user.nickname);
       setNotFound(false);
