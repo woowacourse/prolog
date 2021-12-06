b3649cdd51ac0a033af1c71b633e254e70cf9fa4
xrabcde <xrabcde@gmail.com>
2021-12-07 00:45:36 +0900

    test: StudylogViewedRepository 테스트코드 작성

---
+++ backend/src/test/java/wooteco/prolog/studylog/studylog/domain/repository/StudylogViewedRepositoryTest.java
@@ -0,0 +1,87 @@
+package wooteco.prolog.studylog.studylog.domain.repository;
+
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import wooteco.prolog.member.domain.Member;
+import wooteco.prolog.member.domain.Role;
+import wooteco.prolog.member.domain.repository.MemberRepository;
+import wooteco.prolog.studylog.domain.Level;
+import wooteco.prolog.studylog.domain.Mission;
+import wooteco.prolog.studylog.domain.Studylog;
+import wooteco.prolog.studylog.domain.StudylogViewed;
+import wooteco.prolog.studylog.domain.repository.*;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class StudylogViewedRepositoryTest {
+
+    private static final Member 웨지 = new Member("sihyung92", "웨지", Role.CREW, 2222L,
+            "https://avatars.githubusercontent.com/u/51393021?v=4");
+
+    private static final Member 바다 = new Member("xrabcde", "바다", Role.CREW, 1111L,
+            "https://avatars.githubusercontent.com/u/56033755?v=4");
+
+    private Studylog studylog;
+    private Member member;
+    private Member bada;
+
+    @Autowired
+    private StudylogRepository studylogRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private LevelRepository levelRepository;
+
+    @Autowired
+    private MissionRepository missionRepository;
+
+    @Autowired
+    private StudylogViewedRepository studylogViewedRepository;
+
+    @BeforeEach
+    void setUp() {
+        member = memberRepository.save(웨지);
+        bada = memberRepository.save(바다);
+        Level level = levelRepository.save(new Level("레벨1"));
+        Mission mission = missionRepository.save(new Mission("미션", level));
+        studylog = studylogRepository
+                .save(new Studylog(member, "제목", "내용", mission, Lists.emptyList()));
+    }
+
+    @DisplayName("멤버 아이디와 스터디로그 아이디로 조회한 적이 있다면 true, 없다면 false를 반환한다.")
+    @Test
+    void existsByMemberIdAndStudylogId() {
+        //given
+        StudylogViewed studylogViewed = new StudylogViewed(bada, studylog);
+        //when
+        studylogViewedRepository.save(studylogViewed);
+        boolean expectTrue = studylogViewedRepository.existsByMemberIdAndStudylogId(bada.getId(), studylog.getId());
+        boolean expectFalse = studylogViewedRepository.existsByMemberIdAndStudylogId(member.getId(), studylog.getId());
+        //then
+        assertThat(expectTrue).isTrue();
+        assertThat(expectFalse).isFalse();
+    }
+
+    @DisplayName("멤버 아이디로 조회한 글목록을 가져온다.")
+    @Test
+    void findByMemberId() {
+        //given
+        StudylogViewed studylogViewed = new StudylogViewed(bada, studylog);
+        studylogViewedRepository.save(studylogViewed);
+        List<StudylogViewed> expected = Collections.singletonList(studylogViewed);
+        //when
+        List<StudylogViewed> actual = studylogViewedRepository.findByMemberId(bada.getId());
+        //then
+        assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
+    }
+}
