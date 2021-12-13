2efcfd659eb094099ca49222065bd8c68da1e141
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-12-13 16:56:13 +0900

    refactor: 역량 수정 유효성 검증 함수에 항상 return이 있도록 수정

--- frontend/src/pages/AbilityPage/EditAbilityForm.js
+++ frontend/src/pages/AbilityPage/EditAbilityForm.js
@@ -57,15 +57,20 @@ const EditAbilityForm = ({ id, name, color, description, isParent, onClose, onEd
    * 수정하고자 하는 역량의 정보가 이전과 동일하거나, 이름이 공백인 경우에는 등록이 불가능하다.
    *
    * @param {string} currentName - 현재의 수정하려고하는 역량 이름
+   * @returns boolean || undefined
    */
   const disableSaveButton = () => {
     const { name: currentName, description: currentDesc, color: currentColor } = formData;
+
+    if (!currentName.trim() || !currentColor.trim()) {
+      return true;
+    }
 
     if (currentName === name && currentDesc === description && currentColor === color) {
       return true;
     }
 
-    return !currentName.trim() || !currentColor.trim();
+    return false;
   };
 
   return (
