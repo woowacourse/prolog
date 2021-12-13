48975a24acec55eb35b42329ba0a7113bf762f9c
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-12-13 16:55:17 +0900

    refactor: hexCode 유효성 검증로직 함수에 jsdoc 작성

--- frontend/src/utils/hexCode.ts
+++ frontend/src/utils/hexCode.ts
@@ -1,3 +1,10 @@
+/**
+ * isCorrectHexCode 함수
+ * 사용자가 입력한 hexCode가 올바른 hexCode인지 유효성을 검증한다.
+ *
+ * @param {string} str - 사용자가 입력한 hexCode
+ * @returns boolean
+ */
 export const isCorrectHexCode = (str: string) => {
   const regex = /^#([0-9a-f]{3}){1,2}$/i;
 
