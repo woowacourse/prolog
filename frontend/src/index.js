9d91a43bebb431c1dacd790a2d2b19bbca2aca0b
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2022-01-07 07:42:21 +0900

    feat: 새로고침 시 로그인 정보를 유지하기 위해 redux-persist 적용

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
