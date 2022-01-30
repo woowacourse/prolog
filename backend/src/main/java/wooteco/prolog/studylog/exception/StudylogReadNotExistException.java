60d69bf89446f57a67b19eef20f02f2bf1f0ec52
xrabcde <xrabcde@gmail.com>
2022-01-30 21:50:53 +0900

    refactor: exception 개행 수정

--- backend/src/main/java/wooteco/prolog/studylog/exception/StudylogReadNotExistException.java
+++ backend/src/main/java/wooteco/prolog/studylog/exception/StudylogReadNotExistException.java
@@ -2,7 +2,6 @@ package wooteco.prolog.studylog.exception;
 
 import wooteco.prolog.common.exception.BadRequestException;
 
-public class StudylogReadNotExistException extends
-        BadRequestException {
+public class StudylogReadNotExistException extends BadRequestException {
 
 }
