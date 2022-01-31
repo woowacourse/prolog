6b7ebe734334bfdb9d2a6a5a0038ad4aae564950
xrabcde <xrabcde@gmail.com>
2022-01-31 09:55:28 +0900

    fix: studylog 삭제가 안되는 오류 수정 (#678)
    * fix: studylog 삭제 시 해당 글에 대한 스크랩과 읽음여부도 같이 삭제되도록 수정
    
    * refactor: exception 개행 수정

--- backend/src/main/java/wooteco/prolog/studylog/domain/repository/StudylogReadRepository.java
+++ backend/src/main/java/wooteco/prolog/studylog/domain/repository/StudylogReadRepository.java
@@ -4,9 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import wooteco.prolog.studylog.domain.StudylogRead;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface StudylogReadRepository extends JpaRepository<StudylogRead, Long> {
     boolean existsByMemberIdAndStudylogId(Long memberId, Long studylogId);
+
+    Optional<StudylogRead> findByMemberIdAndStudylogId(Long memberId, Long studylogId);
 
     List<StudylogRead> findByMemberId(Long memberId);
 }
