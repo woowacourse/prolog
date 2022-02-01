5a3651fff6344d288b09ca29b320759e71278e77
sunhpark42 <sunhpark42@gmail.com>
2022-02-01 09:45:43 +0900

    feat: 소개 작성 시 preview에 커스텀 마크다운 스타일 적용

--- frontend/src/styles/markdown.styles.js
+++ frontend/src/styles/markdown.styles.js
@@ -2,48 +2,51 @@ import { css } from '@emotion/react';
 import { COLOR } from '../constants';
 
 export const markdownStyle = css`
-  && * {
-    margin: 0;
-    line-height: 2;
-  }
+  .toastui-editor-md-preview,
+  .toastui-editor-contents {
+    && * {
+      margin: 0;
+      line-height: 2;
+    }
 
-  && h1:first-of-type {
-    margin-top: 0;
-  }
+    && h1:first-of-type {
+      margin-top: 0;
+    }
 
-  h1 {
-    border-bottom: 2px solid ${COLOR.LIGHT_GRAY_300};
-    padding: 0;
-  }
+    h1 {
+      border-bottom: 2px solid ${COLOR.LIGHT_GRAY_300};
+      padding: 0 0 7px;
+    }
 
-  h3 {
-    font-size: 1.8rem;
-  }
+    h3 {
+      font-size: 1.8rem;
+    }
 
-  a {
-    text-decoration: none;
-    font-size: 1.6rem;
-  }
+    a {
+      text-decoration: none;
+      font-size: 1.6rem;
+    }
 
-  p {
-    font-size: 1.4rem;
-  }
+    p {
+      font-size: 1.4rem;
+    }
 
-  && ul li::before {
-    margin-top: 8px;
-    transform: translateY(70%);
-  }
+    && ul li::before {
+      margin-top: 8px;
+      transform: translateY(70%);
+    }
 
-  && ol li::before {
-    font-size: 1.4rem;
-    margin-top: 3px;
-  }
+    && ol li::before {
+      font-size: 1.4rem;
+      margin-top: 3px;
+    }
 
-  && hr {
-    margin: 2rem 0;
-  }
+    && hr {
+      margin: 2rem 0;
+    }
 
-  && blockquote {
-    margin: 2rem 0;
+    && blockquote {
+      margin: 2rem 0;
+    }
   }
 `;
