676aa8d5743afafbe7cd8f4d7ef341e46c93d7ff
gracefulBrown <brown@woowahan.com>
2022-03-07 12:43:38 +0900

    feat(member): restore /members/me api

--- backend/src/main/java/wooteco/prolog/member/ui/MemberController.java
+++ backend/src/main/java/wooteco/prolog/member/ui/MemberController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import wooteco.prolog.login.aop.MemberOnly;
 import wooteco.prolog.login.domain.AuthMemberPrincipal;
 import wooteco.prolog.login.ui.LoginMember;
 import wooteco.prolog.member.application.MemberService;
@@ -47,5 +48,11 @@ public class MemberController {
     public ResponseEntity<MembersResponse> show(@PageableDefault(direction = DESC, sort = "id") Pageable pageable) {
         MembersResponse response = memberService.findAll(pageable);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping(value = "/me", produces = MediaType.APPLICATION_JSON_VALUE)
+    @MemberOnly
+    public ResponseEntity<MemberResponse> findMemberInfoOfMine(@AuthMemberPrincipal LoginMember member) {
+        return ResponseEntity.ok().body(MemberResponse.of(memberService.findById(member.getId())));
     }
 }
