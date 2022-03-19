5d26cde33b873b205ae4921630a2bd4bfe1b033d
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2022-03-20 00:19:32 +0900

    feat: useAbility, useParentAbilityForm 생성

---
+++ frontend/src/hooks/Ability/useAbility.ts
@@ -0,0 +1,58 @@
+import { useMutation, useQuery, useQueryClient } from 'react-query';
+import AbilityRequest from '../../apis/ability';
+import { DEFAULT_ABILITY_FORM } from './useParentAbilityForm';
+
+interface AbilityForm {
+  isOpened: boolean;
+  name: string;
+  description: string;
+  color: string;
+  isParent: boolean | null;
+}
+
+interface Props {
+  username: string;
+  setAddFormStatus: (data: AbilityForm) => void;
+  addFormClose: () => void;
+}
+
+const useAbility = ({ username, setAddFormStatus, addFormClose }: Props) => {
+  const queryClient = useQueryClient();
+
+  /** 전체 역량 조회 */
+  const { data: abilities = [] } = useQuery([`${username}-abilities`], () =>
+    AbilityRequest.getAbilityList({ url: `/members/${username}/abilities` })
+  );
+
+  /** 역량 등록 */
+  const onAddAbility = useMutation(
+    (ability) => AbilityRequest.addAbility({ url: `/abilities`, data: ability }),
+    {
+      onSuccess: () => {
+        setAddFormStatus({ ...DEFAULT_ABILITY_FORM, isOpened: true });
+        queryClient.invalidateQueries([`${username}-abilities`]);
+      },
+      onError: () => {
+        alert('역량 등록에 실패하였습니다.');
+      },
+    }
+  );
+
+  /** 역량 삭제 */
+  const onDeleteAbility = useMutation(
+    (id) => AbilityRequest.deleteAbility({ url: `/abilities/${id}` }),
+    {
+      onSuccess: () => {
+        addFormClose();
+        queryClient.invalidateQueries([`${username}-abilities`]);
+      },
+      onError: () => {
+        alert('역량 삭제에 실패하였습니다. 잠시후 다시 시도해주세요.');
+      },
+    }
+  );
+
+  return { abilities, onAddAbility, onDeleteAbility };
+};
+
+export default useAbility;
---
+++ frontend/src/hooks/Ability/useParentAbilityForm.ts
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+export const DEFAULT_ABILITY_FORM = {
+  isOpened: false,
+  name: '',
+  description: '',
+  color: '#000000',
+  isParent: null,
+};
+
+const useParentAbilityForm = () => {
+  const [addFormStatus, setAddFormStatus] = useState(DEFAULT_ABILITY_FORM);
+
+  /** 부모 역량 추가 섹션 열기 */
+  const addFormOpen = () => {
+    setAddFormStatus((prevState) => ({ ...prevState, isOpened: true }));
+  };
+
+  /** 부모 역량 추가 섹션 닫기 */
+  const addFormClose = () => {
+    setAddFormStatus((prevState) => ({ ...prevState, isOpened: false }));
+  };
+
+  return { addFormStatus, setAddFormStatus, addFormClose, addFormOpen };
+};
+
+export default useParentAbilityForm;
