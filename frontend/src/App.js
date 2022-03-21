c62adae893f1a05af8aceddec988af2522228964
gracefulBrown <brown@woowahan.com>
2022-03-21 19:28:51 +0900

    feat(ga)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
-import {
-  QueryClient,
-  QueryClientProvider,
-} from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import ReactGA from 'react-ga';
+const TRACKING_ID = process.env.REACT_APP_GA_TRACKING_ID;
+ReactGA.initialize(TRACKING_ID);
 
 import useSnackBar from './hooks/useSnackBar';
 import GlobalStyles from './GlobalStyles';
 import PageRouter from './PageRouter';
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 const App = () => {
   const { isSnackBarOpen, SnackBar } = useSnackBar();
