78b374c3bb37180b17e97c1b47d5c046b8b83dc7
gracefulBrown <brown@woowahan.com>
2022-03-22 13:35:49 +0900

    refactor(docu) change ability documentation

--- backend/src/main/java/wooteco/prolog/common/JpaAuditingConfig.java
+++ backend/src/main/java/wooteco/prolog/common/JpaAuditingConfig.java
@@ -1,10 +1,12 @@
 package wooteco.prolog.common;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@Configuration
+@Profile("!docu")
 @EnableJpaAuditing
-public class DataJpaConfig {
+@Configuration
+public class JpaAuditingConfig {
 
 }
