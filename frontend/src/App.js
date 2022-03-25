4bcea10b6760cdd13f6c2da1ce3d152620967f39
류성현 <brown@woowahan.com>
2022-03-25 11:32:01 +0900

    feat(gtm): add event (#756)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
-import {
-  QueryClient,
-  QueryClientProvider,
-} from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import TagManager from 'react-gtm-module';
 
 import useSnackBar from './hooks/useSnackBar';
 import GlobalStyles from './GlobalStyles';
 import PageRouter from './PageRouter';
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
+
+const tagManagerArgs = {
+  gtmId: process.env.REACT_APP_GTM_ID,
+};
+
+TagManager.initialize(tagManagerArgs);
 
 const App = () => {
   const { isSnackBarOpen, SnackBar } = useSnackBar();
