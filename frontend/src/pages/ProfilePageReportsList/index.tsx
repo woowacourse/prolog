34cb5829abd2cdaa607a0cac63cef853d6415ab0
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2022-04-02 12:52:11 +0900

    feat: 리포트 리스트 페이지 타임라인 UI 구현

--- frontend/src/pages/ProfilePageReportsList/index.tsx
+++ frontend/src/pages/ProfilePageReportsList/index.tsx
@@ -1,28 +1,6 @@
-import { useContext, useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { css } from '@emotion/react';
-
 import { Ability } from '../ProfilePageReports/AbilityGraph';
 
-import useRequest from '../../hooks/useRequest';
-import { requestGetReportList } from '../../service/requests';
-import { UserContext } from '../../contexts/UserProvider';
-
-import { Chip, Pagination } from '../../components';
-
-import { COLOR, REQUEST_REPORT_TYPE } from '../../constants';
-
-import {
-  Container,
-  AddNewReportLink,
-  ReportList,
-  Card,
-  AbilityList,
-  StudylogCount,
-  Badge,
-} from './styles';
-
-import { ReactComponent as StudylogIcon } from '../../assets/images/post.svg';
+import * as Styled from './styles';
 
 type Report = {
   id: number;
@@ -44,141 +22,78 @@ const defaultReports = {
 };
 
 const ProfilePageReportsList = () => {
-  const { username } = useParams<{ username: string }>();
+  //   if (!reportList?.length) {
+  //     return (
+  //       <Container
+  //         css={css`
+  //           height: 70vh;
 
-  const [reports, setReports] = useState(defaultReports);
+  //           display: flex;
+  //           flex-direction: column;
+  //           justify-content: center;
+  //           align-items: center;
 
-  const { user: loginUser } = useContext(UserContext);
-  const { username: loginUsername } = loginUser;
+  //           p {
+  //             margin: 0;
+  //             font-size: 2rem;
+  //             line-height: 1.5;
+  //           }
+  //         `}
+  //       >
+  //         <p>등록된 리포트가 없습니다.</p>
+  //         {isOwner && (
+  //           <>
+  //             <p>리포트를 작성해주세요.</p>
+  //             <AddNewReportLink to={`/${username}/reports/write`}>새 리포트 등록</AddNewReportLink>
+  //           </>
+  //         )}
+  //       </Container>
+  //     );
+  //   }
 
-  const isOwner = username === loginUsername;
-
-  // const { response: reports, fetchData: getReports } = useRequest({}, (page = 1) =>
-  //   requestGetReportList({
-  //     username,
-  //     type: REQUEST_REPORT_TYPE.ALL,
-  //     size: 4,
-  //     page,
-  //   })
-  // );
-
-  // useEffect(() => {
-  //   getReports();
-  // }, [username]);
-
-  const getReports = async (page = 1) => {
-    try {
-      const response = await requestGetReportList({
-        username,
-        type: REQUEST_REPORT_TYPE.ALL,
-        size: 4,
-        page,
-      });
-
-      if (!response.ok) {
-        throw new Error(await response.text());
-      }
-
-      const fetchReports = await response.json();
-      setReports({ ...fetchReports, currPage: page });
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    getReports();
-  }, []);
-
-  const { reports: reportList } = reports;
-
-  const onSetPage = (page: number) => {
-    getReports(page);
-  };
-
-  if (!reportList?.length) {
-    return (
-      <Container
-        css={css`
-          height: 70vh;
-
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-
-          p {
-            margin: 0;
-            font-size: 2rem;
-            line-height: 1.5;
-          }
-        `}
-      >
-        <p>등록된 리포트가 없습니다.</p>
-        {isOwner && (
-          <>
-            <p>리포트를 작성해주세요.</p>
-            <AddNewReportLink to={`/${username}/reports/write`}>새 리포트 등록</AddNewReportLink>
-          </>
-        )}
-      </Container>
-    );
-  }
-
+  // TODO1. 작성된 리포트가 없다면, 리포트 작성하기 페이지를 보여준다.
   return (
-    <Container>
-      {isOwner && (
-        <AddNewReportLink
-          to={`/${username}/reports/write`}
-          css={css`
-            position: absolute;
-            bottom: 0.5rem;
-            right: 0.5rem;
-          `}
-        >
-          새 리포트 등록
-        </AddNewReportLink>
-      )}
+    <Styled.Container>
+      <h2>리포트페이지</h2>
 
-      <ReportList>
-        {reportList?.map(
-          ({ id, title, description, abilityGraph, studylogs, represent, createdAt }: Report) => (
-            <Card key={id}>
-              {represent && (
-                <Badge>
-                  <span>대표 리포트</span>
-                </Badge>
-              )}
+      <Styled.TimelineWrapper>
+        <Styled.AddNewReportLink to={`/devhyun637/reports/write`}>
+          <span>+</span>
+        </Styled.AddNewReportLink>
 
-              <Link to={`/${username}/reports/${id}`}>
-                <h4>{title}</h4>
-                <p>{description}</p>
+        <Styled.Reports>
+          <Styled.Report>
+            <Styled.ReportDate>2022.03.01 ~ 2022.03.31</Styled.ReportDate>
+            <Styled.ReportDesc>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores nobis sit minima
+              rerum blanditiis sed, fuga soluta ad quisquam rem voluptates delectus eaque aliquid
+              ducimus nemo eum. Qui, sapiente voluptatum!
+            </Styled.ReportDesc>
+            <Styled.TextButton>리포트 보러가기 {'>'} </Styled.TextButton>
+          </Styled.Report>
 
-                <AbilityList>
-                  {abilityGraph.abilities
-                    .filter(({ isPresent }) => isPresent)
-                    .map(({ id, name }: Ability) => (
-                      <li key={id}>
-                        <Chip title={name} backgroundColor={`${COLOR.LIGHT_BLUE_100}`}>
-                          {name}
-                        </Chip>
-                      </li>
-                    ))}
-                </AbilityList>
+          <Styled.Report>
+            <Styled.ReportDate>2022.03.01 ~ 2022.03.31</Styled.ReportDate>
+            <Styled.ReportDesc>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores nobis sit minima
+              rerum blanditiis sed, fuga soluta ad quisquam rem voluptates delectus eaque aliquid
+              ducimus nemo eum. Qui, sapiente voluptatum!
+            </Styled.ReportDesc>
+            <Styled.TextButton>리포트 보러가기 {'>'} </Styled.TextButton>
+          </Styled.Report>
 
-                <StudylogCount>
-                  <StudylogIcon />
-                  <p>{studylogs.length}개의 학습로그</p>
-                </StudylogCount>
-
-                <time>{new Date(createdAt).toLocaleDateString()}</time>
-              </Link>
-            </Card>
-          )
-        )}
-      </ReportList>
-      <Pagination dataInfo={reports} onSetPage={onSetPage} />
-    </Container>
+          <Styled.Report>
+            <Styled.ReportDate>2022.03.01 ~ 2022.03.31</Styled.ReportDate>
+            <Styled.ReportDesc>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores nobis sit minima
+              rerum blanditiis sed, fuga soluta ad quisquam rem voluptates delectus eaque aliquid
+              ducimus nemo eum. Qui, sapiente voluptatum!
+            </Styled.ReportDesc>
+            <Styled.TextButton>리포트 보러가기 {'>'} </Styled.TextButton>
+          </Styled.Report>
+        </Styled.Reports>
+      </Styled.TimelineWrapper>
+    </Styled.Container>
   );
 };
 
--- frontend/src/pages/ProfilePageReportsList/styles.ts
+++ frontend/src/pages/ProfilePageReportsList/styles.ts
@@ -6,219 +6,166 @@ import { COLOR } from '../../constants';
 
 export const Container = styled.section<{ css?: SerializedStyles }>`
   width: 100%;
-  height: fit-content;
+  height: 100%;
   padding: 2rem;
-  padding-left: 0;
   margin-bottom: 7rem;
 
   position: relative;
 
-  border-radius: 1.5rem;
-  border: 0.5px solid ${COLOR.LIGHT_GRAY_200};
-  background-color: ${COLOR.LIGHT_GRAY_100};
-
   ${({ css }) => css};
 `;
 
-export const AddNewReportLink = styled(NavLink)<{ css?: SerializedStyles }>`
-  width: fit-content;
-  height: 3rem;
-  margin: 1rem;
-  padding: 0.8rem 2rem;
+// export const AddNewReportLink = styled(NavLink)<{ css?: SerializedStyles }>`
+//   width: fit-content;
+//   height: 3rem;
+//   margin: 1rem;
+//   padding: 0.8rem 2rem;
 
-  display: flex;
-  align-items: center;
-  z-index: 0;
+//   display: flex;
+//   align-items: center;
+//   z-index: 0;
 
-  border-radius: 1rem;
-  background-color: ${COLOR.LIGHT_BLUE_800};
+//   border-radius: 1rem;
+//   background-color: ${COLOR.LIGHT_BLUE_800};
 
-  font-size: 1.3rem;
-  color: ${COLOR.WHITE};
+//   font-size: 1.3rem;
+//   color: ${COLOR.WHITE};
 
-  ${({ css }) => css};
+//   ${({ css }) => css};
 
-  :hover {
-    background-color: ${COLOR.DARK_BLUE_300};
-  }
-`;
+//   :hover {
+//     background-color: ${COLOR.DARK_BLUE_300};
+//   }
+// `;
 
-export const ReportList = styled.ol`
-  width: 100%;
-  height: 55rem;
-  margin-left: 2rem;
-  position: relative;
-
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  align-content: space-between;
-  row-gap: 2rem;
-`;
-
-export const Card = styled.li`
-  width: 38rem;
-  height: 27rem;
-
-  position: relative;
-
-  border-radius: 0.5rem;
-  background-color: ${COLOR.WHITE};
-
-  word-break: break-all;
-  word-wrap: break-word;
-  line-height: 1.25;
-
-  :hover {
-    box-shadow: 0.5rem 0.5rem 0.5rem ${COLOR.LIGHT_GRAY_200};
-
-    transform: scale(1.02);
-    transition: transform 0.1s;
-
-    cursor: pointer;
-  }
-
-  a {
-    width: 100%;
-    height: 100%;
-    padding: 1.5rem;
-
-    display: block;
-
-    > p {
-      min-height: 3.3rem;
-      margin: 1rem 0;
-
-      display: -webkit-box;
-      -webkit-line-clamp: 3;
-      -webkit-box-orient: vertical;
-      overflow: hidden;
-
-      font-size: 1.3rem;
-      color: ${COLOR.DARK_GRAY_500};
-    }
-  }
-
-  h4 {
-    margin: 1rem 0;
-
-    display: -webkit-box;
-    -webkit-line-clamp: 2;
-    -webkit-box-orient: vertical;
-    overflow: hidden;
-
-    font-size: 1.8rem;
-  }
-
-  time {
-    display: block;
-    position: absolute;
-    bottom: 1rem;
-    right: 1rem;
-
-    text-align: right;
-    font-size: 1.2rem;
-    color: ${COLOR.DARK_GRAY_600};
-  }
-`;
-
-export const AbilityList = styled.ul`
-  width: 100%;
-  max-height: 6rem;
-  overflow: hidden;
-
-  display: flex;
-  flex-wrap: wrap;
-  align-items: flex-start;
-
-  > li {
-    max-width: 100%;
-    margin: 0.5rem 0;
-
-    div {
-      max-width: 100%;
-
-      span {
-        overflow: hidden;
-        white-space: nowrap;
-        text-overflow: ellipsis;
-        font-size: 1.3rem;
-        color: ${COLOR.DARK_GRAY_700};
-      }
-    }
-  }
-`;
-
-export const StudylogCount = styled.div`
-  margin: 0.5rem;
-  margin-top: 1.5rem;
-
-  display: flex;
-  position: absolute;
-  bottom: 2rem;
-
-  svg {
-    width: 2rem;
-  }
-
-  && > p {
-    margin: 0 1rem;
-
-    font-size: 1.4rem;
-    line-height: 2rem;
-    color: ${COLOR.BLACK_900};
-  }
-`;
-
-export const Badge = styled.div`
-  width: 10rem;
-  height: 10rem;
-  overflow: hidden;
-  position: absolute;
-  top: -1rem;
-  right: -1rem;
-
-  ::before,
-  ::after {
+export const TimelineWrapper = styled.div`
+  :before {
     content: '';
-    display: block;
-
+    height: 80%;
     position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 2px;
+    background-color: ${COLOR.DARK_BLUE_900};
+  }
+`;
 
-    border: 5px solid ${COLOR.DARK_BLUE_500};
+export const Reports = styled.ul``;
+
+export const AddNewReportLink = styled(NavLink)`
+  width: 3rem;
+  height: 3rem;
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border: 1px solid ${COLOR.DARK_BLUE_900};
+  background-color: ${COLOR.DARK_BLUE_900};
+  color: ${COLOR.WHITE};
+  border-radius: 50%;
+  font-size: 2.5rem;
+
+  span {
+    padding-bottom: 0.2rem;
   }
 
-  ::before {
-    z-index: 0;
-    top: 0;
-    left: 0;
+  :hover {
+    border: 1px solid ${COLOR.DARK_BLUE_900};
+    background-color: ${COLOR.LIGHT_BLUE_200};
+    color: ${COLOR.DARK_BLUE_900};
+  }
+`;
+
+export const Report = styled.li`
+  width: 35rem;
+  height: 10rem;
+  margin-bottom: 5rem;
+  padding: 1rem;
+  position: relative;
+
+  border-radius: 2rem;
+  background-color: ${COLOR.LIGHT_BLUE_200};
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+
+  :first-of-type {
+    margin-top: 10rem;
   }
 
-  ::after {
-    z-index: 0;
-    bottom: 0;
-    right: 0;
+  :nth-of-type(odd) {
+    float: right;
+    clear: left;
+    border-top-left-radius: 0;
+
+    :after {
+      left: 0;
+      transform: translateX(-4.6rem);
+    }
   }
 
-  > span {
-    width: 16rem;
-    display: block;
-    padding: 0.3rem 0;
+  :nth-of-type(even) {
+    float: left;
+    clear: right;
+    border-top-right-radius: 0;
 
+    :after {
+      right: 0;
+      transform: translateX(4.6rem);
+    }
+  }
+
+  :after {
+    content: '';
     position: absolute;
-    left: -2rem;
-    top: 2.3rem;
-    transform: rotate(45deg);
+    top: 2rem;
+    width: 1.5rem;
+    height: 1.5rem;
+    border: 1px solid ${COLOR.DARK_BLUE_900};
+    border-radius: 50%;
+    background-color: ${COLOR.DARK_BLUE_900};
+  }
 
-    background-color: ${COLOR.LIGHT_BLUE_800};
-    box-shadow: 0 0.5rem 1rem ${COLOR.BLACK_OPACITY_200};
-    border: 0.2rem dotted ${COLOR.WHITE};
-    outline: 0.4rem solid ${COLOR.LIGHT_BLUE_800};
+  :hover {
+    transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
 
-    color: ${COLOR.WHITE};
-    font-size: 1.4rem;
-    text-shadow: 0 1px 1px ${COLOR.BLACK_OPACITY_200};
-    text-align: center;
+    :after {
+      border: 1px solid ${COLOR.LIGHT_BLUE_400};
+      background-color: ${COLOR.LIGHT_BLUE_400};
+    }
+  }
+`;
 
-    z-index: 1;
+export const ReportDate = styled.span`
+  font-size: 1.2rem;
+  font-weight: 500;
+  position: absolute;
+  top: -2.5rem;
+  left: 0;
+`;
+
+export const ReportDesc = styled.p`
+  margin: 0;
+
+  display: -webkit-box;
+  overflow: hidden;
+  font-size: 1.2rem;
+  text-overflow: ellipsis;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+`;
+
+export const TextButton = styled.button`
+  position: absolute;
+  bottom: 1rem;
+  font-size: 1.2rem;
+  font-weight: 600;
+
+  border: none;
+
+  :hover {
+    font-size: 1.3rem;
+    color: ${COLOR.RED_900};
   }
 `;
