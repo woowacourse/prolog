3a9a78611a2c5eb631aa81182416c1f1400bbe11
gracefulBrown <brown@woowahan.com>
2022-04-09 15:59:29 +0900

    refactor(level): 레벨을 세션으로 변경한다. feat(studylog): 학습로그 등록 시 세션과 미션을 선택적으로 등록할 수 있다.

---
+++ backend/src/acceptanceTest/java/wooteco/prolog/fixtures/SessionAcceptanceFixture.java
@@ -0,0 +1,9 @@
+package wooteco.prolog.fixtures;
+
+import wooteco.prolog.session.application.dto.SessionRequest;
+
+public class SessionAcceptanceFixture {
+
+    public static SessionRequest session1 = new SessionRequest("세션1");
+    public static SessionRequest session2 = new SessionRequest("세션2");
+}
---
+++ backend/src/main/java/wooteco/prolog/studylog/application/PopularStudylogService.java
@@ -0,0 +1,103 @@
+package wooteco.prolog.studylog.application;
+
+import static java.util.stream.Collectors.toList;
+
+import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import wooteco.prolog.studylog.application.dto.StudylogResponse;
+import wooteco.prolog.studylog.application.dto.StudylogsResponse;
+import wooteco.prolog.studylog.domain.PopularStudylog;
+import wooteco.prolog.studylog.domain.Studylog;
+import wooteco.prolog.studylog.domain.repository.PopularStudylogRepository;
+import wooteco.prolog.studylog.domain.repository.StudylogRepository;
+
+@Service
+@AllArgsConstructor
+@Transactional(readOnly = true)
+public class PopularStudylogService {
+
+    private static final int A_WEEK = 7;
+
+    private final StudylogService studylogService;
+    private final StudylogRepository studylogRepository;
+    private final PopularStudylogRepository popularStudylogRepository;
+
+    @Transactional
+    public void updatePopularStudylogs(Pageable pageable) {
+        deleteAllLegacyPopularStudylogs();
+
+        List<Studylog> studylogs = findStudylogsByDays(pageable, LocalDateTime.now());
+        List<PopularStudylog> popularStudylogs = studylogs.stream()
+            .map(it -> new PopularStudylog(it.getId()))
+            .collect(toList());
+
+        popularStudylogRepository.saveAll(popularStudylogs);
+    }
+
+    public StudylogsResponse findPopularStudylogs(Pageable pageable, Long memberId, boolean isAnonymousMember) {
+        List<Studylog> studylogs = getSortedPopularStudyLogs();
+        PageImpl<Studylog> page = new PageImpl<>(studylogs, pageable, studylogs.size());
+        StudylogsResponse studylogsResponse = StudylogsResponse.of(page, memberId);
+
+        if (isAnonymousMember) {
+            return studylogsResponse;
+        }
+        List<StudylogResponse> data = studylogsResponse.getData();
+        studylogService.updateScrap(data, studylogService.findScrapIds(memberId));
+        studylogService.updateRead(data, studylogService.findReadIds(memberId));
+        return studylogsResponse;
+    }
+
+    private void deleteAllLegacyPopularStudylogs() {
+        List<PopularStudylog> popularStudylogs = popularStudylogRepository.findAllByDeletedFalse();
+
+        for (PopularStudylog popularStudylog : popularStudylogs) {
+            popularStudylog.delete();
+        }
+    }
+
+    private List<Studylog> getSortedPopularStudyLogs() {
+        return studylogRepository.findAllById(getPopularStudylogIds())
+            .stream()
+            .sorted(Comparator.comparing(Studylog::getPopularScore).reversed())
+            .collect(toList());
+    }
+
+    private List<Long> getPopularStudylogIds() {
+        return popularStudylogRepository.findAllByDeletedFalse()
+            .stream()
+            .map(PopularStudylog::getStudylogId)
+            .collect(toList());
+    }
+
+    private List<Studylog> findStudylogsByDays(Pageable pageable, LocalDateTime dateTime) {
+        int decreaseDays = 0;
+        int searchFailedCount = 0;
+
+        while (true) {
+            decreaseDays += A_WEEK;
+            List<Studylog> studylogs = studylogRepository.findByPastDays(dateTime.minusDays(decreaseDays));
+
+            if (studylogs.size() >= pageable.getPageSize()) {
+                return studylogs.stream()
+                    .sorted(Comparator.comparing(Studylog::getPopularScore).reversed())
+                    .collect(toList())
+                    .subList(0, pageable.getPageSize());
+            }
+
+            if (searchFailedCount >= 2) {
+                return studylogs.stream()
+                    .sorted(Comparator.comparing(Studylog::getPopularScore).reversed())
+                    .collect(toList());
+            }
+
+            searchFailedCount += 1;
+        }
+    }
+}
