90f181a8e92c7ec4757ebf26882e14f81fc9d061
dwl5 <ssop6403@gmail.com>
2022-04-11 00:06:48 +0900

    feat: 스터디로그 조회 수 증가 방지 쿠키 적용

---
+++ backend/src/main/java/wooteco/prolog/studylog/application/ViewedStudyLogCookieGenerator.java
@@ -0,0 +1,39 @@
+package wooteco.prolog.studylog.application;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletResponse;
+import java.time.Duration;
+import java.time.LocalTime;
+
+
+@Component
+public class ViewedStudyLogCookieGenerator {
+    private static final String VIEWED_STUDY_LOG_COOKIE_NAME = "viewed";
+
+    public ResponseCookie generateCookie(String name, String value) {
+        Duration duration = Duration.between(LocalTime.now(), LocalTime.MAX);
+        return ResponseCookie.from(name, value)
+                .httpOnly(true)
+                .sameSite("None")
+                .secure(true)
+                .maxAge(duration.getSeconds())
+                .build();
+    }
+
+    public boolean isViewed(String studyLogIds, String studyLogId) {
+        return studyLogIds.contains("/" + studyLogId + "/");
+    }
+
+    public void setViewedStudyLogCookie(String studyLogIds, String studyLogId, HttpServletResponse response) {
+        if (isViewed(studyLogIds, studyLogId)) {
+            return;
+        }
+
+        String cookieValue = studyLogIds.concat("/" + studyLogId);
+        response.setHeader(HttpHeaders.SET_COOKIE, generateCookie(VIEWED_STUDY_LOG_COOKIE_NAME, cookieValue).toString());
+    }
+}
