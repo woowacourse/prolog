d47f2a4d9b24505f992b96f2f58be95b17d2d307
wilgur513 <wilgur513@naver.com>
2022-05-11 16:01:41 +0900

    test: @DisplayName 적용 및 Member Id 하드 코딩

--- backend/src/test/java/wooteco/prolog/report/application/ReportServiceTest.java
+++ backend/src/test/java/wooteco/prolog/report/application/ReportServiceTest.java
@@ -5,16 +5,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import wooteco.prolog.common.PageableResponse;
-import wooteco.prolog.member.domain.Member;
-import wooteco.prolog.member.domain.Role;
-import wooteco.prolog.member.domain.repository.MemberRepository;
 import wooteco.prolog.report.application.dto.ReportResponse;
 import wooteco.prolog.report.domain.Report;
 import wooteco.prolog.report.domain.repository.ReportRepository;
@@ -29,23 +26,14 @@ class ReportServiceTest {
     @Autowired
     private ReportRepository reportRepository;
 
-    @Autowired
-    private MemberRepository memberRepository;
-
-    private Member member;
-
-    @BeforeEach
-    void setUp() {
-        member = memberRepository.save(new Member("brown", "브라운", Role.CREW, 1L, "imageUrl"));
-    }
-
+    @DisplayName("정렬된 리포트 목록 조회")
     @Test
-    void findAllReports() {
-        Report report1 = new Report("title1", "desc1", member.getId(), LocalDate.of(2022, 3, 5),
+    void findAllSortedReports() {
+        Report report1 = new Report("title1", "desc1", 1L, LocalDate.of(2022, 3, 5),
             LocalDate.of(2022, 3, 6));
-        Report report2 = new Report("title2", "desc2", member.getId(), LocalDate.of(2022, 3, 6),
+        Report report2 = new Report("title2", "desc2", 1L, LocalDate.of(2022, 3, 6),
             LocalDate.of(2022, 3, 7));
-        Report report3 = new Report("title3", "desc3", member.getId(), LocalDate.of(2022, 4, 5),
+        Report report3 = new Report("title3", "desc3", 1L, LocalDate.of(2022, 4, 5),
             LocalDate.of(2022, 4, 10));
 
         reportRepository.save(report1);
