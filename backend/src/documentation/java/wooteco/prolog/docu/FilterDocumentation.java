c26350a013b4ca79672f603cda2df342187538a5
Rookie (JiHoon Choi) <48710213+Wishoon@users.noreply.github.com>
2022-05-23 23:55:56 +0900

    feat: Member nickname을 기준으로 오름차순 정렬 기능 추가 (#872)
    * feat: Member nickname을 기준으로 오름차순 정렬 기능 추가
    
    * refactor: findAll() -> findAllOrderByNickNameAsc() 로 수정
    
    * refactor: findAll() -> findAllOrderByNickNameAsc() 로 수정
    * test: 인수 테스트, 문서 테스트 작성
    
    Co-authored-by: 류성현 <brown@woowahan.com>

--- backend/src/documentation/java/wooteco/prolog/docu/FilterDocumentation.java
+++ backend/src/documentation/java/wooteco/prolog/docu/FilterDocumentation.java
@@ -27,7 +27,7 @@ public class FilterDocumentation extends NewDocumentation {
 
     @Test
     void 필터_목록_조회() {
-        when(filterService.showAll(any())).thenReturn(FILTER_RESPONSE);
+        when(filterService.showAll(any())).thenReturn(FILTER_RESPONSE_OF_NICKNAME_ASC_SORT);
 
         given
             .when().get("/filters")
@@ -39,5 +39,15 @@ public class FilterDocumentation extends NewDocumentation {
         Lists.newArrayList(new MissionResponse(1L, "지하철 노선도 미션 1", new SessionResponse(1L, "세션1")), new MissionResponse(2L, "지하철 노선도 미션 2", new SessionResponse(2L, "세션2"))),
         Lists.newArrayList(new TagResponse(1L, "자바"), new TagResponse(2L, "파이썬"), new TagResponse(3L, "자바스크립트")),
         Lists.newArrayList(new MemberResponse(1L, "username", "nickname", Role.CREW, "imageUrl"))
+    );
+
+    private static final FilterResponse FILTER_RESPONSE_OF_NICKNAME_ASC_SORT = new FilterResponse(
+            Lists.newArrayList(new SessionResponse(1L, "세션1"), new SessionResponse(2L, "세션2")),
+            Lists.newArrayList(new MissionResponse(1L, "지하철 노선도 미션 1", new SessionResponse(1L, "세션1")), new MissionResponse(2L, "지하철 노선도 미션 2", new SessionResponse(2L, "세션2"))),
+            Lists.newArrayList(new TagResponse(1L, "자바"), new TagResponse(2L, "파이썬"), new TagResponse(3L, "자바스크립트")),
+            Lists.newArrayList(
+                    new MemberResponse(1L, "username", "브라운", Role.CREW, "imageUrl"),
+                    new MemberResponse(2L, "username", "서니", Role.CREW, "imageUrl"),
+                    new MemberResponse(3L, "username", "현구막", Role.CREW, "imageUrl"))
     );
 }
