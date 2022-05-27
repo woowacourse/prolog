b450d2bf3e2836488f9edeb13391f412aa7adf88
gytks4 <gytks4@naver.com>
2022-05-27 21:13:58 +0900

    refactor: COLOR TS로 마이그레이션

--- frontend/src/components/Banner/Banner.styles.ts
+++ frontend/src/components/Banner/Banner.styles.ts
@@ -93,7 +93,7 @@ export const bannerTextAreaStyle = css`
 `;
 
 export const getBannerThemeByBgColor = (
-  backgroundColor = COLOR.WHITE,
+  backgroundColor: string = COLOR.WHITE,
   backgroundImage?: string
 ) => css`
   background-color: ${backgroundColor};
