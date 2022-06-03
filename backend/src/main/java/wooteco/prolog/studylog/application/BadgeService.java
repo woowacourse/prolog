c48605223c3e3d0f32c795308faa626a19cf7127
her0807 <her0807@naver.com>
2022-06-03 20:04:05 +0900

    refactor: 배지 관련 메서드 추출

--- backend/src/main/java/wooteco/prolog/studylog/application/BadgeService.java
+++ backend/src/main/java/wooteco/prolog/studylog/application/BadgeService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import wooteco.prolog.member.application.MemberService;
+import wooteco.prolog.member.domain.Member;
 import wooteco.prolog.studylog.domain.Badge;
 import wooteco.prolog.studylog.domain.repository.BadgeRepository;
 
@@ -16,21 +17,37 @@ public class BadgeService {
     private final BadgeRepository badgeRepository;
 
     public List<Badge> findBadges(String username, List<Long> sessions) {
-        memberService.findByUsername(username);
+        Member member = memberService.findByUsername(username);
 
+        int studylogCount = badgeRepository.countStudylogByUsernameDuringSessions(
+            member.getUsername(),
+            sessions);
+
+        int likeCount = badgeRepository.countLikesByUsernameDuringSessions(
+            member.getUsername(), sessions);
+
+        return createBadges(studylogCount, likeCount);
+    }
+
+    private List<Badge> createBadges(int studylogCount, int likeCount) {
         List<Badge> badges = new ArrayList<>();
-        int count = badgeRepository.countStudylogByUsernameDuringSessions(username, sessions);
 
-        if (count >= 7) {
+        if (isPassionKing(studylogCount)) {
             badges.add(new Badge("열정왕"));
         }
 
-        count = badgeRepository.countLikesByUsernameDuringSessions(username, sessions);
-
-        if (count >= 15) {
+        if (isComplimentKing(likeCount)) {
             badges.add(new Badge("칭찬왕"));
         }
 
         return badges;
+    }
+
+    private boolean isPassionKing(int studylogCount) {
+        return studylogCount >= 7;
+    }
+
+    private boolean isComplimentKing(int likeCount) {
+        return likeCount >= 15;
     }
 }
