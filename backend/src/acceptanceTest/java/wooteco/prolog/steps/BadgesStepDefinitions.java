2da8ed2d7718963cc62ea966ab98058b7714f7b2
이한울 <dkfghg3@naver.com>
2022-06-03 18:18:31 +0900

    test: 열정왕 관련 테스트 작성

---
+++ backend/src/acceptanceTest/java/wooteco/prolog/steps/BadgesStepDefinitions.java
@@ -0,0 +1,52 @@
+package wooteco.prolog.steps;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static wooteco.prolog.fixtures.StudylogAcceptanceFixture.STUDYLOG8;
+import static wooteco.prolog.fixtures.StudylogAcceptanceFixture.STUDYLOG9;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import wooteco.prolog.AcceptanceSteps;
+import wooteco.prolog.common.exception.ExceptionDto;
+import wooteco.prolog.studylog.application.dto.BadgeResponse;
+
+public class BadgesStepDefinitions extends AcceptanceSteps {
+
+  @When("존재하지 않는 멤버의 배지를 조회하면")
+  public void 존재하지않는멤버의배지를조회하면() {
+    String username = "notFound";
+    context.invokeHttpGet("/members/" + username + "/badges");
+  }
+
+  @Then("존재하지 않는 멤버 관련 예외가 발생한다")
+  public void 존재하지않는멤버관련예외가발생한다() {
+    assertThat(context.response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    assertThat(context.response.as(ExceptionDto.class).getCode()).isEqualTo(1004);
+  }
+
+  @Given("여러개의 스터디로그를 작성하고")
+  public void 여러개의스터디로그를작성하고() {
+    for (int i = 0; i < 5; i++) {
+      context.invokeHttpPostWithToken("/studylogs", STUDYLOG8.getStudylogRequest());
+    }
+    for (int i = 0; i < 2; i++) {
+      context.invokeHttpPostWithToken("/studylogs", STUDYLOG9.getStudylogRequest());
+    }
+  }
+
+  @When("배지를 조회하면")
+  public void 배지를조회하면() {
+    String username = String.valueOf(context.storage.get("username"));
+    context.invokeHttpGet("/members/" + username + "/badges");
+  }
+
+  @Then("열정왕 배지를 부여한다.")
+  public void 열정왕배지를부여한다() {
+    List<BadgeResponse> badges = context.response.jsonPath().getList("badges", BadgeResponse.class);
+    assertThat(context.response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    assertThat(badges).containsExactly(new BadgeResponse("url", "열정왕"));
+  }
+}
