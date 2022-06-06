6f0431621795152a7368b45a1a740579e8328443
her0807 <her0807@naver.com>
2022-06-06 17:04:02 +0900

    refactor: sql 에서 불필요한 Join 제거

--- backend/src/main/java/wooteco/prolog/studylog/domain/repository/BadgeRepository.java
+++ backend/src/main/java/wooteco/prolog/studylog/domain/repository/BadgeRepository.java
@@ -12,46 +12,42 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class BadgeRepository {
 
-  private static final ResultSetExtractor<Integer> COUNT_RESULT_SET_EXTRACTOR = rs -> {
-    if (rs.next()) {
-      return rs.getInt(1);
+    private static final ResultSetExtractor<Integer> COUNT_RESULT_SET_EXTRACTOR = rs -> {
+        if (rs.next()) {
+            return rs.getInt(1);
+        }
+        return 0;
+    };
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    public BadgeRepository(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
     }
-    return 0;
-  };
-  private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
-  public BadgeRepository(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
-    this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
-  }
+    public int countLikesByUserIdDuringSessions(Long userId, List<Long> sessions) {
+        String sql = "select count(st.id)\n"
+            + "from studylog as st \n"
+            + "Join likes as l on l.studylog_id = st.id\n"
+            + "where st.session_id in (:sessions) and st.member_id = :userId";
 
-  public int countStudylogByUsernameDuringSessions(String username, List<Long> sessions) {
-    String sql = "select count(st.id)\n"
-        + "from studylog as st \n"
-        + "Join session as s on st.session_id = s.id\n"
-        + "Join member as mem on st.member_id = mem.id\n"
-        + "where s.id in (:sessions) and mem.username = :username and st.deleted = false";
+        Map<String, Object> map = new HashMap<>();
+        map.put("sessions", sessions);
+        map.put("userId", userId);
+        SqlParameterSource parameters = new MapSqlParameterSource(map);
 
-    Map<String, Object> map = new HashMap<>();
-    map.put("sessions", sessions);
-    map.put("username", username);
-    SqlParameterSource parameters = new MapSqlParameterSource(map);
+        return namedParameterJdbcTemplate.query(sql, parameters, COUNT_RESULT_SET_EXTRACTOR);
+    }
 
-    return namedParameterJdbcTemplate.query(sql, parameters, COUNT_RESULT_SET_EXTRACTOR);
-  }
+    public int countStudylogByUserIdDuringSessions(Long userId, List<Long> sessions) {
+        String sql = "select count(st.id)\n"
+            + "from studylog as st \n"
+            + "where st.session_id in (:sessions) and st.member_id = :userId and st.deleted = false";
 
-  public int countLikesByUsernameDuringSessions(String username, List<Long> sessions) {
-    String sql = "select count(st.id)\n"
-        + "from studylog as st \n"
-        + "Join session as s on st.session_id = s.id\n"
-        + "Join likes as l on l.studylog_id = st.id\n"
-        + "Join member as mem on l.member_id = mem.id\n"
-        + "where s.id in (:sessions) and mem.username = :username";
+        Map<String, Object> map = new HashMap<>();
+        map.put("sessions", sessions);
+        map.put("userId", userId);
+        SqlParameterSource parameters = new MapSqlParameterSource(map);
 
-    Map<String, Object> map = new HashMap<>();
-    map.put("sessions", sessions);
-    map.put("username", username);
-    SqlParameterSource parameters = new MapSqlParameterSource(map);
-
-    return namedParameterJdbcTemplate.query(sql, parameters, COUNT_RESULT_SET_EXTRACTOR);
-  }
+        return namedParameterJdbcTemplate.query(sql, parameters, COUNT_RESULT_SET_EXTRACTOR);
+    }
 }
