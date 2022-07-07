9db1f93f13cc9a66ab3ae87dd320050c60c62f71
kwannee <yleh1126@naver.com>
2022-07-07 19:38:22 +0900

    rebase: filtering 커밋 제거

--- frontend/src/pages/MainPage/PopularStudyLogList.tsx
+++ frontend/src/pages/MainPage/PopularStudyLogList.tsx
@@ -16,11 +16,21 @@ const PopularStudyLogList = ({ studylogs }: { studylogs: Studylog[] }): JSX.Elem
     >
       <h2 css={[SectionHeaderGapStyle]}>😎 인기있는 학습로그</h2>
       <ul css={[PopularStudylogListStyle]}>
+<<<<<<< HEAD
         {studylogs?.map((item: Studylog) => (
           <li key={item.id}>
             <PopularStudylogItem item={item} />
           </li>
         ))}
+=======
+        {studylogs[getKeyByValue(studyLogCategory, selectedCategory) as Category].data.map(
+          ({ studylogResponse, scrapedCount }) => (
+            <li key={studylogResponse.id}>
+              <PopularStudylogItem item={{ ...studylogResponse, scrapedCount }} />
+            </li>
+          )
+        )}
+>>>>>>> 950ce71 (fix: 인기있는 학습로그 api 명세 변경에 따른 수정)
       </ul>
     </section>
   );
