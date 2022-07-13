cda67de855cbf942eda78946a2ad18ce191e7993
hand <sns2831@gmail.com>
2022-07-13 17:23:34 +0900

    chore: batch enable 설정 추가.

--- batch/src/main/java/kr/co/techcourse/prolog/batch/BatchApplication.java
+++ batch/src/main/java/kr/co/techcourse/prolog/batch/BatchApplication.java
@@ -1,8 +1,10 @@
 package kr.co.techcourse.prolog.batch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableBatchProcessing
 @SpringBootApplication
 public class BatchApplication {
 
