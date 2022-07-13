3aed9801966d58820c51b3e369c9d9b2cad8bdb8
hand <sns2831@gmail.com>
2022-07-13 17:07:34 +0900

    chore: batch init

---
+++ batch/src/main/java/kr/co/techcourse/prolog/batch/BatchApplication.java
@@ -0,0 +1,13 @@
+package kr.co.techcourse.prolog.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchApplication.class, args);
+	}
+
+}
---
+++ batch/src/test/java/kr/co/techcourse/prolog/batch/BatchApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.techcourse.prolog.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
