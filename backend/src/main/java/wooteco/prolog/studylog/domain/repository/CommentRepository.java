d5e7b76ba9f7730c1f4346ce4d3394673938bcab
Wishoon <heonga2@gmail.com>
2022-07-23 15:05:48 +0900

    feat: 단일 스터디로그에 대한 댓글 리스트 조회 Repository 기능 구현

--- backend/src/main/java/wooteco/prolog/studylog/domain/repository/CommentRepository.java
+++ backend/src/main/java/wooteco/prolog/studylog/domain/repository/CommentRepository.java
@@ -1,8 +1,16 @@
 package wooteco.prolog.studylog.domain.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import wooteco.prolog.studylog.domain.Comment;
+import wooteco.prolog.studylog.domain.Studylog;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
+    @Query("SELECT c FROM Comment c"
+        + " JOIN FETCH c.studylog s"
+        + " JOIN FETCH c.member m"
+        + " WHERE s = :studylog")
+    List<Comment> findCommentByStudylog(Studylog studylog);
 }
