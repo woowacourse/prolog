3a83a601114bcdf309a3811dd992f6f07af64555
Wishoon <heonga2@gmail.com>
2022-07-23 14:25:48 +0900

    test: 댓글 등록 문서 테스트 작성

---
+++ backend/src/documentation/java/wooteco/prolog/docu/CommentDocumentation.java
@@ -0,0 +1,87 @@
+package wooteco.prolog.docu;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import wooteco.prolog.Documentation;
+import wooteco.prolog.session.application.dto.MissionRequest;
+import wooteco.prolog.session.application.dto.MissionResponse;
+import wooteco.prolog.session.application.dto.SessionRequest;
+import wooteco.prolog.session.application.dto.SessionResponse;
+import wooteco.prolog.studylog.application.dto.CommentCreateRequest;
+import wooteco.prolog.studylog.application.dto.StudylogRequest;
+import wooteco.prolog.studylog.application.dto.TagRequest;
+
+public class CommentDocumentation extends Documentation {
+
+    @Test
+    void 댓글을_등록한다() {
+        // given
+        Long studylogId = 스터디로그_등록함(createStudylogRequest());
+
+        // when
+        ExtractableResponse<Response> extract = given("comment/create")
+            .header("Authorization", "Bearer " + 로그인_사용자.getAccessToken())
+            .body(createCommentRequest())
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when().post("/studylogs/" + studylogId + "/comments")
+            .then().log().all().extract();
+
+        // then
+        assertThat(extract.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(extract.header("Location")).isNotNull();
+    }
+
+    private StudylogRequest createStudylogRequest() {
+        String title = "스터디로그 제목";
+        String content = "스터디로그에 본문 내용임.\n" + "여기에 글을 작성할 수 있음\n";
+        Long sessionId = 세션_등록함(new SessionRequest("백엔드Java 레벨1 - 2022"));
+        Long missionId = 미션_등록함(new MissionRequest("[BE] 레벨1 - 미션이름", sessionId));
+        List<TagRequest> tags = Arrays.asList(new TagRequest("tag"));
+
+        return new StudylogRequest(title, content, sessionId, missionId, tags);
+    }
+
+    private CommentCreateRequest createCommentRequest() {
+        return new CommentCreateRequest("댓글의 내용입니다.");
+    }
+
+    private Long 스터디로그_등록함(StudylogRequest request) {
+        ExtractableResponse<Response> extract = RestAssured.given().log().all()
+            .header("Authorization", "Bearer " + 로그인_사용자.getAccessToken())
+            .body(request)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when().log().all()
+            .post("/studylogs")
+            .then().log().all().extract();
+
+        return Long.parseLong(extract.header("Location").split("/studylogs/")[1]);
+    }
+
+    private Long 세션_등록함(SessionRequest request) {
+        return RestAssured.given().log().all()
+            .body(request)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .post("/sessions")
+            .then().log().all()
+            .extract().as(SessionResponse.class).getId();
+    }
+
+    private Long 미션_등록함(MissionRequest request) {
+        return RestAssured.given()
+            .body(request)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .post("/missions")
+            .then().log().all()
+            .extract().as(MissionResponse.class).getId();
+    }
+}
