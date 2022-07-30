638cf4ff566202681ac05abaa65320a146f341a0
이한울 <dkfghg3@naver.com>
2022-07-30 15:58:18 +0900

    fix: version update 에 따른 @Param 추가

--- backend/src/main/java/wooteco/prolog/update/UpdatedContentsRepository.java
+++ backend/src/main/java/wooteco/prolog/update/UpdatedContentsRepository.java
@@ -3,9 +3,10 @@ package wooteco.prolog.update;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UpdatedContentsRepository extends JpaRepository<UpdatedContents, Long> {
 
     @Query("select uc from UpdatedContents uc where uc.updateContent = :content")
-    Optional<UpdatedContents> findByContent(UpdateContent content);
+    Optional<UpdatedContents> findByContent(@Param("content") UpdateContent content);
 }
