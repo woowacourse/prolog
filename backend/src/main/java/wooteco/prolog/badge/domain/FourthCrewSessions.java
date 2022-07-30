c78e62f50590bce7d6274bf230bd66d6f445a588
이한울 <dkfghg3@naver.com>
2022-07-30 18:48:55 +0900

    feat: 세션 예외 추가

--- backend/src/main/java/wooteco/prolog/badge/domain/FourthCrewSessions.java
+++ backend/src/main/java/wooteco/prolog/badge/domain/FourthCrewSessions.java
@@ -2,6 +2,7 @@ package wooteco.prolog.badge.domain;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public enum FourthCrewSessions {
 
@@ -11,7 +12,18 @@ public enum FourthCrewSessions {
     private final List<Long> sessions;
 
     FourthCrewSessions(Long... sessions) {
+        validateSessionCount(sessions);
         this.sessions = Arrays.asList(sessions);
+    }
+
+    private void validateSessionCount(Long[] sessions) {
+        if (Objects.isNull(sessions) || isValidLength(sessions)) {
+            throw new IllegalArgumentException("세션은 최소 1이상 존재해야 합니다.");
+        }
+    }
+
+    private boolean isValidLength(Long[] sessions) {
+        return sessions.length < 1;
     }
 
     public List<Long> getSessions() {
