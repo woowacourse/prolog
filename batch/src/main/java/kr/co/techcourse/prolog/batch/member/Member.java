ee650addcf3347698aeeb3b3da9b5c3e32b77ce9
정진혁 <wilgur513@naver.com>
2022-08-04 17:56:16 +0900

    미션 완료

---
+++ batch/src/main/java/kr/co/techcourse/prolog/batch/member/Member.java
@@ -0,0 +1,31 @@
+package kr.co.techcourse.prolog.batch.member;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Member() {
+    }
+
+    public Member(final String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
