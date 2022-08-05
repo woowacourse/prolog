6646d75d87566b65fd7021538d803441ff0c810e
hand <sns2831@gmail.com>
2022-08-05 15:51:54 +0900

    refactor : 베루스 job ConditionalOnProperty 추가.

--- batch/src/main/java/kr/co/techcourse/prolog/batch/job/sample/tasklet/verus/SampleBatchJob.java
+++ batch/src/main/java/kr/co/techcourse/prolog/batch/job/sample/tasklet/verus/SampleBatchJob.java
@@ -8,9 +8,11 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@ConditionalOnProperty(name = "spring.batch.job.names", havingValue = "verusJob")
 @Configuration
 public class SampleBatchJob {
 
