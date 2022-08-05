a97fd9194032a03135ed07e9f61bdcb15e08f997
2yujeong <76840965+2yujeong@users.noreply.github.com>
2022-08-05 15:19:47 +0900

    feat: sample batch job 과제 구현 (#962)
    * feat: batch job 과제 구현
    
    * refactor : eve batch 패키지 이동.
    
    Co-authored-by: hand <sns2831@gmail.com>

---
+++ batch/src/main/java/kr/co/techcourse/prolog/batch/job/eve/MemberBatchJob.java
@@ -0,0 +1,62 @@
+package kr.co.techcourse.prolog.batch.job.eve;
+
+import kr.co.techcourse.prolog.batch.job.eve.domain.Member;
+import kr.co.techcourse.prolog.batch.job.eve.domain.MemberRepository;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnProperty(name = "spring.batch.job.names", havingValue = "eveJob")
+public class MemberBatchJob {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final MemberRepository memberRepository;
+    private final StepDataShareBean<String> stepDataShareBean;
+    private int count = 0;
+
+    public MemberBatchJob(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory,
+                          MemberRepository memberRepository, StepDataShareBean<String> stepDataShareBean) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+        this.memberRepository = memberRepository;
+        this.stepDataShareBean = stepDataShareBean;
+    }
+
+    @Bean
+    public Job sampleJob() {
+        return jobBuilderFactory.get("eveJob")
+                .start(eve1())
+                .next(eve2())
+                .build();
+    }
+
+    @Bean
+    public Step eve1() {
+        return stepBuilderFactory.get("eve1")
+                .tasklet((contribution, chunkContext) -> {
+                    String nickname = "eve";
+                    stepDataShareBean.put("nickname", nickname);
+                    memberRepository.save(new Member(nickname));
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step eve2() {
+        return stepBuilderFactory.get("eve2")
+                .tasklet((contribution, chunkContext) -> {
+                    Member member = memberRepository.findByNickname(stepDataShareBean.get("nickname"));
+                    System.out.println(member.getNickname());
+                    return RepeatStatus.continueIf(++count < 10);
+                })
+                .build();
+    }
+}
---
+++ batch/src/main/java/kr/co/techcourse/prolog/batch/job/eve/domain/MemberRepository.java
@@ -0,0 +1,8 @@
+package kr.co.techcourse.prolog.batch.job.eve.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Member findByNickname(String nickname);
+}
