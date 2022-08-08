578e26abc3e207d814151e92160844050dbe1a62
Wishoon <heonga2@gmail.com>
2022-08-08 10:38:04 +0900

    test: 인기있는 학습로그의 인수테스트 수정

--- backend/src/acceptanceTest/java/wooteco/prolog/steps/StudylogStepDefinitions.java
+++ backend/src/acceptanceTest/java/wooteco/prolog/steps/StudylogStepDefinitions.java
@@ -23,6 +23,7 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import wooteco.prolog.AcceptanceSteps;
 import wooteco.prolog.fixtures.StudylogAcceptanceFixture;
+import wooteco.prolog.studylog.application.dto.PopularStudylogsResponse;
 import wooteco.prolog.studylog.application.dto.StudylogMissionRequest;
 import wooteco.prolog.studylog.application.dto.StudylogRequest;
 import wooteco.prolog.studylog.application.dto.StudylogResponse;
@@ -397,11 +398,11 @@ public class StudylogStepDefinitions extends AcceptanceSteps {
     public void 스터디로그가Id순서로조회된다(String studylogIds) {
         context.invokeHttpGet("/studylogs/popular");
         assertThat(context.response.statusCode()).isEqualTo(HttpStatus.OK.value());
-        StudylogsResponse studylogsResponse = context.response.as(StudylogsResponse.class);
+        PopularStudylogsResponse studylogsResponse = context.response.as(PopularStudylogsResponse.class);
 
         List<String> ids = Arrays.asList(studylogIds.split(", "));
         for (int i = 0; i < ids.size(); i++) {
-            StudylogResponse response = studylogsResponse.getData().get(i);
+            StudylogResponse response = studylogsResponse.getAllResponse().getData().get(i);
             Long id = Long.parseLong(ids.get(i));
             assertThat(response.getId()).isEqualTo(id);
         }
