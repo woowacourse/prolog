0ea8c9cbc0e6d23c37557b75d7f70caeb70cb1ae
2yujeong <yjfflq4872@gmail.com>
2022-08-10 11:27:12 +0900

    refactor: Bean name 설정 추가

--- batch/src/main/java/kr/co/techcourse/prolog/batch/job/sample/tasklet/east/repository/MemberRepository.java
+++ batch/src/main/java/kr/co/techcourse/prolog/batch/job/sample/tasklet/east/repository/MemberRepository.java
@@ -2,6 +2,8 @@ package kr.co.techcourse.prolog.batch.job.sample.tasklet.east.repository;
 
 import kr.co.techcourse.prolog.batch.job.sample.tasklet.east.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository("east.MemberRepository")
 public interface MemberRepository extends JpaRepository<Member, Long> {
 }
--- batch/src/main/java/kr/co/techcourse/prolog/batch/job/sample/tasklet/eve/domain/MemberRepository.java
+++ batch/src/main/java/kr/co/techcourse/prolog/batch/job/sample/tasklet/eve/domain/MemberRepository.java
@@ -1,7 +1,9 @@
 package kr.co.techcourse.prolog.batch.job.sample.tasklet.eve.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository("eve.MemberRepository")
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Member findByNickname(String nickname);
