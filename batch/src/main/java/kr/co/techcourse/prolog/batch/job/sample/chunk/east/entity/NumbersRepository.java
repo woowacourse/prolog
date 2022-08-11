e8833f24ea58fdeb98af02562c0958944348212f
Donghoon Shin <64204666+EastHShin@users.noreply.github.com>
2022-08-11 17:08:09 +0900

    feat: sample batch chunk job 과제 구현 (#972)

---
+++ batch/src/main/java/kr/co/techcourse/prolog/batch/job/sample/chunk/east/entity/NumbersRepository.java
@@ -0,0 +1,6 @@
+package kr.co.techcourse.prolog.batch.job.sample.chunk.east.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NumbersRepository extends JpaRepository<Numbers, Long> {
+}
