f9ba3bebca5cfa99348d94af42b605ebfeff02b8
gracefulBrown <brown@woowahan.com>
2022-08-13 14:32:52 +0900

    feat: add count header

---
+++ backend/src/main/java/wooteco/prolog/common/ResourceSizeAdvice.java
@@ -0,0 +1,29 @@
+package wooteco.prolog.common;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import java.util.Collection;
+
+@ControllerAdvice
+public class ResourceSizeAdvice implements ResponseBodyAdvice<Object> {
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return ResponseEntity.class.isAssignableFrom(returnType.getParameterType());
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        if (body instanceof Collection) {
+            response.getHeaders().add("X-Total-Count", String.valueOf(((Collection<?>) body).size()));
+        }
+        return body;
+    }
+}
