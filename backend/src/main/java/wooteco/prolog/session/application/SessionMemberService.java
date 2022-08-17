974829892b2e84abbcb028d710f3123623781609
Hyeon9mak <jinha3507@gmail.com>
2022-08-17 23:24:08 +0900

    feat: 강의에서 자신을 제거하는 API 구현

--- backend/src/main/java/wooteco/prolog/session/application/SessionMemberService.java
+++ backend/src/main/java/wooteco/prolog/session/application/SessionMemberService.java
@@ -74,4 +74,20 @@ public class SessionMemberService {
         Member member = memberService.findById(memberId);
         return sessionMemberRepository.findByMember(member);
     }
+
+    @Transactional
+    public void deleteRegistedSession(Long sessionId, Long memberId) {
+        Member member = memberService.findById(memberId);
+        SessionMember sessionMember = findSessionMemberBySessionIdAndMemberId(
+            sessionId,
+            member
+        );
+
+        sessionMemberRepository.delete(sessionMember);
+    }
+
+    private SessionMember findSessionMemberBySessionIdAndMemberId(Long sessionId, Member member) {
+        return sessionMemberRepository.findBySessionIdAndMember(sessionId, member)
+            .orElseThrow(SessionNotFoundException::new);
+    }
 }
