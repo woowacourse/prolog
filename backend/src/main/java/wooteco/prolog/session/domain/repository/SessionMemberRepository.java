e840cc7c3e08ccf8bbd6b640bdff2a3e5bd9b28c
Hyeon9mak <jinha3507@gmail.com>
2022-08-20 22:09:26 +0900

    feat: 수강중인 강의를 제거하는(강의에서 나를 제거하는) API (#990)
    * hotfix: 학습로그 렌더링 되지 않는 현상 해결 (#941)
    
    * fix: 게시글 렌더링 되지 않는 현상 해결
    
    * feat: 게시글에 Scrap 수 추가
    
    * refactor: SessionMember 조회 메서드 수정
    
    MemberId를 통한 조회에서 Member 엔티티를 통한 조회로 변경
    
    * feat: Session ID와 Member 엔티티를 통해 SessionMember 엔티티 조회하는 로직 추가
    
    * feat: 강의에서 자신을 제거하는 API 구현
    
    Co-authored-by: 김의진 <gytks4@naver.com>

--- backend/src/main/java/wooteco/prolog/session/domain/repository/SessionMemberRepository.java
+++ backend/src/main/java/wooteco/prolog/session/domain/repository/SessionMemberRepository.java
@@ -1,12 +1,16 @@
 package wooteco.prolog.session.domain.repository;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import wooteco.prolog.member.domain.Member;
 import wooteco.prolog.session.domain.SessionMember;
 
 public interface SessionMemberRepository extends JpaRepository<SessionMember, Long> {
 
     List<SessionMember> findAllBySessionId(Long sessionId);
 
-    List<SessionMember> findByMemberId(Long memberId);
+    List<SessionMember> findByMember(Member member);
+
+    Optional<SessionMember> findBySessionIdAndMember(Long sessionId, Member member);
 }
