e48473c8d7d071eef381de03ee01a28b8389c627
gracefulBrown <brown@woowahan.com>
2022-08-21 20:17:37 +0900

    fix(sql)

--- backend/src/main/resources/db/migration/prod/V26__create_comment_table.sql
+++ backend/src/main/resources/db/migration/prod/V26__create_comment_table.sql
@@ -4,15 +4,14 @@ CREATE TABLE comment
     member_id   BIGINT NOT NULL,
     studylog_id BIGINT NOT NULL,
     content     TEXT(255) NOT NULL,
-    isDelete    TINYINT(1) DEFAULT 0 NOT NULL
+    is_delete   TINYINT(1) DEFAULT 0 NOT NULL
     PRIMARY KEY (id)
 ) ENGINE=InnoDB;
 
-
 ALTER TABLE member
-    ADD CONSTRAINT FK_comment_MEMBER
+    ADD CONSTRAINT FK_COMMENT_MEMBER
         FOREIGN KEY (member_id) REFERENCES member (id);
 
 ALTER TABLE studylog
-    ADD CONSTRAINT FK_comment_studylog
+    ADD CONSTRAINT FK_COMMENT_STUDYLOG
         FOREIGN KEY (studylog_id) REFERENCES studylog (id);
