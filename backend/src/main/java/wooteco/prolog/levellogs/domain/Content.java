85a11aa8496e2e5150230f8286a572dbc780304a
her0807 <her0807@naver.com>
2022-08-21 13:58:19 +0900

    refactor: trim 검증 순서 변경

--- backend/src/main/java/wooteco/prolog/levellogs/domain/Content.java
+++ backend/src/main/java/wooteco/prolog/levellogs/domain/Content.java
@@ -1,8 +1,8 @@
 package wooteco.prolog.levellogs.domain;
 
-import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -12,29 +12,28 @@ import wooteco.prolog.studylog.exception.StudylogContentNullOrEmptyException;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@EqualsAndHashCode
 @ToString
 @Embeddable
 public class Content {
 
-    @Column(nullable = false, columnDefinition = "text")
-    private String content;
+	@Column(nullable = false, columnDefinition = "text")
+	private String content;
 
-    public Content(String content) {
-        validateNullOrEmpty(content, length(content));
-        this.content = content;
-    }
+	public Content(String content) {
+		validateNullOrEmpty(content, length(content));
+		this.content = content;
+	}
 
-    private int length(String title) {
-        if (Objects.nonNull(title)) {
-            return title.trim().length();
-        }
-        return 0;
-    }
+	private int length(String title) {
+		if (title != null) {
+			return title.trim().length();
+		}
+		return 0;
+	}
 
-    private void validateNullOrEmpty(String content, int trimedContentLength) {
-        if (Objects.isNull(content) || trimedContentLength == 0) {
-            throw new StudylogContentNullOrEmptyException();
-        }
-    }
+	private void validateNullOrEmpty(String content, int trimedContentLength) {
+		if ((content == null) || trimedContentLength == 0) {
+			throw new StudylogContentNullOrEmptyException();
+		}
+	}
 }
