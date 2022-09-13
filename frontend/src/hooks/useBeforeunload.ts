0109c50e0e0eb2cfa5555c511a62b02138a5fae5
gytks4 <gytks4@naver.com>
2022-09-13 09:14:20 +0900

    refactor: useBeforeunload 타입 수정

--- frontend/src/hooks/useBeforeunload.ts
+++ frontend/src/hooks/useBeforeunload.ts
@@ -1,7 +1,7 @@
 import { Editor as ToastEditor } from '@toast-ui/react-editor';
-import { useEffect, MutableRefObject } from 'react';
+import { useEffect, RefObject } from 'react';
 
-const useBeforeunload = (editorContentRef: MutableRefObject<ToastEditor>) => {
+const useBeforeunload = (editorContentRef: RefObject<ToastEditor>) => {
   useEffect(() => {
     window.addEventListener('beforeunload', (event) => {
       const content = editorContentRef.current?.getInstance().getMarkdown() || '';
