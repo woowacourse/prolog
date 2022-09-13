2ff2569ab097bb0543395c2696f9cdabdfb4b0ce
gytks4 <gytks4@naver.com>
2022-09-13 09:15:54 +0900

    feat: onSubmit 로직 useLevellogComment에 추가

--- frontend/src/hooks/Levellog/useLevellogComment.ts
+++ frontend/src/hooks/Levellog/useLevellogComment.ts
@@ -1,3 +1,6 @@
+import { useRef } from 'react';
+import { Editor as ToastEditor } from '@toast-ui/react-editor';
+
 import { CommentRequest } from '../../models/Comment';
 import {
   useCreateLevellogComment,
@@ -5,8 +8,10 @@ import {
   useEditLevellogCommentMutation,
   useFetchLevellogComments,
 } from '../queries/levellogComment';
+import { ALERT_MESSAGE } from '../../constants';
 
 const useLevellogComment = (levellogId: number) => {
+  const editorContentRef = useRef<ToastEditor>(null);
   const { data } = useFetchLevellogComments(levellogId);
   const levellogComments = data?.data;
 
@@ -26,11 +31,27 @@ const useLevellogComment = (levellogId: number) => {
     deleteLevellogCommentMutation.mutate(commentId);
   };
 
+  const onSubmitLevellogComment = (event) => {
+    event.preventDefault();
+
+    const content = editorContentRef.current?.getInstance().getMarkdown() || '';
+
+    if (content.length === 0) {
+      alert(ALERT_MESSAGE.NO_CONTENT);
+      return;
+    }
+
+    createLevellogComment({ content });
+    editorContentRef.current?.getInstance().setMarkdown('');
+  };
+
   return {
     levellogComments,
+    editorContentRef,
     createLevellogComment,
     editLevellogComment,
     deleteLevellogComment,
+    onSubmitLevellogComment,
   };
 };
 
