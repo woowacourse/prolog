ff7a9b935996708d621d0ac1e24cfd60f56d03bf
JUDONGHYEOK <judonghyeok@gmail.com>
2022-09-24 12:30:20 +0900

    feat:Image 업로드기능 추가

---
+++ frontend/src/hooks/useImage.ts
@@ -0,0 +1,30 @@
+import { requestImageUpload } from '../service/requests';
+import useSnackBar from './useSnackBar';
+
+const useImage = () => {
+  const { openSnackBar } = useSnackBar();
+
+  const uploadImage = async (blob, callback) => {
+    if (blob.size > 1024 * 1024 * 10) return openSnackBar('10MB 이하의 이미지를 업로드해주세요.');
+
+    try {
+      const imageData = new FormData();
+      imageData.append('image', blob);
+
+      const response = await requestImageUpload(imageData);
+
+      if (!response.ok) throw new Error('이미지 업로드에 실패하였습니다');
+
+      const { imageUrl } = await response.json();
+
+      callback(imageUrl, blob.name);
+    } catch (error) {
+      console.error(error);
+      openSnackBar('해당 이미지를 업로드 할 수 없습니다.');
+    }
+  };
+
+  return { uploadImage };
+};
+
+export default useImage;
--- frontend/src/service/requests.js
+++ frontend/src/service/requests.js
@@ -291,3 +291,9 @@ export const requestGetMatchedStudylogs = ({ accessToken, startDate, endDate }) 
       Authorization: `Bearer ${accessToken}`,
     },
   });
+
+export const requestImageUpload = (formData) =>
+  fetch(`${BASE_URL}/image`, {
+    method: 'POST',
+    body: formData,
+  });
