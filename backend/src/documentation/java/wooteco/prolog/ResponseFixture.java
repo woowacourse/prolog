9c4011f847c455f070427756915a5d7ec384b24e
youngwooyoo <fbduddn97@naver.com>
2022-09-25 18:37:57 +0900

    refactor(ProfileDocumentation): mock이용한 테스트로 수정 + response fixture 생성

---
+++ backend/src/documentation/java/wooteco/prolog/ResponseFixture.java
@@ -0,0 +1,73 @@
+package wooteco.prolog;
+
+import static wooteco.prolog.member.domain.Role.CREW;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import wooteco.prolog.member.application.dto.MemberResponse;
+import wooteco.prolog.session.application.dto.MissionResponse;
+import wooteco.prolog.session.application.dto.SessionResponse;
+import wooteco.prolog.studylog.application.dto.StudylogResponse;
+import wooteco.prolog.studylog.application.dto.StudylogsResponse;
+import wooteco.prolog.studylog.application.dto.TagResponse;
+
+public class ResponseFixture {
+
+    public static final SessionResponse SESSION_RESPONSE =
+            new SessionResponse(1L, "세션1");
+
+    public static final MissionResponse MISSION_RESPONSE1 =
+            new MissionResponse(1L, "지하철 노선도 미션", SESSION_RESPONSE);
+
+    public static final MissionResponse MISSION_RESPONSE2 =
+            new MissionResponse(1L, "로또 미션", SESSION_RESPONSE);
+
+    public static final MemberResponse MEMBER_RESPONSE = new MemberResponse(
+            1L,
+            "soulG",
+            "잉",
+            CREW,
+            "https://avatars.githubusercontent.com/u/52682603?v=4"
+    );
+
+    private static final StudylogResponse STUDY_LOG_RESPONSE1 = new StudylogResponse(
+            1L,
+            MEMBER_RESPONSE,
+            LocalDateTime.now(),
+            LocalDateTime.now(),
+            SESSION_RESPONSE,
+            MISSION_RESPONSE1,
+            "JAVA",
+            "Spring Data JPA를 학습함.",
+            List.of(new TagResponse(3L, "java"), new TagResponse(4L, "jpa")),
+            List.of(),
+            false,
+            false,
+            0,
+            false,
+            0
+    );
+
+    private static final StudylogResponse STUDY_LOG_RESPONSE2 = new StudylogResponse(
+            2L,
+            MEMBER_RESPONSE,
+            LocalDateTime.now(),
+            LocalDateTime.now(),
+            SESSION_RESPONSE,
+            MISSION_RESPONSE2,
+            "SPA",
+            "SPA 방식으로 앱을 구현하였음.\nrouter 를 구현 하여 이용함.\n",
+            List.of(new TagResponse(1L, "spa"), new TagResponse(2L, "router")),
+            List.of(),
+            false,
+            false,
+            0,
+            false,
+            0
+    );
+
+    public static final StudylogsResponse STUDY_LOGS_RESPONSE = new StudylogsResponse(List.of(
+            STUDY_LOG_RESPONSE1,
+            STUDY_LOG_RESPONSE2
+    ), 2L, 1, 1);
+}
