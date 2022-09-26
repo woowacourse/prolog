0dd2f9ffc6b348b28d62fb2332b8f4190d3bdf5d
Wishoon <heonga2@gmail.com>
2022-09-26 16:07:33 +0900

    test: 레벨로그 댓글 인수테스트 작성

--- backend/src/acceptanceTest/java/wooteco/prolog/fixtures/CommentAcceptanceFixture.java
+++ backend/src/acceptanceTest/java/wooteco/prolog/fixtures/CommentAcceptanceFixture.java
@@ -1,12 +1,15 @@
 package wooteco.prolog.fixtures;
 
+import wooteco.prolog.comment.ui.dto.CommentLevellogCreateRequest;
 import wooteco.prolog.comment.ui.dto.CommentStudylogChangeRequest;
 import wooteco.prolog.comment.ui.dto.CommentStudylogCreateRequest;
 
 public enum CommentAcceptanceFixture {
 
-    COMMENT("스터디로그의 댓글 내용입니다."),
-    UPDATED_COMMENT("수정된 스터디로그의 댓글 내용입니다.");
+    STUDY_LOG_COMMENT("스터디로그의 댓글 내용입니다."),
+    STUDY_LOG_UPDATE_COMMENT("수정된 스터디로그의 댓글 내용입니다."),
+    LEVEL_LOG_COMMENT("레벨로그의 댓글 내용입니다."),
+    LEVEL_LOG_UPDATE_COMENT("수정된 레벨로그의 댓글 내용입니다.");
 
     private final String content;
 
@@ -14,11 +17,19 @@ public enum CommentAcceptanceFixture {
         this.content = content;
     }
 
-    public CommentStudylogCreateRequest getCreateRequest() {
-        return new CommentStudylogCreateRequest(COMMENT.content);
+    public CommentStudylogCreateRequest getCreateStudylogRequest() {
+        return new CommentStudylogCreateRequest(STUDY_LOG_COMMENT.content);
     }
 
-    public CommentStudylogChangeRequest getUpdateRequest() {
-        return new CommentStudylogChangeRequest(UPDATED_COMMENT.content);
+    public CommentStudylogChangeRequest getUpdateStudylogRequest() {
+        return new CommentStudylogChangeRequest(STUDY_LOG_UPDATE_COMMENT.content);
+    }
+
+    public CommentLevellogCreateRequest getCreateLevellogRequest() {
+        return new CommentLevellogCreateRequest(STUDY_LOG_COMMENT.content);
+    }
+
+    public CommentLevellogCreateRequest getUpdateLevellogRequest() {
+        return new CommentLevellogCreateRequest(STUDY_LOG_UPDATE_COMMENT.content);
     }
 }
--- backend/src/acceptanceTest/java/wooteco/prolog/steps/CommentStepDefinitions.java
+++ backend/src/acceptanceTest/java/wooteco/prolog/steps/CommentStepDefinitions.java
@@ -1,8 +1,9 @@
 package wooteco.prolog.steps;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static wooteco.prolog.fixtures.CommentAcceptanceFixture.COMMENT;
-import static wooteco.prolog.fixtures.CommentAcceptanceFixture.UPDATED_COMMENT;
+import static wooteco.prolog.fixtures.CommentAcceptanceFixture.LEVEL_LOG_UPDATE_COMENT;
+import static wooteco.prolog.fixtures.CommentAcceptanceFixture.STUDY_LOG_COMMENT;
+import static wooteco.prolog.fixtures.CommentAcceptanceFixture.STUDY_LOG_UPDATE_COMMENT;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -21,7 +22,14 @@ public class CommentStepDefinitions extends AcceptanceSteps {
     @When("{long}번 스터디로그에 대한 댓글을 작성하면")
     public void 스터디로그에_대한_댓글을_작성하면(Long studylogId) {
         context.invokeHttpPostWithToken("/studylogs/" + studylogId + "/comments",
-            COMMENT.getCreateRequest());
+            STUDY_LOG_COMMENT.getCreateStudylogRequest());
+    }
+
+    @Given("{long}번 레벨로그에 대한 댓글을 작성하고")
+    @When("{long}번 레벨로그에 대한 댓글을 작성하면")
+    public void 레벨로그에_대한_댓글을_작성하면(Long levellogId) {
+        context.invokeHttpPostWithToken("/levellogs/" + levellogId + "/comments",
+            STUDY_LOG_COMMENT.getCreateLevellogRequest());
     }
 
     @Then("댓글이 작성된다")
@@ -54,15 +62,39 @@ public class CommentStepDefinitions extends AcceptanceSteps {
             ));
     }
 
+    @When("{long}번 레벨로그의 댓글을 조회하면")
+    public void 레벨로그의_댓글을_조회하면(Long levellogId) {
+        context.invokeHttpPostWithToken("/levellogs/" + levellogId + "/comments");
+    }
+
+    @Then("해당 레벨로그의 댓글 목록을 조회한다")
+    public void 해당_레벨로그의_댓글_목록을_조회한다() {
+        int statusCode = context.response.statusCode();
+        CommentsResponse commentsResponse = context.response.as(CommentsResponse.class);
+
+        assertThat(statusCode).isEqualTo(HttpStatus.OK.value());
+        assertThat(commentsResponse.getData())
+            .usingRecursiveComparison()
+            .ignoringFields("createdAt").isEqualTo(List.of(
+                new CommentResponse(1L, new CommentMemberResponse(1L, GithubResponses.브라운.getLogin(),
+                    GithubResponses.브라운.getName(), GithubResponses.브라운.getAvatarUrl(), "CREW"),
+                    "레벨로그의 댓글 내용입니다.", null),
+                new CommentResponse(2L, new CommentMemberResponse(2L, GithubResponses.웨지.getLogin(),
+                    GithubResponses.웨지.getName(), GithubResponses.웨지.getAvatarUrl(), "CREW"),
+                    "레벨로그의 댓글 내용입니다.", null)
+            ));
+    }
+
     @When("{long}번 스터디로그에 대한 {long}번 댓글을 수정하면")
     public void 스터디로그에_대한_댓글을_수정하면(Long studylogId, Long commentId) {
         context.invokeHttpPutWithToken("/studylogs/" + studylogId + "/comments/" + commentId,
-            UPDATED_COMMENT.getUpdateRequest());
+            STUDY_LOG_UPDATE_COMMENT.getUpdateStudylogRequest());
     }
 
-    @When("{long}번 스터디로그에 대한 {long}번 댓글을 삭제하면")
-    public void 스터디로그에_대한_댓글을_삭제하면(Long studylogId, Long commentId) {
-        context.invokeHttpDeleteWithToken("/studylogs/" + studylogId + "/comments/" + commentId);
+    @When("{long}번 레벨로그에 대한 {long}번 댓글을 수정하면")
+    public void 레벨로그에_대한_댓글을_수정하면(Long levellogId, Long commentLevellogId) {
+        context.invokeHttpPutWithToken("/studylogs/" + levellogId + "/comments/" + commentLevellogId,
+            LEVEL_LOG_UPDATE_COMENT.getUpdateLevellogRequest());
     }
 
     @Then("댓글이 수정된다")
@@ -72,10 +104,21 @@ public class CommentStepDefinitions extends AcceptanceSteps {
         assertThat(statusCode).isEqualTo(HttpStatus.NO_CONTENT.value());
     }
 
+    @When("{long}번 스터디로그에 대한 {long}번 댓글을 삭제하면")
+    public void 스터디로그에_대한_댓글을_삭제하면(Long studylogId, Long commentId) {
+        context.invokeHttpDeleteWithToken("/studylogs/" + studylogId + "/comments/" + commentId);
+    }
+
+    @When("{long}번 레벨로그에 대한 {long}번 댓글을 삭제하면")
+    public void 레벨로그에_대한_댓글을_삭제하면(Long levellogId, Long commentLevellogId) {
+        context.invokeHttpDeleteWithToken("/studylogs/" + levellogId + "/comments/" + commentLevellogId);
+    }
+
     @Then("댓글이 삭제된다")
     public void 댓글이_삭제_된다() {
         int statusCode = context.response.statusCode();
 
         assertThat(statusCode).isEqualTo(HttpStatus.NO_CONTENT.value());
     }
+
 }
