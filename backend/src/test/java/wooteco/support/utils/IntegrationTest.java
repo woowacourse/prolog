5ba075b9a2d2ff6d1d031d9acc50bc8387120477
정진혁 <wilgur513@naver.com>
2022-10-01 01:35:19 +0900

    refactor: 실패하는 테스트 코드 수정

--- backend/src/test/java/wooteco/support/utils/IntegrationTest.java
+++ backend/src/test/java/wooteco/support/utils/IntegrationTest.java
@@ -11,7 +11,6 @@ import org.springframework.transaction.annotation.Transactional;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @SpringBootTest
-@Transactional
 @ExtendWith(PersistenceRollbackExtension.class)
 public @interface IntegrationTest {
 
