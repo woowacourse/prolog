cddeae5d42d77972e666f216e0c3bce3b0d0751e
정진혁 <wilgur513@naver.com>
2022-10-01 01:07:39 +0900

    refactor: IntegrationTest를 Transactional을 사용해 롤백

--- backend/src/test/java/wooteco/support/utils/IntegrationTest.java
+++ backend/src/test/java/wooteco/support/utils/IntegrationTest.java
@@ -6,10 +6,12 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @SpringBootTest
+@Transactional
 @ExtendWith(PersistenceRollbackExtension.class)
 public @interface IntegrationTest {
 
