2970279c816c973ebb1bc5f19771caeabfb883e5
Marco <59413128+wonsss@users.noreply.github.com>
2022-10-12 14:39:54 +0900

    feat: 로드맵을 위한 하위 컴포넌트 구현(sidesheet modal) (#1065)

---
+++ frontend/src/components/@shared/SideSheet/SideSheet.style.ts
@@ -0,0 +1,42 @@
+import styled from '@emotion/styled';
+import { css, keyframes } from '@emotion/react';
+import { SideSheetProps } from './SideSheet';
+
+export type SideSheetContentProps = Pick<SideSheetProps, 'width'>;
+
+const slide = keyframes`
+  from{
+    transform: translateX(100%);
+  }
+  to {
+    transform: translateX(0);
+  }
+`;
+
+export const SideSheetContent = styled.div<SideSheetContentProps>`
+  ${({ width }) => css`
+    position: fixed;
+    top: 0;
+    right: 0;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    width: ${width ?? '500px'};
+    height: 100%;
+    overflow-y: auto;
+    padding: 34px 0;
+    background-color: #fff;
+    z-index: 101;
+    animation: 0.25s ease-in forwards ${slide};
+  `}
+`;
+
+export const Backdrop = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.4);
+  z-index: 100;
+`;
