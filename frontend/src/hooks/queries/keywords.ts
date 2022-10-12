348d3f0a50eeaadbebf5ae29785c7f600d3410a1
gytks4 <gytks4@naver.com>
2022-10-12 15:47:01 +0900

    feat: Quiz, Keyword useQuery 훅 단 정의

---
+++ frontend/src/hooks/queries/keywords.ts
@@ -0,0 +1,63 @@
+import { useQuery } from 'react-query';
+import {
+  getKeyword,
+  getTopKeywordList,
+  getChildKeywordList,
+  getQuizListByKeyword,
+} from '../../apis/keywords';
+
+const QUERY_KEY = {
+  keyword: 'keyword',
+  topKeywordList: 'topKeywordList',
+  childKeywordList: 'childKeywordList',
+  quizListByKeyword: 'quizListByKeyword',
+};
+
+export const useGetKeyword = ({
+  sessionId,
+  keywordId,
+}: {
+  sessionId: number;
+  keywordId: number;
+}) => {
+  return useQuery([QUERY_KEY.keyword], () =>
+    getKeyword({
+      sessionId,
+      keywordId,
+    })
+  );
+};
+
+export const useGetTopKeywordList = (sessionId: number) => {
+  return useQuery([QUERY_KEY.topKeywordList], () => getTopKeywordList(sessionId));
+};
+
+export const useGetChildKeywordList = ({
+  sessionId,
+  keywordId,
+}: {
+  sessionId: number;
+  keywordId: number;
+}) => {
+  return useQuery([QUERY_KEY.childKeywordList], () =>
+    getChildKeywordList({
+      sessionId,
+      keywordId,
+    })
+  );
+};
+
+export const useGetQuizListByKeyword = ({
+  sessionId,
+  keywordId,
+}: {
+  sessionId: number;
+  keywordId: number;
+}) => {
+  return useQuery([QUERY_KEY.quizListByKeyword], () =>
+    getQuizListByKeyword({
+      sessionId,
+      keywordId,
+    })
+  );
+};
