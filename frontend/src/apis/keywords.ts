858c6b956db9ef9643cba88f53833ff0f3cfd8a5
gytks4 <gytks4@naver.com>
2022-10-12 15:40:37 +0900

    feat: Quiz, Keyword fetcher 정의

---
+++ frontend/src/apis/keywords.ts
@@ -0,0 +1,50 @@
+import { client } from '.';
+import { KeywordListResponse, KeywordResponse } from '../models/Keywords';
+
+export const getKeyword = async ({
+  sessionId,
+  keywordId,
+}: {
+  sessionId: number;
+  keywordId: number;
+}) => {
+  const response = await client.get<KeywordResponse>(
+    `/sessions/${sessionId}/keywords/${keywordId}`
+  );
+
+  return response.data;
+};
+
+export const getTopKeywordList = async (sessionId: number) => {
+  const response = await client.get<KeywordListResponse>(`/sessions/${sessionId}/keywords`);
+
+  return response.data;
+};
+
+export const getChildKeywordList = async ({
+  sessionId,
+  keywordId,
+}: {
+  sessionId: number;
+  keywordId: number;
+}) => {
+  const response = await client.get<KeywordListResponse>(
+    `/sessions/${sessionId}/keywords/${keywordId}/children`
+  );
+
+  return response.data;
+};
+
+export const getQuizListByKeyword = async ({
+  sessionId,
+  keywordId,
+}: {
+  sessionId: number;
+  keywordId: number;
+}) => {
+  const response = await client.get<KeywordListResponse>(
+    `/sessions/${sessionId}/keywords/${keywordId}/quizs`
+  );
+
+  return response.data;
+};
