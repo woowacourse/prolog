a498d0129a97b7c8b4340645046f2d765c79c6a7
youngwooyoo <fbduddn97@naver.com>
2022-10-16 22:08:27 +0900

    refactor(StudylogRssFeedDocumentation): mock 이용한 테스트로 수정

--- backend/src/documentation/java/wooteco/prolog/docu/StudylogRssFeedDocumentation.java
+++ backend/src/documentation/java/wooteco/prolog/docu/StudylogRssFeedDocumentation.java
@@ -7,10 +7,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static wooteco.prolog.ResponseFixture.MEMBER;
 
 import io.restassured.module.mockmvc.response.ValidatableMockMvcResponse;
-import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -36,13 +34,13 @@ class StudylogRssFeedDocumentation extends NewDocumentation {
     @Test
     void RSS_피드를_조회한다() {
         //given
-//        given(studylogService.readRssFeeds(any()))
-//                .willReturn(List.of(
-//                        StudylogRssFeedResponse.of(
-//                                new Studylog(MEMBER, "Prolog | 우아한테크코스 학습로그 저장소","dd",new Session(1L, "세션1"),
-//                                        new Mission("자동차미션", new Session(1L, "레벨1")), List.of(new Tag(1L, "자바")))
-//                                ,"http://localhost:8080"))
-//                );
+        given(studylogService.readRssFeeds(any()))
+                .willReturn(List.of(
+                        StudylogRssFeedResponse.of(
+                                new Studylog(MEMBER, "Prolog | 우아한테크코스 학습로그 저장소","dd",new Session(1L, "세션1"),
+                                        new Mission("자동차미션", new Session(1L, "레벨1")), List.of(new Tag(1L, "자바")))
+                                ,"http://localhost:8080"))
+                );
 
         //when
         ValidatableMockMvcResponse response = given
@@ -57,65 +55,4 @@ class StudylogRssFeedDocumentation extends NewDocumentation {
         //docs
         response.apply(document("rss-feed/read"));
     }
-//
-//    private ExtractableResponse<Response> 스터디로그_등록함(List<StudylogRequest> request) {
-//        return RestAssured.given().log().all()
-//            .header("Authorization", "Bearer " + 로그인_사용자.getAccessToken())
-//            .body(request)
-//            .contentType(APPLICATION_JSON_VALUE)
-//            .when().log().all()
-//            .post("/studylogs")
-//            .then().log().all().extract();
-//    }
-//
-//    private StudylogRequest studylogRequest1() {
-//        Long sessionId = 세션_등록함(new SessionRequest("세션 1"));
-//        Long missionId = 미션_등록함(new MissionRequest("세션 1 - 어떤 미션", sessionId));
-//
-//        return new StudylogRequest(
-//            "어떤 타이틀",
-//            "어떤 내용",
-//            missionId,
-//            Arrays.asList(new TagRequest("어떤 태그 1"), new TagRequest("어떤 태그 2"))
-//        );
-//    }
-//
-//    private StudylogRequest studylogRequest2() {
-//        Long sessionId = 세션_등록함(new SessionRequest("세션 2"));
-//        Long missionId = 미션_등록함(new MissionRequest("세션 2 - 어쩌구 미션", sessionId));
-//
-//        return new StudylogRequest(
-//            "어쩌구 타이틀",
-//            "어쩌구 내용",
-//            missionId,
-//            Arrays.asList(new TagRequest("어쩌구 태그 1"), new TagRequest("어쩌구 태그 2"))
-//        );
-//    }
-//
-//    private Long 세션_등록함(SessionRequest request) {
-//        return RestAssured
-//            .given().log().all()
-//            .body(request)
-//            .contentType(APPLICATION_JSON_VALUE)
-//            .when()
-//            .post("/sessions")
-//            .then()
-//            .log().all()
-//            .extract()
-//            .as(SessionResponse.class)
-//            .getId();
-//    }
-//
-//    private Long 미션_등록함(MissionRequest request) {
-//        return RestAssured.given()
-//            .body(request)
-//            .contentType(APPLICATION_JSON_VALUE)
-//            .when()
-//            .post("/missions")
-//            .then()
-//            .log().all()
-//            .extract()
-//            .as(MissionResponse.class)
-//            .getId();
-//    }
 }
