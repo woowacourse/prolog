34f6033fbf1cfc656eb9e0d3e4ef1bbea89f278f
Youngwoo Yoo <63579113+Yboyu0u@users.noreply.github.com>
2022-10-17 15:29:53 +0900

    refactor: 문서화 테스트 개선 (#1068)
    * refactor: LoginDocument mock이용한 테스트로 수정
    
    * refactor(MissionDocumentation): mock이용한 테스트로 수정
    
    * refactor(ProfileDocumentation): mock이용한 테스트로 수정 + response fixture 생성
    
    * refactor(CommentDocumentation): mock 이용한 테스트로 수정
    
    * refactor(MemberDocumentation, MemberReactionDocumentation): mock 이용한 테스트로 수정
    
    * refactor(SessionMemberDocumentation): mock 이용한 테스트로 수정
    
    * refactor(TagDocumentation): mock 이용한 테스트로 수정
    
    * refactor(StudylogOverviewDocumentation): mock 이용한 테스트로 수정
    
    * refactor(StudylogRssFeedDocumentation): mock 이용한 테스트로 수정
    
    * chore: 자바 버전을 8에서 9로 변경
    
    * test(StudylogRssFeedDocumentation): 응답 fixture로 빼기

--- backend/src/documentation/java/wooteco/prolog/docu/LoginDocumentation.java
+++ backend/src/documentation/java/wooteco/prolog/docu/LoginDocumentation.java
@@ -1,23 +1,47 @@
 package wooteco.prolog.docu;
 
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import io.restassured.module.mockmvc.response.ValidatableMockMvcResponse;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import wooteco.prolog.Documentation;
 import wooteco.prolog.GithubResponses;
+import wooteco.prolog.NewDocumentation;
+import wooteco.prolog.login.application.GithubLoginService;
 import wooteco.prolog.login.application.dto.TokenRequest;
 import wooteco.prolog.login.application.dto.TokenResponse;
+import wooteco.prolog.login.ui.GithubLoginController;
 
-public class LoginDocumentation extends Documentation {
+@WebMvcTest(controllers = GithubLoginController.class)
+public class LoginDocumentation extends NewDocumentation {
+
+    @MockBean
+    private GithubLoginService loginService;
 
     @Test
     void 토큰을_생성한다() {
-        TokenRequest params = new TokenRequest(GithubResponses.소롱.getCode());
+        //given
+        given(loginService.createToken(any()))
+                .willReturn(TokenResponse.of(accessToken));
 
-        given("login/token")
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .body(params)
-            .when().post("/login/token")
-            .then().log().all()
-            .extract().as(TokenResponse.class);
+        //when
+        TokenRequest params = new TokenRequest(GithubResponses.소롱.getCode());
+        ValidatableMockMvcResponse response = given
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(params)
+                .when().post("/login/token")
+                .then().log().all();
+
+        //then
+        response.expect(status().isOk());
+
+        //docs
+        response.apply(document("login/token"));
     }
 }
