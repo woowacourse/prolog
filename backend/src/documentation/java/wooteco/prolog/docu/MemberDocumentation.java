bd3e730131ff347e0b60377a7f64cb7d038c3cbe
Youngwoo Yoo <63579113+Yboyu0u@users.noreply.github.com>
2022-10-17 15:29:53 +0900

    refactor: 문서화 테스트 개선 (#1068)
    * refactor: LoginDocument mock이용한 테스트로 수정
    
    * refactor(MissionDocumentation): mock이용한 테스트로 수정
    
    * refactor(ProfileDocumentation): mock이용한 테스트로 수정 + response fixture 생성
    
    * refactor(CommentDocumentation): mock 이용한 테스트로 수정
    
    * refactor(MemberDocumentation, MemberReactionDocumentation): mock 이용한 테스트로 수정
    
    * refactor(SessionMemberDocumentation): mock 이용한 테스트로 수정
    
    * refactor(TagDocumentation): mock 이용한 테스트로 수정
    
    * refactor(StudylogOverviewDocumentation): mock 이용한 테스트로 수정
    
    * refactor(StudylogRssFeedDocumentation): mock 이용한 테스트로 수정
    
    * chore: 자바 버전을 8에서 9로 변경
    
    * test(StudylogRssFeedDocumentation): 응답 fixture로 빼기

--- backend/src/documentation/java/wooteco/prolog/docu/MemberDocumentation.java
+++ backend/src/documentation/java/wooteco/prolog/docu/MemberDocumentation.java
@@ -1,195 +1,86 @@
 package wooteco.prolog.docu;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static wooteco.prolog.ResponseFixture.MEMBER;
 
-import io.restassured.RestAssured;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import io.restassured.module.mockmvc.response.ValidatableMockMvcResponse;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import wooteco.prolog.Documentation;
 import wooteco.prolog.GithubResponses;
-import wooteco.prolog.member.application.dto.MemberScrapRequest;
+import wooteco.prolog.NewDocumentation;
+import wooteco.prolog.member.application.MemberService;
 import wooteco.prolog.member.application.dto.MemberUpdateRequest;
-import wooteco.prolog.session.application.dto.MissionRequest;
-import wooteco.prolog.session.application.dto.MissionResponse;
-import wooteco.prolog.session.application.dto.SessionRequest;
-import wooteco.prolog.session.application.dto.SessionResponse;
-import wooteco.prolog.studylog.application.dto.StudylogRequest;
-import wooteco.prolog.studylog.application.dto.StudylogsResponse;
-import wooteco.prolog.studylog.application.dto.TagRequest;
+import wooteco.prolog.member.ui.MemberController;
 
-public class MemberDocumentation extends Documentation {
+@WebMvcTest(controllers = MemberController.class)
+public class MemberDocumentation extends NewDocumentation {
 
+    @MockBean
+    private MemberService memberService;
+
+    @Deprecated
     @Test
     void 사용자_본인_정보를_조회한다() {
-        given("members/me")
-            .header("Authorization", "Bearer " + 로그인_사용자.getAccessToken())
-            .when().get("/members/me")
-            .then().log().all()
-            .extract();
+        //given
+        given(memberService.findById(any()))
+                .willReturn(MEMBER);
+
+        //when
+        ValidatableMockMvcResponse response = given
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/members/me")
+                .then().log().all();
+
+        //then
+        response.expect(status().isOk());
+
+        //docs
+        response.apply(document("members/me"));
     }
 
     @Test
     void 자신의_사용자_정보를_수정한다() {
-        MemberUpdateRequest memberUpdateRequest = new MemberUpdateRequest(
-            "다른이름",
-            "https://avatars.githubusercontent.com/u/51393021?v=4"
-        );
-        given("members/edit")
-            .header("Authorization", "Bearer " + 로그인_사용자.getAccessToken())
-            .body(memberUpdateRequest)
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when().put("/members/{username}", GithubResponses.소롱.getLogin())
-            .then().log().all()
-            .extract();
+        //given, when
+        MemberUpdateRequest params = new MemberUpdateRequest(
+                "다른이름",
+                "https://avatars.githubusercontent.com/u/51393021?v=4");
+        ValidatableMockMvcResponse response = given
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(params)
+                .when().put("/members/{username}", GithubResponses.소롱.getLogin())
+                .then().log().all();
+
+        //then
+        response.expect(status().isOk());
+
+        //docs
+        response.apply(document("members/edit"));
     }
 
     @Test
     void 사용자_정보를_조회한다() {
-        given("members/read")
-            .header("Authorization", "Bearer " + 로그인_사용자.getAccessToken())
-            .when().get("/members/{username}", GithubResponses.소롱.getLogin())
-            .then().log().all()
-            .extract();
-    }
-
-    @Test
-    void 스터디로그를_스크랩한다() {
         //given
-        ExtractableResponse<Response> studylogResponse = 스터디로그_등록함(createStudylogRequest(1));
-        String location = studylogResponse.header("Location");
-        String logId = location.substring(location.lastIndexOf('/') + 1);
+        given(memberService.findByUsername(any()))
+                .willReturn(MEMBER);
 
         //when
-        String scrapedLogLocation = given("members/scrap/create")
-            .header("Authorization", "Bearer " + 로그인_사용자.getAccessToken())
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .body(new MemberScrapRequest(Long.valueOf(logId)))
-            .when()
-            .post("/members/scrap")
-            .header("Location");
+        ValidatableMockMvcResponse response = given
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/members/{username}", GithubResponses.소롱.getLogin())
+                .then().log().all();
 
         //then
-        RestAssured.given()
-            .header("Authorization", "Bearer " + 로그인_사용자.getAccessToken())
-            .when().get(scrapedLogLocation)
-            .then().log().all()
-            .assertThat()
-            .body("id", equalTo(Integer.parseInt(logId)));
-    }
+        response.expect(status().isOk());
 
-    @Test
-    void 스터디로그를_스크랩취소한다() {
-        //given
-        ExtractableResponse<Response> studylogResponse = 스터디로그_등록함(createStudylogRequest(1));
-        String location = studylogResponse.header("Location");
-        Long logId = Long.valueOf(location.substring(location.lastIndexOf('/') + 1));
-
-        스크랩_등록함(logId);
-
-        //when & then
-        given("members/scrap/delete")
-            .header("Authorization", "Bearer " + 로그인_사용자.getAccessToken())
-            .when()
-            .delete("/members/scrap?studylog="+ logId)
-            .then().log().all()
-            .assertThat()
-            .statusCode(equalTo(HttpStatus.NO_CONTENT.value()));
-    }
-
-    @Test
-    void 스크랩한_스터디로그를_목록을_조회한다() {
-        //given
-        int scrapSize = 4;
-        ExtractableResponse<Response> studylogResponse = 스터디로그_등록함(createStudylogRequest(scrapSize));
-        String location = studylogResponse.header("Location");
-        Long logId = Long.valueOf(location.substring(location.lastIndexOf('/') + 1));
-
-        for (int i = 0; i < scrapSize; i++) {
-            스크랩_등록함(logId + i);
-        }
-
-        //when
-        int listSize = given("members/scrap/list")
-            .header("Authorization", "Bearer " + 로그인_사용자.getAccessToken())
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .body(new MemberScrapRequest(logId))
-            .when()
-            .get("/members/scrap")
-            .then().log().all()
-            .extract()
-            .as(StudylogsResponse.class)
-            .getData()
-            .size();
-
-        //then
-        assertThat(listSize).isEqualTo(scrapSize);
-    }
-
-    private List<StudylogRequest> createStudylogRequest(int size) {
-        List<StudylogRequest> studylogRequests = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
-            String title = "뭐라도 스터디로그가 있어야하니까";
-            String content = "SPA 방식으로 앱을 구현하였음.\n" + "router 를 구현 하여 이용함.\n";
-            Long sessionId = 세션_등록함(new SessionRequest("세션" + i));
-            Long missionId = 미션_등록함(new MissionRequest(String.format("세션%d 미션", i), sessionId));
-            List<TagRequest> tags = Arrays.asList(new TagRequest(String.format("%d번 태그", i)));
-
-            studylogRequests.add(new StudylogRequest(title, content, missionId, tags));
-        }
-        return studylogRequests;
-    }
-
-    private String 스크랩_등록함(Long logId) {
-        return RestAssured.given()
-            .header("Authorization", "Bearer " + 로그인_사용자.getAccessToken())
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .body(new MemberScrapRequest(logId))
-            .when()
-            .post("/members/scrap")
-            .header("Location");
-    }
-
-    private ExtractableResponse<Response> 스터디로그_등록함(List<StudylogRequest> studylogRequests) {
-        return RestAssured.given().log().all()
-            .header("Authorization", "Bearer " + 로그인_사용자.getAccessToken())
-            .body(studylogRequests)
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when().log().all()
-            .post("/posts")
-            .then().log().all().extract();
-    }
-
-    private Long 세션_등록함(SessionRequest request) {
-        return RestAssured
-            .given().log().all()
-            .body(request)
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when()
-            .post("/sessions")
-            .then()
-            .log().all()
-            .extract()
-            .as(SessionResponse.class)
-            .getId();
-    }
-
-    private Long 미션_등록함(MissionRequest request) {
-        return RestAssured.given()
-            .body(request)
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when()
-            .post("/missions")
-            .then()
-            .log().all()
-            .extract()
-            .as(MissionResponse.class)
-            .getId();
+        //docs
+        response.apply(document("members/read"));
     }
 }
--- backend/src/documentation/java/wooteco/prolog/docu/MissionDocumentation.java
+++ backend/src/documentation/java/wooteco/prolog/docu/MissionDocumentation.java
@@ -1,118 +1,92 @@
 package wooteco.prolog.docu;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doThrow;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static wooteco.prolog.ResponseFixture.MISSION_RESPONSE1;
+import static wooteco.prolog.ResponseFixture.MISSION_RESPONSE2;
 
-import io.restassured.RestAssured;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
-import java.util.Arrays;
+import io.restassured.module.mockmvc.response.ValidatableMockMvcResponse;
 import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import wooteco.prolog.Documentation;
-import wooteco.prolog.session.application.dto.SessionRequest;
-import wooteco.prolog.session.application.dto.SessionResponse;
+import wooteco.prolog.NewDocumentation;
+import wooteco.prolog.session.application.MissionService;
 import wooteco.prolog.session.application.dto.MissionRequest;
-import wooteco.prolog.session.application.dto.MissionResponse;
+import wooteco.prolog.session.ui.MissionController;
+import wooteco.prolog.studylog.exception.DuplicateMissionException;
 
-public class MissionDocumentation extends Documentation {
+@WebMvcTest(controllers = MissionController.class)
+public class MissionDocumentation extends NewDocumentation {
 
-    private Long sessionId;
-
-    @BeforeEach
-    void setUp() {
-        sessionId = 세션_등록함(new SessionRequest("세션1"));
-    }
+    @MockBean
+    private MissionService missionService;
 
     @Test
     void 미션_목록을_조회한다() {
-        // given
-        MissionRequest request1 = new MissionRequest("로또 미션", sessionId);
-        ExtractableResponse<Response> mission1 = 미션_등록함(request1);
-        MissionRequest request2 = new MissionRequest("블랙잭 미션", sessionId);
-        ExtractableResponse<Response> mission2 = 미션_등록함(request2);
+        //given
+        given(missionService.findAll())
+                .willReturn(List.of(MISSION_RESPONSE1, MISSION_RESPONSE2));
 
-        // when
-        ExtractableResponse<Response> response = given("mission/list")
-            .when()
-            .get("/missions")
-            .then()
-            .log().all()
-            .extract();
+        //when
+        ValidatableMockMvcResponse response = given
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/missions")
+                .then().log().all();
 
-        // then
-        List<MissionResponse> categories = response.jsonPath().getList(".", MissionResponse.class);
-        assertThat(categories).usingRecursiveComparison().isEqualTo(
-            Arrays.asList(
-                mission1.as(MissionResponse.class),
-                mission2.as(MissionResponse.class)));
+        //then
+        response.expect(status().isOk());
+
+        //docs
+        response.apply(document("mission/list"));
     }
 
     @Test
     void 미션을_저장한다() {
-        // given
-        MissionRequest request = new MissionRequest("지하철 노선도 미션", sessionId);
+        //given
+        given(missionService.create(any()))
+                .willReturn(MISSION_RESPONSE1);
 
-        // when
-        ExtractableResponse<Response> response = given("mission/create/success")
-            .body(request)
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when()
-            .post("/missions")
-            .then()
-            .log().all()
-            .extract();
+        //when
+        MissionRequest params = new MissionRequest("지하철 노선도 미션", 1L);
+        ValidatableMockMvcResponse response = given
+                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+                .body(params)
+                .when().post("/missions")
+                .then().log().all();
 
-        // then
-        MissionResponse missionResponse = response.as(MissionResponse.class);
-        assertThat(missionResponse.getName()).isEqualTo(request.getName());
+        //then
+        response.expect(status().isOk());
+
+        //docs
+        response.apply(document("mission/create/success"));
     }
 
     @Test
     void 미션_이름이_중복될_경우_예외처리한다() {
-        // given
-        MissionRequest request = new MissionRequest("지하철 노선도 미션", sessionId);
-        미션_등록함(request);
+        //given
+        doThrow(new DuplicateMissionException()).when(missionService)
+                .create(any());
 
-        // when
-        ExtractableResponse<Response> response = given("mission/create/fail")
-            .body(request)
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when()
-            .post("/missions")
-            .then()
-            .log().all()
-            .extract();
+        //when
+        MissionRequest params = new MissionRequest("지하철 노선도 미션", 1L);
+        ValidatableMockMvcResponse response = given
+                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+                .body(params)
+                .when().post("/missions")
+                .then().log().all();
 
-        // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
-        assertThat((String) response.jsonPath().get("message")).isEqualTo("미션이 중복됩니다.");
-    }
+        //then
+        response.expect(status().isBadRequest());
+        response.expect(jsonPath("code").value(3001));
+        response.expect(jsonPath("message").value("미션이 중복됩니다."));
 
-    private ExtractableResponse<Response> 미션_등록함(MissionRequest request) {
-        return RestAssured.given()
-            .body(request)
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when()
-            .post("/missions")
-            .then()
-            .log().all()
-            .extract();
-    }
-
-    private Long 세션_등록함(SessionRequest request) {
-        return RestAssured
-            .given().log().all()
-            .body(request)
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when()
-            .post("/sessions")
-            .then()
-            .log().all()
-            .extract()
-            .as(SessionResponse.class)
-            .getId();
+        //docs
+        response.apply(document("mission/create/fail"));
     }
 }
