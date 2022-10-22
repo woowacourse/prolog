0f74fc97ccf4940f42352530e0221f6940ba1204
Wishoon <heonga2@gmail.com>
2022-10-22 16:28:10 +0900

    feat: Keyword의 부모와 자식 키워드에 대한 전체 조회 도메인 기능 구현

--- backend/src/main/java/wooteco/prolog/roadmap/repository/KeywordRepository.java
+++ backend/src/main/java/wooteco/prolog/roadmap/repository/KeywordRepository.java
@@ -1,8 +1,11 @@
 package wooteco.prolog.roadmap.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import wooteco.prolog.roadmap.Keyword;
 
 public interface KeywordRepository extends JpaRepository<Keyword, Long> {
 
+    @Query("SELECT k FROM Keyword k join fetch k.children where k.id = :keywordId ")
+    Keyword findFetchById(Long keywordId);
 }
---
+++ backend/src/test/java/wooteco/prolog/roadmap/repository/KeywordRepositoryTest.java
@@ -0,0 +1,75 @@
+package wooteco.prolog.roadmap.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import javax.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.TestConstructor;
+import wooteco.prolog.roadmap.Keyword;
+import wooteco.support.utils.RepositoryTest;
+
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@RepositoryTest
+class KeywordRepositoryTest {
+
+    private final KeywordRepository keywordRepository;
+    private final EntityManager em;
+
+    public KeywordRepositoryTest(final KeywordRepository keywordRepository, final EntityManager em) {
+        this.keywordRepository = keywordRepository;
+        this.em = em;
+    }
+
+    @Test
+    void 부모_키워드와_1뎁스까지의_자식_키워드를_함께_조회할_수_있다() {
+        // given
+        Keyword keywordParent = createKeywordParent(Keyword.createKeyword("자바", "자바에 대한 설명", 1, 1, 1L, null));
+        createKeywordChildren(Keyword.createKeyword("List", "List에 대한 설명", 1, 1, 1L, keywordParent));
+        createKeywordChildren(Keyword.createKeyword("Set", "Set에 대한 설명", 2, 1, 1L, keywordParent));
+        Long keywordParentId = keywordParent.getId();
+        em.clear();
+
+        // when
+        Keyword extract = keywordRepository.findFetchById(keywordParentId);
+
+        // then
+        assertAll(
+            () -> assertThat(extract.getId()).isNotNull(),
+            () -> assertThat(extract.getChildren()).hasSize(2));
+    }
+
+    @Test
+    void 부모_키워드와_2뎁스까지의_자식_키워드를_함께_조회할_수_있다() {
+        // given
+        Keyword parent = createKeywordParent(Keyword.createKeyword("자바", "자바에 대한 설명", 1, 1, 1L, null));
+
+        Keyword children1 = createKeywordChildren(Keyword.createKeyword("List", "List에 대한 설명", 1, 1, 1L, parent));
+        Keyword children2 = createKeywordChildren(Keyword.createKeyword("Set", "Set에 대한 설명", 2, 1, 1L, parent));
+
+        createKeywordChildren(Keyword.createKeyword("List.of()", "List.of()에 대한 설명", 1, 1, 1L, children1));
+        createKeywordChildren(Keyword.createKeyword("Set.of()", "Set.of()에 대한 설명", 1, 1, 1L, children2));
+
+        Long keywordParentId = parent.getId();
+        em.clear();
+
+        // when
+        Keyword extract = keywordRepository.findFetchById(keywordParentId);
+
+        // then
+        assertAll(
+            () -> assertThat(extract.getId()).isNotNull(),
+            () -> assertThat(extract.getChildren()).hasSize(2),
+            () -> assertThat(extract.getChildren().get(0).getChildren().get(0).getName()).isEqualTo("List.of()"),
+            () -> assertThat(extract.getChildren().get(1).getChildren().get(0).getName()).isEqualTo("Set.of()")
+        );
+    }
+
+    private Keyword createKeywordParent(final Keyword keyword) {
+        return keywordRepository.save(keyword);
+    }
+
+    private Keyword createKeywordChildren(final Keyword keyword) {
+        return keywordRepository.save(keyword);
+    }
+}
