8e5d884f15bbe02705b9461317145bcf9fe5f2c2
Wishoon <heonga2@gmail.com>
2022-10-22 19:03:25 +0900

    fix: Keyword의 parent 값도 Fetch로 가져오도록 수정

--- backend/src/main/java/wooteco/prolog/roadmap/repository/KeywordRepository.java
+++ backend/src/main/java/wooteco/prolog/roadmap/repository/KeywordRepository.java
@@ -6,6 +6,8 @@ import wooteco.prolog.roadmap.Keyword;
 
 public interface KeywordRepository extends JpaRepository<Keyword, Long> {
 
-    @Query("SELECT k FROM Keyword k join fetch k.children where k.id = :keywordId ")
+    @Query("SELECT k FROM Keyword k "
+        + "LEFT JOIN FETCH k.parent p "
+        + "JOIN FETCH k.children c WHERE k.id = :keywordId ")
     Keyword findFetchById(Long keywordId);
 }
