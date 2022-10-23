e59a834a05d3b52f0a1c10f9144afe368e94403a
her0807 <her0807@naver.com>
2022-10-23 18:33:12 +0900

    chore: keyword db migration

---
+++ backend/src/main/resources/db/migration/prod/V31__create_keyword_table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE keyword
+(
+    id          BIGINT       NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    seq         BIGINT       NOT NULL,
+    importance  BIGINT       NOT NULL,
+    session_id  BIGINT       NOT NULL,
+    parent_id   BIGINT       NOT NULL,
+    CONSTRAINT pk_keyword PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+
+ALTER TABLE keyword
+    ADD CONSTRAINT FK_keyword_session_id
+        FOREIGN KEY (session_id) REFERENCES session (id);
