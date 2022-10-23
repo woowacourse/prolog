30aa929e3991f1a885b36d8eafcf4bda6b2fc34d
her0807 <her0807@naver.com>
2022-10-23 18:32:36 +0900

    fix: keyword 내에 잘못 설정된 mappedBy 수정

---
+++ backend/src/main/java/wooteco/prolog/roadmap/Keyword.java
@@ -0,0 +1,83 @@
+package wooteco.prolog.roadmap;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.BatchSize;
+import wooteco.prolog.roadmap.exception.KeywordOrderException;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Keyword {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false)
+    private int seq;
+
+    @Column(nullable = false)
+    private int importance;
+
+    @Column(name = "session_id", nullable = false)
+    private Long sessionId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Keyword parent;
+
+    @BatchSize(size = 1000)
+    @OneToMany(mappedBy = "children", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Keyword> children = new ArrayList<>();
+
+    public Keyword(final Long id,
+                   final String name,
+                   final String description,
+                   final int seq,
+                   final int importance,
+                   final Long sessionId,
+                   final Keyword parent) {
+        validateOrder(seq);
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.seq = seq;
+        this.importance = importance;
+        this.sessionId = sessionId;
+        this.parent = parent;
+    }
+
+    private void validateOrder(final int order) {
+        if (order <= 0) {
+            throw new KeywordOrderException();
+        }
+    }
+
+    public Keyword(final String name,
+                   final String description,
+                   final int order,
+                   final int importance,
+                   final Long sessionId,
+                   final Keyword parent) {
+        this(null, name, description, order, importance, sessionId, parent);
+    }
+}
