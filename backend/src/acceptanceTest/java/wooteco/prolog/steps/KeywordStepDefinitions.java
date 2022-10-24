d4f04e75ca49954e1008e9171724ab0ee6713616
Wishoon <heonga2@gmail.com>
2022-10-25 00:44:40 +0900

    test: 키워드 단일 조회에 대한 인수테스트 작성

--- backend/src/acceptanceTest/java/wooteco/prolog/steps/KeywordStepDefinitions.java
+++ backend/src/acceptanceTest/java/wooteco/prolog/steps/KeywordStepDefinitions.java
@@ -1,6 +1,7 @@
 package wooteco.prolog.steps;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
 import static wooteco.prolog.fixtures.KeywordAcceptanceFixture.KEYWORD_OF_ROOT;
 
 import io.cucumber.java.en.Given;
@@ -22,6 +23,7 @@ public class KeywordStepDefinitions extends AcceptanceSteps {
         assertThat(context.response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
 
+    @Given("{int}번 세션에 {string}라는 키워드를 순서 {int}, 중요도 {int}로 작성하고")
     @When("{int}번 세션에 {string}라는 키워드를 순서 {int}, 중요도 {int}로 작성하면")
     public void 키워드를_작성하고(int sessionId, String keywordName, int seq, int importance) {
         context.invokeHttpPost(
@@ -29,10 +31,24 @@ public class KeywordStepDefinitions extends AcceptanceSteps {
             KEYWORD_OF_ROOT.getRequest(keywordName, seq, importance));
     }
 
+    @When("{int}번 세션과 {int}번 키워드를 조회하면")
+    public void 키워드를_조회하면(int sessionId, int keywordId) {
+        context.invokeHttpGet(
+            "/sessions/" + sessionId + "/keywords/" + keywordId
+        );
+    }
+
     @Then("키워드가 생성된다")
     public void 키워드가_생성된다() {
         int statusCode = context.response.statusCode();
 
         assertThat(statusCode).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    @Then("키워드가 조회된다")
+    public void 키워드가_조회된다() {
+        int statusCode = context.response.statusCode();
+
+        assertThat(statusCode).isEqualTo(HttpStatus.OK.value());
     }
 }
