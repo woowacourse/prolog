178816f20d605777f4c117f71de0e7c82a262789
Wishoon <heonga2@gmail.com>
2022-10-27 19:23:23 +0900

    feat: update에도 순서에 대한 검증 추가

--- backend/src/main/java/wooteco/prolog/roadmap/Keyword.java
+++ backend/src/main/java/wooteco/prolog/roadmap/Keyword.java
@@ -98,6 +98,7 @@ public class Keyword {
         this.seq = seq;
         this.importance = importance;
         this.parent = keywordParent;
+        validateSeq(seq);
         validateKeywordParent();
     }
 }
