c8ce49e7ff03077b234f7af0fcd2de115a331bd0
seokhongkim <seokhong.kim@woowahan.com>
2023-02-03 14:35:06 +0900

    feature: implement delete EssayAnswer API

--- backend/src/main/java/wooteco/prolog/roadmap/ui/EssayAnswerController.java
+++ backend/src/main/java/wooteco/prolog/roadmap/ui/EssayAnswerController.java
@@ -5,6 +5,7 @@ import static java.util.stream.Collectors.toList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,6 +47,13 @@ public class EssayAnswerController {
         EssayAnswer essayAnswer = essayAnswerService.getById(essayAnswerId);
         EssayAnswerResponse response = EssayAnswerResponse.of(essayAnswer);
         return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/essay-answers/{essayAnswerId}")
+    public ResponseEntity<Void> deleteQuiz(@PathVariable Long essayAnswerId,
+                                           @AuthMemberPrincipal LoginMember member) {
+        essayAnswerService.deleteEssayAnswer(essayAnswerId, member.getId());
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/quizzes/{quizId}")
