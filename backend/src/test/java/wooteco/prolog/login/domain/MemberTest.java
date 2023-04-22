01b531dc3209b07c6e2577bd738f247618e4733f
eunbii0213 <eunbii0213@naver.com>
2023-04-22 20:51:41 +0900

    test: Member 테스트 작성

--- backend/src/test/java/wooteco/prolog/login/domain/MemberTest.java
+++ backend/src/test/java/wooteco/prolog/login/domain/MemberTest.java
@@ -1,14 +1,19 @@
 package wooteco.prolog.login.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.mock;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import wooteco.prolog.member.domain.Member;
+import wooteco.prolog.member.domain.MemberTag;
+import wooteco.prolog.member.domain.MemberTags;
 import wooteco.prolog.member.domain.Role;
+import wooteco.prolog.studylog.domain.Tag;
+import wooteco.prolog.studylog.domain.Tags;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class MemberTest {
 
@@ -37,5 +42,80 @@ class MemberTest {
         assertThat(member1.getNickname()).isEqualTo(loginName);
         assertThat(member.getNickname()).isEqualTo(loginName);
         assertThat(member3.getNickname()).isEqualTo(existName);
+    }
+
+    @Test
+    @DisplayName("updateNickname() : 변경할 닉네임이 빈 값이면 닉네임을 바꾸지 않는다.")
+    void updateNickname() {
+        //given
+        Member member = new Member(1L, "soulg", "judy",
+            Role.CREW, 1234L, "imageUrl");
+
+        //when
+        member.updateNickname("");
+
+        //then
+        Assertions.assertEquals("judy", member.getNickname());
+    }
+
+    @Test
+    @DisplayName("updateImageUrl() : 변경할 이미지가 없다면 이미지를 바꾸지 않는다.")
+    void updateImageUrl() {
+        //given
+        Member member = new Member(1L, "soulg", "judy",
+            Role.CREW, 1234L, "imageUrl");
+
+        //when
+        member.updateImageUrl("");
+
+        //then
+        Assertions.assertEquals("imageUrl", member.getImageUrl());
+    }
+
+    @Test
+    @DisplayName("updateProfileIntro() : 변경할 ProfileIntro가 없다면 ProfileIntro를 바꾸지 않는다.")
+    void updateProfileIntro() {
+        //given
+        Member member = new Member(1L, "soulg", "judy",
+            Role.CREW, 1234L, "imageUrl");
+
+        //when
+        member.updateProfileIntro("");
+
+        //then
+        Assertions.assertNull(member.getProfileIntro());
+    }
+
+    @Test
+    @DisplayName("getMemberTagsWithSort() : 멤버가 가지고 있는 태그의 수로 내림차순 정렬을 할 수 있다.")
+    void test_ifAbsentReplace() {
+        //given
+        Member member = new Member(1L, "a", "member",
+            Role.CREW, 1234L, "imageUrl1");
+
+        Tag tag1 = new Tag(1L, "tag1");
+        Tag tag2 = new Tag(2L, "tag2");
+        Tag tag3 = new Tag(3L, "tag1");
+
+        MemberTag memberTag1 = new MemberTag(member, tag1);
+        MemberTag memberTag2 = new MemberTag(member, tag2);
+        MemberTag memberTag3 = new MemberTag(member, tag3);
+
+        MemberTags memberTags = new MemberTags(List.of(memberTag1, memberTag2, memberTag3));
+
+        //when
+        Member modifiedMember = new Member(1L, "a", "member",
+            Role.CREW, 1234L, "imageUrl1", memberTags);
+
+        List<MemberTag> memberTagsWithSort = modifiedMember.getMemberTagsWithSort();
+
+        for (MemberTag memberTag : memberTagsWithSort) {
+            System.out.println(memberTag.getTag().getName());
+        }
+
+        List<MemberTag> expected = List.of(memberTag2, memberTag1);
+
+        //then
+        Assertions.assertEquals(memberTagsWithSort, expected);
     }
 }
