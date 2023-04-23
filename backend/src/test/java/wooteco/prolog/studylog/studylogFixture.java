c532533edb0ff73a5746f1b42ad8fafd0d8dc5ea
hong-sile <gurwns9325@naver.com>
2023-04-23 14:38:57 +0900

    test: Keyword Test Fixture 추가

---
+++ backend/src/test/java/wooteco/prolog/studylog/studylogFixture.java
@@ -0,0 +1,22 @@
+package wooteco.prolog.studylog;
+
+import wooteco.prolog.member.domain.Member;
+import wooteco.prolog.member.domain.Role;
+import wooteco.prolog.roadmap.domain.Keyword;
+
+public class studylogFixture {
+
+    //Member
+    // to do : null으로 표시한 부분 채우기
+    public static Member TEST_MEMBER_CREW = new Member(1L, "홍혁준", "홍실",
+        Role.CREW, 2L, null, null);
+
+    //Keyword
+    public static final Long SESSION_ID = 3L;
+    public static final Keyword TEST_KEYWORD_JAVA = new Keyword(2L, "자바", "자바에 대한 설명", 1
+        , 5, SESSION_ID, null, null);
+    public static final Keyword TEST_KEYWORD_COLLECTIONS = new Keyword(3L, "컬렉션", "컬렉션 프레임워크에 대한 설명", 1
+        , 5, SESSION_ID, null, null);
+
+    //Session
+}
