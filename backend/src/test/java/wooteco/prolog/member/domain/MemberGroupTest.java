4bcad408e72713b3b43aae3c7186bbf165b321ea
splitube <voicesplit@daum.net>
2023-04-24 14:31:25 +0900

    refactor: MemberGroup 테스트 코드 리팩토링

--- backend/src/test/java/wooteco/prolog/member/domain/MemberGroupTest.java
+++ backend/src/test/java/wooteco/prolog/member/domain/MemberGroupTest.java
@@ -1,42 +1,33 @@
 package wooteco.prolog.member.domain;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 class MemberGroupTest {
 
-    @DisplayName("이름에 백엔드 문자열을 포함할 경우 백엔드 그룹명을 반환한다.")
-    @Test
-    void getGroupNameBackEnd(){
-        MemberGroup memberGroup = new MemberGroup(1L, "5기 백엔드", "5기 백엔드 설명");
-        String groupName = memberGroup.getGroupName();
+    @DisplayName("유효한 그룹명을 조회한다.")
+    @CsvSource(value = {
+        "5기 백엔드:5기 백엔드 설명:백엔드",
+        "5기 프론트엔드:5기 프론트엔드 설명:프론트엔드",
+        "5기 안드로이드:5기 안드로이드 설명:안드로이드",
+    }, delimiter = ':')
+    @ParameterizedTest(name = "이름으로 {0}을 가지고 설명으로 {1}을 가질 때 그룹 명으로 {2}를 반환한다.")
+    void getGroupName(final String name, final String description, final String expected) {
+        final MemberGroup memberGroup = new MemberGroup(1L, name, description);
+        final String groupName = memberGroup.getGroupName();
         assertThat(groupName)
-            .isEqualTo("백엔드");
-    }
-
-    @DisplayName("이름에 프론트엔드 문자열을 포함할 경우 프론트엔드 그룹명을 반환한다.")
-    @Test
-    void getGroupNameFrontEnd(){
-        MemberGroup memberGroup = new MemberGroup(1L, "5기 프론트엔드", "5기 프론트엔드 설명");
-        String groupName = memberGroup.getGroupName();
-        assertThat(groupName)
-            .isEqualTo("프론트엔드");
-    }
-
-    @DisplayName("이름에 안드로이드 문자열을 포함할 경우 안드로이드 그룹명을 반환한다.")
-    @Test
-    void getGroupNameAndroid(){
-        MemberGroup memberGroup = new MemberGroup(1L, "5기 안드로이드", "5기 안드로이드 설명");
-        String groupName = memberGroup.getGroupName();
-        assertThat(groupName)
-            .isEqualTo("안드로이드");
+            .isEqualTo(expected);
     }
 
     @DisplayName("이름에 그룹명을 칭하는 문자열이 없을 경우 null 을 반환한다.")
     @Test
-    void getGroupNameNull(){
-        MemberGroup memberGroup = new MemberGroup(1L, "5기 기타그룹", "5기 기타그룹 설명");
-        String groupName = memberGroup.getGroupName();
+    void getGroupNameNull() {
+        final MemberGroup memberGroup = new MemberGroup(1L, "5기 기타그룹", "5기 기타그룹 설명");
+        final String groupName = memberGroup.getGroupName();
         assertThat(groupName)
             .isNull();
     }
