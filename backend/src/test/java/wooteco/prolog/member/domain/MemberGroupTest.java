b278e37a2be4e0e49e1f147ce41f68d2dea9e810
splitube <voicesplit@daum.net>
2023-04-28 16:46:49 +0900

    style: MemberGroupTest 리뷰 반영
    given-when-then 주석 추가

--- backend/src/test/java/wooteco/prolog/member/domain/MemberGroupTest.java
+++ backend/src/test/java/wooteco/prolog/member/domain/MemberGroupTest.java
@@ -13,12 +13,17 @@ class MemberGroupTest {
     @CsvSource(value = {
         "5기 백엔드:5기 백엔드 설명:백엔드",
         "5기 프론트엔드:5기 프론트엔드 설명:프론트엔드",
-        "5기 안드로이드:5기 안드로이드 설명:안드로이드",
+        "5기 안드로이드:5기 안드로이드 설명:안드로이드"
     }, delimiter = ':')
     @ParameterizedTest(name = "이름으로 {0}을 가지고 설명으로 {1}을 가질 때 그룹 명으로 {2}를 반환한다.")
     void getGroupName(final String name, final String description, final String expected) {
+        //given
         final MemberGroup memberGroup = new MemberGroup(1L, name, description);
+
+        //when
         final String groupName = memberGroup.getGroupName();
+
+        //then
         assertThat(groupName)
             .isEqualTo(expected);
     }
@@ -26,8 +31,13 @@ class MemberGroupTest {
     @DisplayName("이름에 그룹명을 칭하는 문자열이 없을 경우 null 을 반환한다.")
     @Test
     void getGroupNameNull() {
+        //given
         final MemberGroup memberGroup = new MemberGroup(1L, "5기 기타그룹", "5기 기타그룹 설명");
+
+        //when
         final String groupName = memberGroup.getGroupName();
+
+        //then
         assertThat(groupName)
             .isNull();
     }
