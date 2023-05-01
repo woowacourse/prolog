e28df4cc22ed1c4ecb4574e8382aeddcafa3f9bd
hong-sile <gurwns9325@naver.com>
2023-05-01 15:29:26 +0900

    test(MissionTest): 컨벤션에 맞게 메서드 네이밍과 displayname 변경, given-when-then 주석 적용

--- backend/src/test/java/wooteco/prolog/session/domain/MissionTest.java
+++ backend/src/test/java/wooteco/prolog/session/domain/MissionTest.java
@@ -1,40 +1,27 @@
 package wooteco.prolog.session.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static wooteco.prolog.studylog.StudylogFixture.TEST_SESSION;
 
 import com.google.common.base.Supplier;
-import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import wooteco.prolog.studylog.exception.TooLongMissionNameException;
 
 class MissionTest {
 
-    @Nested
-    class 미션을_초기화하는_기능 {
+    private static final Session TEST_SESSION = new Session(4L, 5L, "세션");
 
-        @Test
-        void 미션의_이름이_45자보다_길면_Exception발생() {
-            final StringBuilder sb = new StringBuilder();
-            final String character = ".";
-            for (int i = 0; i < 46; i++) {
-                sb.append(character);
-            }
-
-            final Supplier<Mission> initialMission = () -> new Mission(sb.toString(), TEST_SESSION);
-
-            assertThatThrownBy(initialMission::get).isInstanceOf(TooLongMissionNameException.class);
+    @DisplayName("미션의 이름이 45자보다 길면 Exception 발생")
+    @Test
+    void validateMaxlengthTest() {
+        //given
+        final StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 46; i++) {
+            sb.append(".");
         }
+        final Supplier<Mission> initialMission = () -> new Mission(sb.toString(), TEST_SESSION);
 
-        @Test
-        void 정상적으로_Mission을_초기화하는_기능_테스트() {
-            final Mission mission = new Mission(5L, "자바", TEST_SESSION);
-
-            assertAll(() -> assertThat(mission.getId()).isEqualTo(5L),
-                () -> assertThat(mission.getName()).isEqualTo("자바"),
-                () -> assertThat(mission.getSession().getId()).isEqualTo(TEST_SESSION.getId()));
-        }
+        //when, then
+        assertThatThrownBy(initialMission::get).isInstanceOf(TooLongMissionNameException.class);
     }
 }
