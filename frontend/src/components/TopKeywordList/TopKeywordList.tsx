95b1018737ca6cf0302820d5349cbbe9ede89ade
Dain Lee <feb.dain@gmail.com>
2023-05-02 16:45:51 +0900

    design: 로드맵 > 상위 키워드 선택 버튼 내용이 두 줄로 넘어가는 디자인 수정
    Co-authored-by: solo5star <solo_5@naver.com>

--- frontend/src/components/TopKeywordList/TopKeywordList.tsx
+++ frontend/src/components/TopKeywordList/TopKeywordList.tsx
@@ -6,64 +6,68 @@ import { KeywordResponse } from '../../models/Keywords';
 import ResponsiveButton from '../Button/ResponsiveButton';
 
 interface TopKeywordListProps {
-  sessionId: number;
-  selectedTopKeyword: KeywordResponse | null;
-  handleClickTopKeyword: (keyword: KeywordResponse) => void;
+    sessionId: number;
+    selectedTopKeyword: KeywordResponse | null;
+    handleClickTopKeyword: (keyword: KeywordResponse) => void;
 }
 
 const compareFn = (a: KeywordResponse, b: KeywordResponse) => a.order - b.order;
 
 const TopKeywordList = ({
-  sessionId,
-  selectedTopKeyword,
-  handleClickTopKeyword,
+    sessionId,
+    selectedTopKeyword,
+    handleClickTopKeyword,
 }: TopKeywordListProps) => {
-  const { topKeywordList } = useGetTopKeywordList(sessionId);
+    const { topKeywordList } = useGetTopKeywordList(sessionId);
 
-  useEffect(() => {
-    if (topKeywordList) {
-      handleClickTopKeyword(topKeywordList[0]);
-    }
-  }, [topKeywordList]);
+    useEffect(() => {
+        if (topKeywordList) {
+            handleClickTopKeyword(topKeywordList[0]);
+        }
+    }, [topKeywordList]);
 
-  return (
-    <StyledRoot>
-      {topKeywordList?.sort(compareFn).map((keyword, index) => {
-        const isNotLast = index + 1 !== topKeywordList?.length;
-        const isSelected = selectedTopKeyword?.keywordId === keyword.keywordId;
+    return (
+        <StyledRoot>
+            {topKeywordList?.sort(compareFn).map((keyword, index) => {
+                const isNotFirst = index > 0;
+                const isSelected = selectedTopKeyword?.keywordId === keyword.keywordId;
 
-        return (
-          <StyledWrapper key={keyword.keywordId}>
-            <ResponsiveButton
-              text={keyword.name}
-              backgroundColor={isSelected ? COLOR.LIGHT_BLUE_900 : COLOR.LIGHT_GRAY_400}
-              onClick={() => handleClickTopKeyword(keyword)}
-            ></ResponsiveButton>
-            {isNotLast && <StyledArrow />}
-          </StyledWrapper>
-        );
-      })}
-    </StyledRoot>
-  );
+                return (
+                    <StyledWrapper key={keyword.keywordId}>
+                        {isNotFirst && <StyledArrow />}
+                        <ResponsiveButton
+                            text={keyword.name}
+                            backgroundColor={
+                                isSelected ? COLOR.LIGHT_BLUE_900 : COLOR.LIGHT_GRAY_400
+                            }
+                            onClick={() => handleClickTopKeyword(keyword)}
+                        />
+                    </StyledWrapper>
+                );
+            })}
+        </StyledRoot>
+    );
 };
 
 export default TopKeywordList;
 
 const StyledRoot = styled.div`
-  display: flex;
+    display: flex;
+    flex-wrap: wrap;
+    row-gap: 12px;
 `;
 
 const StyledArrow = styled.div`
-  width: 0;
-  height: 0;
-  border: 9px solid transparent;
-  border-top: 0;
-  border-bottom: 15px solid ${COLOR.LIGHT_BLUE_700};
-  transform: rotate(90deg);
-  margin: 0 12px;
+    width: 0;
+    height: 0;
+    border: 9px solid transparent;
+    border-top: 0;
+    border-bottom: 15px solid ${COLOR.LIGHT_BLUE_700};
+    transform: rotate(90deg);
+    margin: 0 12px;
 `;
 
 const StyledWrapper = styled.div`
-  display: flex;
-  align-items: center;
+    display: flex;
+    align-items: center;
 `;
