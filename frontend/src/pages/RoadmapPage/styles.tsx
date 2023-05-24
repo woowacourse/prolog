b180f533cb295152d3668440655b7a1d6d9d7afa
Patrick <kuyt3122@naver.com>
2023-05-24 16:56:45 +0900

    design: 로드맵에 양쪽 간격을 추가한다 #1315

--- frontend/src/pages/RoadmapPage/styles.tsx
+++ frontend/src/pages/RoadmapPage/styles.tsx
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 export const Root = styled.div`
   display: flex;
   justify-content: center;
-  margin-top: 25px;
+  margin: 25px 20px;
 `;
 
 export const Main = styled.main`
