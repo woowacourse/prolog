06262957de9d9b1e1841aa4c7b0e80775c9fe16b
solo5star <solo_5@naver.com>
2023-06-02 08:39:56 +0000

    docs: mediaQuery JSDoc 작성
    #1333

--- frontend/src/utils/mediaQuery.ts
+++ frontend/src/utils/mediaQuery.ts
@@ -1,5 +1,25 @@
 import SCREEN_BREAKPOINT, { ScreenBreakpoint } from '../constants/screenBreakpoints';
 
+/**
+ * css에서 반응형 디자인을 할 때 사용합니다.
+ *
+ * @example
+ * css`
+ *   font-size: 24px;
+ *   display: grid;
+ *   grid-template-columns: repeat(6, 1fr);
+ *
+ *   ${mediaQuery.md} {
+ *     font-size: 20px;
+ *     grid-template-columns: repeat(4, 1fr);
+ *   }
+ *
+ *   ${mediaQuery.sm} {
+ *     font-size: 16px;
+ *     grid-template-columns: repeat(2, 1fr);
+ *   }
+ * `
+ */
 const mediaQuery = {
   ...(Object.fromEntries(
     Object.entries(SCREEN_BREAKPOINT).map(([breakpoint, maxWidth]) => [
