f40825477d07991f05103c1c8dab8e7ea75912c5
Dain Lee <feb.dain@gmail.com>
2023-06-16 17:48:21 +0900

    refactor: 객체 대신 `Map`으로 상태 관리
     #1333

--- frontend/src/hooks/useScreenMediaQuery.ts
+++ frontend/src/hooks/useScreenMediaQuery.ts
@@ -1,7 +1,8 @@
 import { useEffect, useMemo, useState } from 'react';
 import SCREEN_BREAKPOINT, { ScreenBreakpoint } from '../constants/screenBreakpoints';
 
-type Screen = Record<`is${Capitalize<ScreenBreakpoint>}`, boolean>;
+type ScreenKey = `is${Capitalize<ScreenBreakpoint>}`;
+type Screen = Map<ScreenKey, MediaQueryList>;
 
 /**
  * React 컴포넌트에서 스크린 사이즈에 따라 실행해야 할 코드가 다를 때
@@ -16,28 +17,25 @@ type Screen = Record<`is${Capitalize<ScreenBreakpoint>}`, boolean>;
  *   // ...
  * }
  */
-const useScreenMediaQuery = (): Screen => {
-  const mediaQueries = useMemo<Record<`is${Capitalize<ScreenBreakpoint>}`, MediaQueryList>>(
-    () => ({
-      isXs: window.matchMedia(`(max-width: ${SCREEN_BREAKPOINT.xs}px)`),
-      isSm: window.matchMedia(`(max-width: ${SCREEN_BREAKPOINT.sm}px)`),
-      isMd: window.matchMedia(`(max-width: ${SCREEN_BREAKPOINT.md}px)`),
-      isLg: window.matchMedia(`(max-width: ${SCREEN_BREAKPOINT.lg}px)`),
-      isXl: window.matchMedia(`(max-width: ${SCREEN_BREAKPOINT.xl}px)`),
-    }),
-    []
-  );
+const useScreenMediaQuery = () => {
+  const mediaQueries = useMemo<Screen>(() => {
+    const addIsPrefix = (name: string) => `is${name[0].toUpperCase() + name.slice(1)}`;
 
-  const [screen, setScreen] = useState<Screen>(
-    Object.fromEntries(
-      Object.entries(mediaQueries).map(([name, mediaQuery]) => [name, mediaQuery.matches])
-    ) as Screen
+    return new Map(
+      Object.entries(SCREEN_BREAKPOINT).map(([name, breakpoint]) => {
+        return [addIsPrefix(name) as ScreenKey, matchMedia(`(max-width: ${breakpoint}px)`)];
+      })
+    );
+  }, []);
+
+  const [screen, setScreen] = useState(
+    new Map([...mediaQueries].map(([name, mediaQuery]) => [name, mediaQuery.matches]))
   );
 
   useEffect(() => {
-    const cleanHandlers = Object.entries(mediaQueries).map(([name, mediaQuery]) => {
+    const cleanHandlers = [...mediaQueries].map(([name, mediaQuery]) => {
       const handleMediaQueryChange = ({ matches }: MediaQueryListEvent) => {
-        setScreen((screen) => ({ ...screen, [name]: matches }));
+        setScreen((screen) => new Map([...screen]).set(name, matches));
       };
 
       mediaQuery.addEventListener('change', handleMediaQueryChange);
@@ -48,7 +46,7 @@ const useScreenMediaQuery = (): Screen => {
     return () => cleanHandlers.forEach((cleanHandler) => cleanHandler());
   }, []);
 
-  return screen;
+  return Object.fromEntries(screen);
 };
 
 export default useScreenMediaQuery;
