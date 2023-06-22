fc3cd775debbb74c122d8844576c0f30fb6613b3
WaiNaat <qoli0703@gmail.com>
2023-06-22 13:59:13 +0900

    fix: 메인에 이미 반영된 충돌 코드 수정
    [학습로그] 반응형 디자인 깨짐 현상 수정 #1269

--- frontend/src/components/FilterList/FilterList.styles.js
+++ frontend/src/components/FilterList/FilterList.styles.js
@@ -1,6 +1,7 @@
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import COLOR from '../../constants/color';
+import MEDIA_QUERY from '../../constants/mediaQuery';
 
 const DropdownToggledStyle = css`
   &:before {
@@ -34,12 +35,12 @@ const Container = styled.div`
   & > div:not(:last-child) {
     margin-right: 3.2rem;
 
-    @media screen and (max-width: 420px) {
+    ${MEDIA_QUERY.xs} {
       margin-right: 2rem;
     }
   }
 
-  @media screen and (max-width: 450px) {
+  ${MEDIA_QUERY.xs} {
     font-size: 1rem;
   }
 
@@ -89,40 +90,40 @@ const Container = styled.div`
 `;
 
 const FilterContainer = styled.div`
-display: flex;
-
-& > div:not(:last-child) {
-  margin-right: 3.2rem;
-
-  @media screen and (max-width: 420px) {
-    margin-right: 2rem;
-  }
-}
-
-@media screen and (max-width: 620px) {
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-}
-
-& button {
   display: flex;
-  align-items: center;
-  height: 100%;
-  text-align: center;
-  color: ${COLOR.DARK_GRAY_500};
 
-  ::after {
-    content: '';
-    width: 0;
-    height: 0;
-    transform: translateY(50%);
-    margin-left: 0.2rem;
-    border-top: 0.5rem solid ${COLOR.DARK_GRAY_500};
-    border-bottom: 0.5rem solid transparent;
-    border-left: 0.5rem solid transparent;
-    border-right: 0.5rem solid transparent;
+  & > div:not(:last-child) {
+    margin-right: 3.2rem;
+
+    ${MEDIA_QUERY.xs} {
+      margin-right: 2rem;
+    }
   }
-}
+
+  ${MEDIA_QUERY.sm} {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  & button {
+    display: flex;
+    align-items: center;
+    height: 100%;
+    text-align: center;
+    color: ${COLOR.DARK_GRAY_500};
+
+    ::after {
+      content: '';
+      width: 0;
+      height: 0;
+      transform: translateY(50%);
+      margin-left: 0.2rem;
+      border-top: 0.5rem solid ${COLOR.DARK_GRAY_500};
+      border-bottom: 0.5rem solid transparent;
+      border-left: 0.5rem solid transparent;
+      border-right: 0.5rem solid transparent;
+    }
+  }
 `;
 
 const FilterDetail = styled.button`
@@ -201,21 +202,21 @@ export const NoContent = styled.div`
 `;
 
 const SessionInMission = styled.div`
-    background-color: aliceblue;
-    border-radius: 1rem;
-    padding-left: 0.3rem;
-    padding-right: 0.3rem;
-    font-size: xx-small;
+  background-color: aliceblue;
+  border-radius: 1rem;
+  padding-left: 0.3rem;
+  padding-right: 0.3rem;
+  font-size: xx-small;
 `;
 
 const MissionName = styled.div`
-    margin-top: 0.2rem;
-    margin-left: 0.5rem;
+  margin-top: 0.2rem;
+  margin-left: 0.5rem;
 `;
 
 const MissionWrapper = styled.div`
-    margin-top: 0.2rem;
-    margin-left: 0.5rem;
+  margin-top: 0.2rem;
+  margin-left: 0.5rem;
 `;
 
 export {
