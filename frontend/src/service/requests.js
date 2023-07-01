8cbc3277e5707a7e428d0d4ba46783f58eaa8acd
hae-on <solbi2004@naver.com>
2023-07-01 13:21:48 +0900

    refactor: base url 삭제

--- frontend/src/service/requests.js
+++ frontend/src/service/requests.js
@@ -1,4 +1,3 @@
-import { BASE_URL } from '../configs/environment';
 import { createAxiosInstance } from '../utils/axiosInstance';
 
 const instanceWithoutToken = createAxiosInstance();
@@ -11,7 +10,7 @@ export const requestGetStudylog = ({ id, accessToken }) =>
 /* @deprecated 의존성 완전 삭제 이후 코드 삭제*/
 export const requestGetStudylogs = ({ query, accessToken }) => {
   if (query.type === 'searchParams') {
-    return instanceWithToken(accessToken).get(`${BASE_URL}/studylogs`, {
+    return instanceWithToken(accessToken).get('/studylogs', {
       params: new URLSearchParams(query.data.toString()),
     });
   }
@@ -25,24 +24,24 @@ export const requestGetStudylogs = ({ query, accessToken }) => {
           ({ filterType, filterDetailId }) => `${filterType}=${filterDetailId}`
         )
       : '';
-    return instanceWithToken(accessToken).get(`${BASE_URL}/studylogs`, {
+    return instanceWithToken(accessToken).get('/studylogs', {
       params: new URLSearchParams([...filterQuery, ...searchParams].join('&')),
     });
   }
 
-  return instanceWithToken(accessToken).get(`${BASE_URL}/studylogs`);
+  return instanceWithToken(accessToken).get('/studylogs');
 };
 
 /* @deprecated 의존성 완전 삭제 이후 코드 삭제*/
 export const requestPostStudylog = ({ accessToken, data }) =>
-  instanceWithToken(accessToken).post(`${BASE_URL}/studylogs`, data);
+  instanceWithToken(accessToken).post('/studylogs', data);
 
 /* @deprecated 의존성 완전 삭제 이후 코드 삭제*/
 export const requestEditStudylog = ({ id, data, accessToken }) =>
-  instanceWithToken(accessToken).put(`${BASE_URL}/studylogs/${id}`, data);
+  instanceWithToken(accessToken).put(`/studylogs/${id}`, data);
 
 export const requestDeleteStudylog = ({ id, accessToken }) =>
-  instanceWithToken(accessToken).delete(`${BASE_URL}/studylogs/${id}`);
+  instanceWithToken(accessToken).delete(`/studylogs/${id}`);
 
 export const requestGetUserStudylogs = (username, postSearchParams, filteringOption) => {
   const searchParams = Object.entries(postSearchParams).map(([key, value]) => `${key}=${value}`);
@@ -51,81 +50,77 @@ export const requestGetUserStudylogs = (username, postSearchParams, filteringOpt
     : '';
 
   return instanceWithoutToken.get(
-    `${BASE_URL}/members/${username}/studylogs?${[...filterQuery, ...searchParams].join('&')}`
+    `/members/${username}/studylogs?${[...filterQuery, ...searchParams].join('&')}`
   );
 };
 
 export const requestGetCalendar = (year, month, username) =>
-  instanceWithoutToken.get(
-    `${BASE_URL}/members/${username}/calendar-studylogs?year=${year}&month=${month}`
-  );
+  instanceWithoutToken.get(`/members/${username}/calendar-studylogs?year=${year}&month=${month}`);
 
 export const requestGetMyScrap = ({ username, accessToken, postQueryParams }) => {
   const searchParams = Object.entries(postQueryParams).map(([key, value]) => `${key}=${value}`);
 
   return instanceWithToken(accessToken).get(
-    `${BASE_URL}/members/${username}/scrap?${[...searchParams].join('&')}`
+    `/members/${username}/scrap?${[...searchParams].join('&')}`
   );
 };
 
 /* Filter 관련 request */
 /* @deprecated 의존성 완전 삭제 이후 코드 삭제*/
-export const requestGetFilters = () => instanceWithoutToken.get(`${BASE_URL}/filters`);
+export const requestGetFilters = () => instanceWithoutToken.get('/filters');
 
 /* @deprecated 의존성 완전 삭제 이후 코드 삭제*/
-export const requestGetMissions = () => instanceWithoutToken.get(`${BASE_URL}/missions`);
+export const requestGetMissions = () => instanceWithoutToken.get('/missions');
 
 /* @deprecated 의존성 완전 삭제 이후 코드 삭제*/
-export const requestGetTags = () => instanceWithoutToken.get(`${BASE_URL}/tags`);
+export const requestGetTags = () => instanceWithoutToken.get('/tags');
 
 export const requestGetUserTags = (username) =>
-  instanceWithoutToken.get(`${BASE_URL}/members/${username}/tags`);
+  instanceWithoutToken.get(`/members/${username}/tags`);
 
 /* User 관련 request */
-export const loginRequest = ({ code }) =>
-  instanceWithoutToken.post(`${BASE_URL}/login/token`, { code });
+export const loginRequest = ({ code }) => instanceWithoutToken.post('/login/token', { code });
 
 export const getUserProfileRequest = ({ accessToken }) =>
-  instanceWithToken(accessToken).get(`${BASE_URL}/members/me`);
+  instanceWithToken(accessToken).get('/members/me');
 
 export const requestGetProfile = (username) =>
-  instanceWithoutToken.get(`${BASE_URL}/members/${username}/profile`);
+  instanceWithoutToken.get(`/members/${username}/profile`);
 
 export const requestEditProfile = (data, accessToken) =>
-  instanceWithToken(accessToken).put(`${BASE_URL}/members/${data.username}`, data);
+  instanceWithToken(accessToken).put(`/members/${data.username}`, data);
 
 export const requestGetProfileIntroduction = (username) =>
-  instanceWithoutToken.get(`${BASE_URL}/members/${username}/profile-intro`);
+  instanceWithoutToken.get(`/members/${username}/profile-intro`);
 
 export const requestEditProfileIntroduction = (username, data, accessToken) =>
-  instanceWithToken(accessToken).put(`${BASE_URL}/members/${username}/profile-intro`, data);
+  instanceWithToken(accessToken).put(`/members/${username}/profile-intro`, data);
 
 /* 사용자 리액션 관련 requests */
 export const requestPostScrap = ({ username, accessToken, id: studylogId }) =>
-  instanceWithToken(accessToken).post(`${BASE_URL}/members/${username}/scrap`, {
+  instanceWithToken(accessToken).post(`/members/${username}/scrap`, {
     studylogId,
   });
 
 export const requestDeleteScrap = ({ username, accessToken, id }) =>
-  instanceWithToken(accessToken).delete(`${BASE_URL}/members/${username}/scrap`, {
+  instanceWithToken(accessToken).delete(`/members/${username}/scrap`, {
     params: {
       studylog: id,
     },
   });
 
 export const requestPostLike = ({ accessToken, id }) =>
-  instanceWithToken(accessToken).post(`${BASE_URL}/studylogs/${id}/likes`);
+  instanceWithToken(accessToken).post(`/studylogs/${id}/likes`);
 
 export const requestDeleteLike = ({ accessToken, id }) =>
-  instanceWithToken(accessToken).delete(`${BASE_URL}/studylogs/${id}/likes`);
+  instanceWithToken(accessToken).delete(`/studylogs/${id}/likes`);
 
 export const requestGetMatchedStudylogs = ({ accessToken, startDate, endDate }) =>
-  instanceWithToken(accessToken).get(`${BASE_URL}/studylogs/me`, {
+  instanceWithToken(accessToken).get('/studylogs/me', {
     params: {
       startDate: startDate,
       endDate: endDate,
     },
   });
 
-export const requestImageUpload = (formData) =>
-  instanceWithoutToken.post(`${BASE_URL}/images`, formData);
+export const requestImageUpload = (formData) => instanceWithoutToken.post('/images', formData);
