5f247442d14e8031f561c1af24e2664cf912fa5e
Q Kim <qoli0703@gmail.com>
2023-07-05 20:17:47 +0900

    fix: 빌드 오류 수정
    이전 pr과의 충돌 해결 과정에서 발생한 문제를 해결

--- frontend/src/apis/essayanswers.ts
+++ frontend/src/apis/essayanswers.ts
@@ -1,39 +1,44 @@
-import { client } from '.';
 import { EssayAnswerRequest, EssayEditRequest } from "../models/EssayAnswers";
 import { AxiosPromise, AxiosResponse } from "axios";
-import { httpRequester } from "./studylogs";
 import { Quiz } from "../models/Keywords";
+import { createAxiosInstance } from "../utils/axiosInstance";
 
-const instanceWithoutToken = createAxiosInstance();
+import LOCAL_STORAGE_KEY from "../constants/localStorage";
 
-export const createNewEssayAnswerRequest = (body: EssayAnswerRequest) =>
-  instanceWithoutToken.post(`/essay-answers`, body);
+const anyoneInstance = createAxiosInstance();
+const loggedInUserInstance = createAxiosInstance({
+  accessToken: localStorage.getItem(LOCAL_STORAGE_KEY.ACCESS_TOKEN) ?? '',
+});
+
+export const createNewEssayAnswerRequest = (body: EssayAnswerRequest) => (
+  loggedInUserInstance.post(`/essay-answers`, body)
+);
 
 export const requestGetEssayAnswer = async (essayAnswerId) => {
-  const { data } = await instanceWithoutToken.get(`/essay-answers/${essayAnswerId}`);
-
-  return data;
+  const { data } = await anyoneInstance.get(`/essay-answers/${essayAnswerId}`);
+  return data
 };
 
 export const requestEditEssayAnswer = async (essayAnswerId: number, body: EssayEditRequest) => {
-  await client.patch(`/essay-answers/${essayAnswerId}`, body);
-}
+  await loggedInUserInstance.patch(`/essay-answers/${essayAnswerId}`, body);
+};
 
 export const requestDeleteEssayAnswer = async (essayAnswerId) => {
-  await instanceWithoutToken.delete(`/essay-answers/${essayAnswerId}`);
+  await loggedInUserInstance.delete(`/essay-answers/${essayAnswerId}`);
 };
 
 export const requestGetEssayAnswerList = async (quizId) => {
-  const { data } = await instanceWithoutToken.get(`/quizzes/${quizId}/essay-answers`);
+  const { data } = await anyoneInstance.get(`/quizzes/${quizId}/essay-answers`);
 
   return data;
 };
 
 export const requestGetQuizAsync = async (quizId) => {
-  const { data } = await instanceWithoutToken.get(`/quizzes/${quizId}`);
+  const { data } = await anyoneInstance.get(`/quizzes/${quizId}`);
 
   return data;
 };
 
-export const requestGetQuiz = (quizId: Number): AxiosPromise<AxiosResponse<Quiz>> =>
-  instanceWithoutToken.get<AxiosResponse<Quiz>>(`/quizzes/${quizId}`);
+export const requestGetQuiz = (quizId: Number): AxiosPromise<AxiosResponse<Quiz>> => (
+  anyoneInstance.get<AxiosResponse<Quiz>>(`/quizzes/${quizId}`)
+);
