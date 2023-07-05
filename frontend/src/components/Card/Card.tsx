8b8b3a1cf0e1d803d38cf228eeb863a3b1dafd4d
Dain Lee <feb.dain@gmail.com>
2023-07-05 17:48:26 +0900

    refactor: ts로 마이그레이션

---
+++ frontend/src/components/Card/Card.tsx
@@ -0,0 +1,25 @@
+// import PropTypes from 'prop-types';
+import { SerializedStyles } from '@emotion/react';
+import { Container, Title } from './Card.styles';
+
+interface Props {
+  children: JSX.Element;
+  title?: string;
+  size: string;
+  cssProps?: SerializedStyles;
+  css?: SerializedStyles ;
+  onClick?: () => void;
+}
+
+const Card = ({ title, children, size, cssProps, css, onClick }: Props) => {
+  return (
+    <div>
+      {title && <Title>{title} </Title>}
+      <Container size={size} css={cssProps || css} onClick={onClick}>
+        {children}
+      </Container>
+    </div>
+  );
+};
+
+export default Card;
