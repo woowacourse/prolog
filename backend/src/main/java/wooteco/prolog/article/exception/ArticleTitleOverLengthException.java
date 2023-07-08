11af95f3812a2136a690d03788afbc1275c6b6c6
hong-sile <gurwns9325@naver.com>
2023-07-08 15:59:19 +0900

    feat: 기본적인 Article CRUD 로직 작성

---
+++ backend/src/main/java/wooteco/prolog/article/exception/ArticleTitleOverLengthException.java
@@ -0,0 +1,8 @@
+package wooteco.prolog.article.exception;
+
+import wooteco.prolog.common.exception.BadRequestException;
+
+public class ArticleTitleOverLengthException extends
+    BadRequestException {
+
+}
---
+++ backend/src/main/java/wooteco/prolog/article/ui/ArticleRequest.java
@@ -0,0 +1,20 @@
+package wooteco.prolog.article.ui;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import wooteco.prolog.article.domain.Article;
+import wooteco.prolog.article.domain.Title;
+import wooteco.prolog.article.domain.Url;
+import wooteco.prolog.member.domain.Member;
+
+@Getter
+@AllArgsConstructor
+public class ArticleRequest {
+
+    private final String title;
+    private final String url;
+
+    public Article toArticle(final Member member) {
+        return new Article(member, new Title(title), new Url(url));
+    }
+}
