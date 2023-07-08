32a9f296b9d300ddb5687de282a68a76f3c3cec0
hong-sile <gurwns9325@naver.com>
2023-07-08 15:59:19 +0900

    feat: 기본적인 Article CRUD 로직 작성

---
+++ backend/src/main/java/wooteco/prolog/article/exception/ArticleUrlOverLengthException.java
@@ -0,0 +1,7 @@
+package wooteco.prolog.article.exception;
+
+import wooteco.prolog.common.exception.BadRequestException;
+
+public class ArticleUrlOverLengthException extends BadRequestException {
+
+}
