99e8a1115de6f2d7d7a8b6a29379234892037a60
donghae-kim <donghae1999@gmail.com>
2023-07-10 12:58:56 +0900

    refactor: Controller RequestBody 수정

--- backend/src/main/java/wooteco/prolog/article/ui/ArticleController.java
+++ backend/src/main/java/wooteco/prolog/article/ui/ArticleController.java
@@ -4,13 +4,7 @@ import java.net.URI;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import wooteco.prolog.article.domain.ArticleService;
 import wooteco.prolog.login.domain.AuthMemberPrincipal;
 import wooteco.prolog.login.ui.LoginMember;
@@ -23,7 +17,7 @@ public class ArticleController {
     private final ArticleService articleService;
 
     @PostMapping
-    public ResponseEntity<Void> createArticles(final ArticleRequest articleRequest,
+    public ResponseEntity<Void> createArticles(@RequestBody final ArticleRequest articleRequest,
                                                @AuthMemberPrincipal final LoginMember member) {
         final Long id = articleService.create(articleRequest, member);
         return ResponseEntity.created(URI.create("/articles/" + id)).build();
@@ -36,7 +30,7 @@ public class ArticleController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Void> updateArticle(final ArticleRequest articleRequest,
+    public ResponseEntity<Void> updateArticle(@RequestBody final ArticleRequest articleRequest,
                                               @AuthMemberPrincipal final LoginMember member,
                                               @PathVariable final Long id) {
         articleService.update(id, articleRequest, member);
