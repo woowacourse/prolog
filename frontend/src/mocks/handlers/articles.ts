8196f8a43efa433c3c68e340f6ecfe51325b9ab0
hae-on <solbi2004@naver.com>
2023-07-12 17:35:36 +0900

    feat: msw articles api 구현

---
+++ frontend/src/mocks/handlers/articles.ts
@@ -0,0 +1,31 @@
+import { rest } from 'msw';
+import { BASE_URL } from '../../configs/environment';
+import articles from '../db/articles.json';
+
+// 추후 articles type models에 정의 후 삭제
+export interface Articles {
+  id: number;
+  userName: string;
+  title: string;
+  url: string;
+  createdAt: string;
+}
+
+export const articlesHandler = [
+  rest.get(`${BASE_URL}/articles`, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(articles));
+  }),
+
+  rest.post<Articles[]>(`${BASE_URL}/articles`, (req, res, ctx) => {
+    const newArticle: Articles = {
+      id: articles.length + 1,
+      userName: '도밥',
+      title: '직렬화, 역직렬화는 무엇일까?',
+      url: 'https://think0wise.tistory.com/107',
+      createdAt: new Date().toISOString(),
+    };
+
+    articles.push(newArticle);
+    return res(ctx.status(200), ctx.json(newArticle));
+  }),
+];
