a91ba520623748b6f448dcf58a226d8fc99a04df
splitube <voicesplit@daum.net>
2023-07-12 17:18:27 +0900

    fix: DB 테이블 schema 수정

--- backend/src/main/resources/db/migration/prod/V1.2__create_initial_scheme_squashed.sql
+++ backend/src/main/resources/db/migration/prod/V1.2__create_initial_scheme_squashed.sql
@@ -1,9 +1,10 @@
-create table if not exists prolog.articles
+create table if not exists prolog.article
 (
     id        bigint auto_increment primary key,
     member_id bigint        not null,
     title     varchar(50)   not null,
     url       varchar(1024) not null,
+    created_at datetime(6) not null,
     foreign key (member_id) references prolog.member (id)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
