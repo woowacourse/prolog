9aabf5eb2de0482987ced81a76e38c78b089dd3d
hae-on <solbi2004@naver.com>
2023-07-12 17:36:15 +0900

    feat: articles custom hook 구현

---
+++ frontend/src/hooks/Articles/useArticles.ts
@@ -0,0 +1,28 @@
+import { useContext, useEffect, useState } from 'react';
+import { requestGetArticles, requestPostArticles } from '../../apis/articles';
+import { UserContext } from '../../contexts/UserProvider';
+
+export const useFetchArticles = async () => {
+  const { user } = useContext(UserContext);
+  const { accessToken } = user;
+
+  const [articles, setArticles] = useState();
+
+  const fetchArticles = async () => {
+    const response = await requestGetArticles({ accessToken });
+    setArticles(response.data);
+  };
+
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
+  return { articles };
+};
+
+export const usePostArticles = (data) => {
+  const { user } = useContext(UserContext);
+  const { accessToken } = user;
+
+  requestPostArticles({ accessToken, data });
+};
