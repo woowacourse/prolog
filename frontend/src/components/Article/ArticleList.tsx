96051d07c2ad1744de9bca346a34f3a56ae5f998
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-07-13 21:07:52 +0900

    refactor: 아티클 더미 목록을 api hook으로 개선

--- frontend/src/components/Article/ArticleList.tsx
+++ frontend/src/components/Article/ArticleList.tsx
@@ -1,30 +1,13 @@
 import * as Styled from './ArticleList.style';
 import Article from './Article';
-import type { ArticleType } from '../../models/Article';
-
-const mockArticle: ArticleType = {
-  id: 1,
-  userName: '패트릭',
-  title: 'JS Prototype',
-  url: 'https://poiemaweb.com/js-prototype',
-  createdAt: '2023-07-10',
-};
-
-const mockArticleList = [
-  mockArticle,
-  mockArticle,
-  mockArticle,
-  mockArticle,
-  mockArticle,
-  mockArticle,
-  mockArticle,
-  mockArticle,
-];
+import { useFetchArticles } from '../../hooks/Articles/useArticles';
 
 const ArticleList = () => {
+  const { articles } = useFetchArticles();
+
   return (
     <Styled.Container>
-      {mockArticleList.map((article) => (
+      {articles.map((article) => (
         <Article key={article.id} {...article} />
       ))}
     </Styled.Container>
