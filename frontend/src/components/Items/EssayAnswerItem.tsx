630013261b1d13c7497f42c826047358e78f8dcd
solo5star <solo_5@naver.com>
2023-07-19 08:52:31 +0000

    refactor: 로드맵 퀴즈 답변 페이지 이름 변경
    Co-authored-by: WaiNaat <qoli0703@gmail.com>

--- frontend/src/components/Items/EssayAnswerItem.tsx
+++ frontend/src/components/Items/EssayAnswerItem.tsx
@@ -10,13 +10,20 @@ import {
 import { AlignItemsEndStyle, FlexColumnStyle, FlexStyle } from '../../styles/flex.styles';
 import Card from '../Card/Card';
 import ProfileChip from '../ProfileChip/ProfileChip';
+import { EssayAnswerResponse } from '../../models/EssayAnswers';
 
-const EssayAnswerItem = ({ essayAnswer }) => {
-  const { author, answer } = essayAnswer;
+type EssayAnswerItemProps = (
+  Pick<EssayAnswerResponse, "author" | "answer">
+  & { title?: string; showTitle?: boolean }
+);
+
+const EssayAnswerItem = (props: EssayAnswerItemProps) => {
+  const { author, answer, title, showTitle } = props;
 
   return (
     <Card size="EXTRA_SMALL" cssProps={CardStyle}>
       <div css={ContentStyle}>
+        { showTitle && <h3>{title}</h3> }
         <div css={[ContentsAreaStyle]}>
               <div>{answer}</div>
         </div>
