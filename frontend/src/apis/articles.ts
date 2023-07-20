b3b7f1bbb0d5873a00cefc7abee1c0aa9ec6e50b
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-07-20 18:00:10 +0900

    refactor: get, post api 인스턴스 기존 client 인스턴스로 변경

--- frontend/src/apis/articles.ts
+++ frontend/src/apis/articles.ts
@@ -1,14 +1,6 @@
+import { client } from '.';
 import { ArticleRequest } from '../models/Article';
-import { createAxiosInstance } from '../utils/axiosInstance';
 
-const customAxios = (accessToken?: string) => createAxiosInstance({ accessToken });
+export const requestGetArticles = () => client.get(`/articles`);
 
-export const requestGetArticles = ({ accessToken }) => customAxios(accessToken).get(`/articles`);
-
-export const requestPostArticles = ({
-  accessToken,
-  body,
-}: {
-  accessToken: string;
-  body: ArticleRequest;
-}) => customAxios(accessToken).post('/articles', body);
+export const requestPostArticles = (body: ArticleRequest) => client.post('/articles', body);
