65f5703d0bba83178124870c3d544d5af3352070
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-07-28 22:14:37 +0900

    design: 아티클 스타일 분리 및 호버 추가

--- frontend/src/components/Article/Article.tsx
+++ frontend/src/components/Article/Article.tsx
@@ -1,14 +1,13 @@
 import * as Styled from './Article.style';
 import thumbnailExample from '../../assets/images/article-ex.png';
 import type { ArticleType } from '../../models/Article';
-import styled from '@emotion/styled';
 
 const Article = ({ id, title, userName, url, createdAt }: ArticleType) => {
   const createdDate = createdAt.split('-').join('. ');
 
   return (
     <Styled.Container>
-      <Anchor href={url} target="_blank" rel="noopener noreferrer">
+      <Styled.Anchor href={url} target="_blank" rel="noopener noreferrer">
         <Styled.ThumbnailWrapper>
           <Styled.Thumbnail src={thumbnailExample} />
         </Styled.ThumbnailWrapper>
@@ -17,11 +16,9 @@ const Article = ({ id, title, userName, url, createdAt }: ArticleType) => {
           <Styled.Title>{title}</Styled.Title>
           <Styled.CreatedAt>{createdDate}</Styled.CreatedAt>
         </Styled.ArticleInfoContainer>
-      </Anchor>
+      </Styled.Anchor>
     </Styled.Container>
   );
 };
 
 export default Article;
-
-export const Anchor = styled.a``;
