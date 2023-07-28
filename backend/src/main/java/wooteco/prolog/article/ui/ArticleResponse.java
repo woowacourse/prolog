61f65a4febd7fd37e1647a81c2d23239747a5178
hong-sile <gurwns9325@naver.com>
2023-07-28 16:33:30 +0900

    refactor: 반환형식 Raw한 LocalDateTime에서 yyyy-MM-dd hh:mm을 변경

--- backend/src/main/java/wooteco/prolog/article/ui/ArticleResponse.java
+++ backend/src/main/java/wooteco/prolog/article/ui/ArticleResponse.java
@@ -1,5 +1,7 @@
 package wooteco.prolog.article.ui;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import wooteco.prolog.article.domain.Article;
@@ -12,7 +14,8 @@ public class ArticleResponse {
     private final String userName;
     private final String title;
     private final String url;
-    private final String createdAt;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    private final LocalDateTime createdAt;
 
     private ArticleResponse() {
         this(null, null, null, null, null);
@@ -23,8 +26,7 @@ public class ArticleResponse {
         final String nickName = article.getMember().getNickname();
         final String title = article.getTitle().getTitle();
         final String url = article.getUrl().getUrl();
-        final String createdAt = article.getCreatedAt().toString();
-
+        final LocalDateTime createdAt = article.getCreatedAt();
         return new ArticleResponse(id, nickName, title, url, createdAt);
     }
 }
