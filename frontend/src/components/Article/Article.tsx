ae1799777c465f9e49d5e0d45bc860d99b2810d8
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-07-28 22:12:54 +0900

    feat: 아티클 링크 이동 기능 추가

--- frontend/src/components/Article/Article.tsx
+++ frontend/src/components/Article/Article.tsx
@@ -1,22 +1,27 @@
 import * as Styled from './Article.style';
 import thumbnailExample from '../../assets/images/article-ex.png';
 import type { ArticleType } from '../../models/Article';
+import styled from '@emotion/styled';
 
 const Article = ({ id, title, userName, url, createdAt }: ArticleType) => {
   const createdDate = createdAt.split('-').join('. ');
 
   return (
     <Styled.Container>
-      <Styled.ThumbnailWrapper>
-        <Styled.Thumbnail src={thumbnailExample} />
-      </Styled.ThumbnailWrapper>
-      <Styled.ArticleInfoContainer>
-        <Styled.UserName>{userName}</Styled.UserName>
-        <Styled.Title>{title}</Styled.Title>
-        <Styled.CreatedAt>{createdDate}</Styled.CreatedAt>
-      </Styled.ArticleInfoContainer>
+      <Anchor href={url} target="_blank" rel="noopener noreferrer">
+        <Styled.ThumbnailWrapper>
+          <Styled.Thumbnail src={thumbnailExample} />
+        </Styled.ThumbnailWrapper>
+        <Styled.ArticleInfoContainer>
+          <Styled.UserName>{userName}</Styled.UserName>
+          <Styled.Title>{title}</Styled.Title>
+          <Styled.CreatedAt>{createdDate}</Styled.CreatedAt>
+        </Styled.ArticleInfoContainer>
+      </Anchor>
     </Styled.Container>
   );
 };
 
 export default Article;
+
+export const Anchor = styled.a``;
