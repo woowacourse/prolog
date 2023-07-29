fc6551abd259b5cf9bf5a2362bae0598f26e1b3f
amaran-th <songsy405@naver.com>
2023-07-29 12:20:04 +0900

    test: 큐컴버 인수 테스트 추가
    #1397

---
+++ backend/src/acceptanceTest/java/wooteco/prolog/steps/KeywordRecommendedPostStepDefinitions.java
@@ -0,0 +1,66 @@
+package wooteco.prolog.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.springframework.http.HttpStatus;
+import wooteco.prolog.AcceptanceSteps;
+import wooteco.prolog.roadmap.application.dto.RecommendedRequest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static wooteco.prolog.fixtures.KeywordAcceptanceFixture.KEYWORD_REQUEST;
+
+public class KeywordRecommendedPostStepDefinitions extends AcceptanceSteps {
+
+    @Given("{int}번 세션에 {string}라는 키워드를 순서 {int}, 중요도 {int}, 부모 키워드 {long}로 작성하고")
+    public void 키워드를_부모_키워드와_함께_작성하고(int sessionId, String keywordName, int seq, int importance,
+                                     long parentId) {
+        context.invokeHttpPost(
+            "/sessions/" + sessionId + "/keywords",
+            KEYWORD_REQUEST.getSaveChild(keywordName, seq, importance, parentId));
+    }
+
+    @Given("{int}번 키워드에 대해 추천 포스트 {string}를 작성하고")
+    @When("{int}번 키워드에 대해 추천 포스트 {string}를 작성하면")
+    public void 추천_포스트를_추가하면(int keywordId, String url) {
+        context.invokeHttpPost(
+            "/keywords/"+keywordId+"/recommended-posts",
+            new RecommendedRequest(url)
+        );
+    }
+
+    @When("{int}번 키워드에 대한 {int}번 추천 포스트를 {string}로 수정하면")
+    public void 추천_포스트를_수정하면(int keywordId, int recommendedId, String url) {
+        context.invokeHttpPut(
+            "/keywords/"+keywordId+"/recommended-posts/"+recommendedId,
+            new RecommendedRequest(url));
+    }
+
+    @When("{int}번 키워드에 대한 {int}번 추천 포스트를 삭제하면")
+    public void 추천_포스트를_삭제하면(int keywordId, int recommendedId) {
+        context.invokeHttpDelete(
+            "/keywords/" + keywordId + "/recommended-posts/" + recommendedId
+        );
+    }
+
+    @Then("추천 포스트가 생성된다")
+    public void 추천_포스트가_생성된다() {
+        int statusCode = context.response.statusCode();
+
+        assertThat(statusCode).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    @Then("추천 포스트가 수정된다")
+    public void 추천_포스트가_수정된다() {
+        int statusCode = context.response.statusCode();
+
+        assertThat(statusCode).isEqualTo(HttpStatus.OK.value());
+    }
+
+    @Then("추천 포스트가 삭제된다")
+    public void 추천_포스트가_삭제된다() {
+        int statusCode = context.response.statusCode();
+
+        assertThat(statusCode).isEqualTo(HttpStatus.NO_CONTENT.value());
+    }
+}
