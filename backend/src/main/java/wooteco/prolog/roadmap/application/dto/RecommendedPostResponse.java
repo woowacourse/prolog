25c3f7e58c11ad94bf97cde7d4555eaef548e468
amaran-th <songsy405@naver.com>
2023-07-30 12:26:49 +0900

    fix: RecommendedPostResponse dto 수정
    - id 필드 추가
    
    #1397

--- backend/src/main/java/wooteco/prolog/roadmap/application/dto/RecommendedPostResponse.java
+++ backend/src/main/java/wooteco/prolog/roadmap/application/dto/RecommendedPostResponse.java
@@ -1,18 +1,17 @@
 package wooteco.prolog.roadmap.application.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import wooteco.prolog.roadmap.domain.RecommendedPost;
 
+@AllArgsConstructor
 @Getter
 public class RecommendedPostResponse {
 
+    private final Long id;
     private final String url;
 
-    public RecommendedPostResponse(final String url) {
-        this.url = url;
-    }
-
     public static RecommendedPostResponse from(final RecommendedPost recommendedPost) {
-        return new RecommendedPostResponse(recommendedPost.getUrl());
+        return new RecommendedPostResponse(recommendedPost.getId(), recommendedPost.getUrl());
     }
 }
