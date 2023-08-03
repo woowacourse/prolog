2c77205f5524ebf1bb78d0ef18304aa79c84b43a
nuyh <hjaehyun25@gmail.com>
2023-08-03 16:26:36 +0900

    refactor: RecommendedPost 삽입 응답값 수정
    #1397

--- backend/src/main/java/wooteco/prolog/roadmap/ui/RecommendedController.java
+++ backend/src/main/java/wooteco/prolog/roadmap/ui/RecommendedController.java
@@ -1,12 +1,17 @@
 package wooteco.prolog.roadmap.ui;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import wooteco.prolog.roadmap.application.RecommendedService;
 import wooteco.prolog.roadmap.application.dto.RecommendedRequest;
 import wooteco.prolog.roadmap.application.dto.RecommendedUpdateRequest;
-
-import java.net.URI;
 
 @RestController
 @RequestMapping("/keywords/{keywordId}/recommended-posts")
@@ -22,8 +27,7 @@ public class RecommendedController {
     public ResponseEntity<Void> createRecommendedPost(@PathVariable("keywordId") Long keywordId,
                                                       @RequestBody RecommendedRequest request) {
         final Long id = recommendedService.create(keywordId, request);
-        return ResponseEntity.created(
-            URI.create("/keywords/" + keywordId + "/recommended-posts/" + id)).build();
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PutMapping("/{recommendedId}")
