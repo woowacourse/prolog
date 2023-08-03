b66f4700cc60c5d0ab1a1c0463e58ecf388b1bc5
hong-sile <gurwns9325@naver.com>
2023-08-03 17:44:12 +0900

    test: url을 ogtag로 파싱하는 기능 인수테스트 추가

--- backend/src/acceptanceTest/java/wooteco/prolog/fixtures/ArticleFixture.java
+++ backend/src/acceptanceTest/java/wooteco/prolog/fixtures/ArticleFixture.java
@@ -1,6 +1,7 @@
 package wooteco.prolog.fixtures;
 
 import wooteco.prolog.article.ui.ArticleRequest;
+import wooteco.prolog.article.ui.ArticleUrlRequest;
 
 public class ArticleFixture {
 
@@ -8,4 +9,6 @@ public class ArticleFixture {
         "이미지 URL");
     public static final ArticleRequest ARTICLE_REQUEST2 = new ArticleRequest("두번째 아티클 제목",
         "두번째 아티클 주소", "이미지 URL");
+    public static final ArticleUrlRequest ARTICLE_URL_REQUEST = new ArticleUrlRequest(
+        "https://www.woowahan.com/");
 }
