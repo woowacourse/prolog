e0e34d4ed36ac9b2013746aa78587019bb00d09f
hong-sile <gurwns9325@naver.com>
2023-08-07 15:53:30 +0900

    test: github.com으로 대상 uri 변경

--- backend/src/test/java/wooteco/prolog/article/application/OgTagParserTest.java
+++ backend/src/test/java/wooteco/prolog/article/application/OgTagParserTest.java
@@ -18,10 +18,11 @@ public class OgTagParserTest {
         // "https://www.woowahan.com/" 사이트에 요청을 하기에 외부 의존성이 존재하는 테스트입니다.
 
         //given
-        final String url = "https://www.woowahan.com/";
+        final String url = "https://github.com/";
         final Map<OgType, String> expected = new HashMap<>();
-        expected.put(OgType.IMAGE, "https://woowahan-cdn.woowahan.com/static/image/share_kor.jpg");
-        expected.put(OgType.TITLE, "우아한형제들");
+        expected.put(OgType.IMAGE,
+            "https://github.githubassets.com/images/modules/open_graph/github-logo.png");
+        expected.put(OgType.TITLE, "Build software better, together");
 
         //when
         final Map<OgType, String> actual = ogTagParser.parse(url);
@@ -31,5 +32,4 @@ public class OgTagParserTest {
             .usingRecursiveComparison()
             .isEqualTo(expected);
     }
-
 }
