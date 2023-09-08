bc66db06fc38f705c2380c3c24fe104702fcf213
java-saeng <gusdn85554@naver.com>
2023-09-08 17:46:18 +0900

    refactor : 사용하지 않는 path 변수 삭제하여 각 다른 엔드 포인트로 변경
    #1525

--- backend/src/main/java/wooteco/prolog/roadmap/ui/QuizController.java
+++ backend/src/main/java/wooteco/prolog/roadmap/ui/QuizController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import wooteco.prolog.login.domain.AuthMemberPrincipal;
 import wooteco.prolog.login.ui.LoginMember;
@@ -20,49 +20,55 @@ import wooteco.prolog.roadmap.application.dto.QuizzesResponse;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/sessions/{sessionId}/keywords/{keywordId}/quizs")
 public class QuizController {
 
     private final QuizService quizService;
 
     //: todo admin login 생기면 검증 추가
-    @PostMapping
-    public ResponseEntity<Void> create(@PathVariable Long sessionId, @PathVariable Long keywordId,
-                                       @RequestBody QuizRequest quizRequest) {
+    @PostMapping("/sessions/{sessionId}/keywords/{keywordId}/quizzes")
+    public ResponseEntity<Void> create(
+        @PathVariable Long sessionId,
+        @PathVariable Long keywordId,
+        @RequestBody QuizRequest quizRequest
+    ) {
         final Long quizId = quizService.createQuiz(keywordId, quizRequest);
 
         return ResponseEntity.created(
-                URI.create("/sessions/" + sessionId + "/keywords/" + keywordId + "/quizs/" + quizId))
+                URI.create("/sessions/" + sessionId + "/keywords/" + keywordId + "/quizs/" + quizId)
+            )
             .build();
     }
 
-    @GetMapping("/{quizId}")
-    public ResponseEntity<QuizResponse> findQuizById(@PathVariable Long quizId,
-                                                     @AuthMemberPrincipal LoginMember member) {
+    @GetMapping("/quizzes/{quizId}")
+    public ResponseEntity<QuizResponse> findQuizById(
+        @PathVariable Long quizId,
+        @AuthMemberPrincipal LoginMember member
+    ) {
         return ResponseEntity.ok(quizService.findById(quizId, member.getId()));
     }
 
-    @GetMapping
-    public ResponseEntity<QuizzesResponse> findQuizzesByKeyword(@PathVariable Long sessionId,
-                                                                @PathVariable Long keywordId,
-                                                                @AuthMemberPrincipal LoginMember member) {
+    @GetMapping("/quizzes")
+    public ResponseEntity<QuizzesResponse> findQuizzesByKeyword(
+        @RequestParam Long keywordId,
+        @AuthMemberPrincipal LoginMember member
+    ) {
         return ResponseEntity.ok(quizService.findQuizzesByKeywordId(keywordId, member.getId()));
     }
 
-    @PutMapping("/{quizId}")
-    ResponseEntity<Void> updateQuiz(@PathVariable Long sessionId,
-                                    @PathVariable Long keywordId,
-                                    @PathVariable Long quizId,
-                                    @RequestBody QuizRequest quizRequest) {
+    @PutMapping("/quizzes/{quizId}")
+    ResponseEntity<Void> updateQuiz(
+        @PathVariable Long quizId,
+        @RequestBody QuizRequest quizRequest
+    ) {
         quizService.updateQuiz(quizId, quizRequest);
         return ResponseEntity.noContent().build();
     }
 
 
-    @DeleteMapping("/{quizId}")
-    public ResponseEntity<Void> deleteQuiz(@PathVariable Long sessionId,
-                                           @PathVariable Long keywordId,
-                                           @PathVariable Long quizId) {
+    @DeleteMapping("/quizzes/{quizId}")
+    public ResponseEntity<Void> deleteQuiz(
+        @PathVariable Long quizId
+    ) {
         quizService.deleteQuiz(quizId);
         return ResponseEntity.noContent().build();
     }
