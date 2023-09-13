047159ac6418892afe6a7d6148e8296545ecbc7f
hong-sile <gurwns9325@naver.com>
2023-09-13 17:14:35 +0900

    test: 아티클 북마크 restdocs 테스트 추가

---
+++ backend/src/documentation/java/wooteco/prolog/docu/ArticleDocumentation.java
@@ -0,0 +1,40 @@
+package wooteco.prolog.docu;
+
+import static io.netty.handler.codec.http.HttpHeaders.Values.APPLICATION_JSON;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import io.restassured.module.mockmvc.response.ValidatableMockMvcResponse;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import wooteco.prolog.NewDocumentation;
+import wooteco.prolog.article.application.ArticleService;
+import wooteco.prolog.article.ui.ArticleBookmarkRequest;
+import wooteco.prolog.article.ui.ArticleController;
+
+@WebMvcTest(controllers = ArticleController.class)
+public class ArticleDocumentation extends NewDocumentation {
+
+    @MockBean
+    private ArticleService articleService;
+
+    @Test
+    void 아티클에_북마크를_변경한다() {
+        //given, when
+        final ArticleBookmarkRequest bookmarkRequest = new ArticleBookmarkRequest(true);
+
+        final ValidatableMockMvcResponse response = given
+            .header("Authorization", "Bearer " + accessToken)
+            .contentType(APPLICATION_JSON)
+            .body(bookmarkRequest)
+            .when().put("/articles/{bookmark-id}/bookmark", 1L)
+            .then().log().all();
+
+        //then
+        response.expect(status().isOk());
+
+        //docs
+        response.apply(document("article/bookmark"));
+    }
+}
