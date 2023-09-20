46cf9efb0b15c7d273e1b77c8a4ee7e5ed23530e
solo5star <solo_5@naver.com>
2023-09-20 07:16:46 +0000

    feat: 명세에 맞춰 타입 업데이트 및 모킹 데이터 추가
    Co-authored-by: Q Kim <qoli0703@gmail.com>

--- frontend/src/models/Keywords.ts
+++ frontend/src/models/Keywords.ts
@@ -28,7 +28,18 @@ export interface KeywordResponse {
   importance: number;
   parentKeywordId: number | null;
   description: string;
+  // 해당 키워드의 총 퀴즈의 갯수
+  totalQuizCount: number;
+  // 내가 완료한 해당 키워드의 퀴즈의 갯수
+  doneQuizCount: number;
+  // 키워드에 대해 추천 포스트 URL 목록
+  recommendedPosts: RecommendedPostResponse[];
   childrenKeywords: KeywordResponse[];
+}
+
+export interface RecommendedPostResponse {
+  id: number;
+  url: string;
 }
 
 export interface RoadmapResponse {
@@ -42,8 +53,11 @@ export interface SubKeywordListResponse {
 export interface Quiz {
   quizId: number;
   question: string;
+  // 퀴즈에 대해서 답변을 달았는지 여부
+  isLearning: boolean;
 }
 
 export interface QuizListResponse {
+  keywordId: number;
   data: Quiz[];
 }
