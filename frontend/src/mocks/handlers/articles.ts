d9501c1c4b430581cfc514aea0552043f449523b
hae-on <solbi2004@naver.com>
2023-09-20 17:43:19 +0900

    refactor: msw filtering 기능 구현

--- frontend/src/mocks/handlers/articles.ts
+++ frontend/src/mocks/handlers/articles.ts
@@ -1,15 +1,18 @@
 import { rest } from 'msw';
 import { BASE_URL } from '../../configs/environment';
 import articles from '../db/articles.json';
+import articlesFrontend from '../db/articles-frontend.json';
+import articlesBackend from '../db/articles-backend.json';
+import articlesAndroid from '../db/articles-android.json';
 import metaOg from '../db/metaog.json';
 import { ArticleType } from '../../models/Article';
 
 const articleUrl = 'https://think0wise.tistory.com/107';
 
 export const articlesHandler = [
-  rest.get(`${BASE_URL}/articles`, (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(articles));
-  }),
+  // rest.get(`${BASE_URL}/articles`, (req, res, ctx) => {
+  //   return res(ctx.status(200), ctx.json(articles));
+  // }),
 
   rest.get(`${BASE_URL}/meta-og?url=${articleUrl}`, async (req, res, ctx) => {
     const data = metaOg;
@@ -37,13 +40,24 @@ export const articlesHandler = [
     return res(ctx.status(200));
   }),
 
-  rest.get(`${BASE_URL}/articles/`, (req, res, ctx) => {
-    const course = req.url.searchParams.get('course');
-    const onlyBookmarked = req.url.searchParams.get('onlyBookmarked');
+  rest.get(`${BASE_URL}/articles`, (req, res, ctx) => {
+    const course = req.url.searchParams.get('course') ?? 'all';
+    const onlyBookmarked = req.url.searchParams.get('onlyBookmarked') as string;
 
-    const filteredArticle = articles.filter(
-      (article) => onlyBookmarked === String(article.isBookMarked)
-    );
+    const filteredCourse = (course: string) => {
+      if (course === 'all') return articles;
+      if (course === 'frontend') return articlesFrontend;
+      if (course === 'backend') return articlesBackend;
+      if (course === 'android') return articlesAndroid;
+    };
+
+    if (onlyBookmarked === 'false') {
+      return res(ctx.status(200), ctx.json(filteredCourse(course)));
+    }
+
+    const filteredArticle = filteredCourse(course)?.filter((article) => {
+      return onlyBookmarked === String(article.isBookMarked) ? true : false;
+    });
 
     return res(ctx.status(200), ctx.json(filteredArticle));
   }),
