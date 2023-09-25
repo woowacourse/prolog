0058b62c3d0bbc6d84bd9a4e5930fc238276c351
solo5star <solo_5@naver.com>
2023-09-25 06:31:08 +0000

    refactor: axiosInstance->client
    Co-authored-by: Q Kim <qoli0703@gmail.com>

--- frontend/src/apis/keywords.ts
+++ frontend/src/apis/keywords.ts
@@ -7,9 +7,6 @@ import {
   RoadmapRequest,
   RoadmapResponse,
 } from '../models/Keywords';
-import { createAxiosInstance } from '../utils/axiosInstance';
-
-const instanceWithoutToken = createAxiosInstance();
 
 export const getRoadmap = async ({ curriculumId }: RoadmapRequest) => {
   const response = await client.get<RoadmapResponse>(`/roadmaps?curriculumId=${curriculumId}`);
@@ -18,15 +15,13 @@ export const getRoadmap = async ({ curriculumId }: RoadmapRequest) => {
 };
 
 export const getKeyword = async ({ keywordId }: KeywordRequest) => {
-  const response = await instanceWithoutToken.get<KeywordResponse>(`/keywords/${keywordId}`);
+  const response = await client.get<KeywordResponse>(`/keywords/${keywordId}`);
 
   return response.data;
 };
 
 export const getQuizListByKeyword = async ({ keywordId }: QuizListByKeywordRequest) => {
-  const response = await instanceWithoutToken.get<QuizListResponse>(
-    `/quizzes?keywordId=${keywordId}`
-  );
+  const response = await client.get<QuizListResponse>(`/quizzes?keywordId=${keywordId}`);
 
   return response.data;
 };
