e1ddbda1b78bf6fcea67c7fe34454fccb37f52b4
solo5star <solo_5@naver.com>
2023-09-27 08:14:53 +0000

    fix: Bearer null 로 전송되는 문제 해결

--- frontend/src/apis/index.ts
+++ frontend/src/apis/index.ts
@@ -2,9 +2,11 @@ import axios from 'axios';
 import { BASE_URL } from '../configs/environment';
 import LOCAL_STORAGE_KEY from '../constants/localStorage';
 
+const accessToken = localStorage.getItem(LOCAL_STORAGE_KEY.ACCESS_TOKEN);
+
 export const client = axios.create({
   baseURL: BASE_URL,
   headers: {
-    Authorization: `Bearer ${localStorage.getItem(LOCAL_STORAGE_KEY.ACCESS_TOKEN)}`,
+    ...(!accessToken && accessToken !== 'null' ? { Authorization: `Bearer ${accessToken}` } : {}),
   },
 });
