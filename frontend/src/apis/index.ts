16c8bcb9a749f6f08688485035a6a5bed495d769
solo5star <solo_5@naver.com>
2023-10-06 08:37:18 +0000

    fix: 잘못된 accessToken 로드 동작 수정

--- frontend/src/apis/index.ts
+++ frontend/src/apis/index.ts
@@ -2,11 +2,18 @@ import axios from 'axios';
 import { BASE_URL } from '../configs/environment';
 import LOCAL_STORAGE_KEY from '../constants/localStorage';
 
-const accessToken = localStorage.getItem(LOCAL_STORAGE_KEY.ACCESS_TOKEN);
+const getAccessToken = () => {
+  const accessToken = localStorage.getItem(LOCAL_STORAGE_KEY.ACCESS_TOKEN);
+  if (!accessToken || accessToken === 'null') return null;
+
+  return accessToken;
+};
+
+const accessToken = getAccessToken();
 
 export const client = axios.create({
   baseURL: BASE_URL,
   headers: {
-    ...(!accessToken && accessToken !== 'null' ? { Authorization: `Bearer ${accessToken}` } : {}),
+    ...(accessToken ? { Authorization: `Bearer ${accessToken}` } : {}),
   },
 });
