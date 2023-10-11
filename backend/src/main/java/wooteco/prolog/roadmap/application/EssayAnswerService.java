3c9ee5ea96b41a78ca056da295f25e0d5e523f16
hong-sile <gurwns9325@naver.com>
2023-10-11 17:42:53 +0900

    feat: EssayAnswer 작성 시 비크루인원의 경우 예외처리

--- backend/src/main/java/wooteco/prolog/roadmap/application/EssayAnswerService.java
+++ backend/src/main/java/wooteco/prolog/roadmap/application/EssayAnswerService.java
@@ -2,6 +2,7 @@ package wooteco.prolog.roadmap.application;
 
 import static wooteco.prolog.common.exception.BadRequestCode.CURRICULUM_NOT_FOUND_EXCEPTION;
 import static wooteco.prolog.common.exception.BadRequestCode.ESSAY_ANSWER_NOT_FOUND_EXCEPTION;
+import static wooteco.prolog.common.exception.BadRequestCode.MEMBER_NOT_ALLOWED;
 import static wooteco.prolog.common.exception.BadRequestCode.ROADMAP_QUIZ_NOT_FOUND_EXCEPTION;
 import static wooteco.prolog.common.exception.BadRequestCode.ROADMAP_SESSION_NOT_FOUND_EXCEPTION;
 
@@ -16,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 import wooteco.prolog.common.exception.BadRequestException;
 import wooteco.prolog.member.application.MemberService;
 import wooteco.prolog.member.domain.Member;
+import wooteco.prolog.member.domain.Role;
 import wooteco.prolog.roadmap.application.dto.EssayAnswerRequest;
 import wooteco.prolog.roadmap.application.dto.EssayAnswerSearchRequest;
 import wooteco.prolog.roadmap.application.dto.EssayAnswerUpdateRequest;
@@ -61,10 +63,17 @@ public class EssayAnswerService {
             .orElseThrow(() -> new BadRequestException(ROADMAP_QUIZ_NOT_FOUND_EXCEPTION));
 
         Member member = memberService.findById(memberId);
+        validateMemberIsCrew(member);
         EssayAnswer essayAnswer = new EssayAnswer(quiz, essayAnswerRequest.getAnswer(), member);
         essayAnswerRepository.save(essayAnswer);
 
         return essayAnswer.getId();
+    }
+
+    private void validateMemberIsCrew(final Member member) {
+        if (member.hasLowerImportanceRoleThan(Role.CREW)) {
+            throw new BadRequestException(MEMBER_NOT_ALLOWED);
+        }
     }
 
     @Transactional
