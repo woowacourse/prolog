dc4fae9b2e3c52cff146d84131ebdc3d537075fd
nuyh <hjaehyun25@gmail.com>
2023-11-01 11:03:18 +0900

    feat: 유저 ID로 답변 개수 조회 기능 구현
    #1609

---
+++ backend/src/main/java/wooteco/prolog/roadmap/domain/repository/dto/KeywordIdAndDoneQuizCount.java
@@ -0,0 +1,7 @@
+package wooteco.prolog.roadmap.domain.repository.dto;
+
+public interface KeywordIdAndDoneQuizCount {
+    long getKeywordId();
+
+    int getDoneQuizCount();
+}
--- backend/src/test/java/wooteco/prolog/roadmap/repository/KeywordRepositoryTest.java
+++ backend/src/test/java/wooteco/prolog/roadmap/repository/KeywordRepositoryTest.java
@@ -1,12 +1,5 @@
 package wooteco.prolog.roadmap.repository;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
-
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import javax.persistence.EntityManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +10,13 @@ import wooteco.prolog.roadmap.domain.repository.KeywordRepository;
 import wooteco.prolog.session.domain.Session;
 import wooteco.prolog.session.domain.repository.SessionRepository;
 import wooteco.support.utils.RepositoryTest;
+
+import javax.persistence.EntityManager;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @RepositoryTest
 class KeywordRepositoryTest {
@@ -112,53 +112,6 @@ class KeywordRepositoryTest {
 
         // then
         assertThat(extractParentKeyword.size()).isEqualTo(1);
-    }
-
-    @Test
-    @DisplayName("세션 ID 리스트로 키워드 리스트를 조회한다")
-    void findBySessionIdIn() {
-        //given
-        final Curriculum curriculum = curriculumRepository.save(new Curriculum("커리큘럼1"));
-
-        final Session session1 = sessionRepository.save(new Session(curriculum.getId(), "세션1"));
-        final Session session2 = sessionRepository.save(new Session(curriculum.getId(), "세션2"));
-        final Session session3 = sessionRepository.save(new Session(curriculum.getId(), "세션3"));
-        final Session session4 = sessionRepository.save(new Session(curriculum.getId(), "세션4"));
-        final Session session5 = sessionRepository.save(new Session(curriculum.getId(), "세션5"));
-
-        final Keyword keyword1 = keywordRepository.save(
-            Keyword.createKeyword("자바1", "자바 설명1", 1, 5, session1.getId(), null));
-        final Keyword keyword2 = keywordRepository.save(
-            Keyword.createKeyword("자바2", "자바 설명2", 2, 5, session1.getId(), keyword1));
-        final Keyword keyword3 = keywordRepository.save(
-            Keyword.createKeyword("자바3", "자바 설명3", 3, 5, session1.getId(), null));
-        final Keyword keyword4 = keywordRepository.save(
-            Keyword.createKeyword("자바4", "자바 설명4", 4, 5, session1.getId(), keyword3));
-        keywordRepository.save(
-            Keyword.createKeyword("자바5", "자바 설명5", 5, 5, session2.getId(), null));
-        keywordRepository.save(
-            Keyword.createKeyword("자바6", "자바 설명6", 6, 5, session2.getId(), keyword1));
-        keywordRepository.save(
-            Keyword.createKeyword("자바7", "자바 설명7", 7, 5, session2.getId(), null));
-        keywordRepository.save(
-            Keyword.createKeyword("자바8", "자바 설명8", 8, 5, session3.getId(), keyword2));
-        final Keyword keyword9 = keywordRepository.save(
-            Keyword.createKeyword("자바9", "자바 설명9", 9, 5, session4.getId(), keyword2));
-        final Keyword keyword10 = keywordRepository.save(
-            Keyword.createKeyword("자바10", "자바 설명10", 10, 5, session5.getId(), null));
-
-        final HashSet<Long> sessionIds = new HashSet<>(
-            Arrays.asList(session1.getId(), session4.getId(), session5.getId())
-        );
-
-        //when
-        final List<Keyword> keywords = keywordRepository.findBySessionIdIn(sessionIds);
-
-        //then
-        assertThat(keywords)
-            .usingRecursiveComparison()
-            .ignoringFields("id", "parent.id")
-            .isEqualTo(Arrays.asList(keyword1, keyword2, keyword3, keyword4, keyword9, keyword10));
     }
 
     @Test
