899af12d619627154443e36cd2005c5fb0cf62db
gracefulBrown <brown@woowahan.com>
2024-12-26 18:32:43 +0900

    fix: 태그와 멤버 필터 제거

--- backend/src/main/java/wooteco/prolog/studylog/application/FilterService.java
+++ backend/src/main/java/wooteco/prolog/studylog/application/FilterService.java
@@ -4,14 +4,11 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import wooteco.prolog.login.ui.LoginMember;
-import wooteco.prolog.member.application.MemberService;
-import wooteco.prolog.member.application.dto.MemberResponse;
 import wooteco.prolog.session.application.MissionService;
 import wooteco.prolog.session.application.SessionService;
 import wooteco.prolog.session.application.dto.MissionResponse;
 import wooteco.prolog.session.application.dto.SessionResponse;
 import wooteco.prolog.studylog.application.dto.FilterResponse;
-import wooteco.prolog.studylog.application.dto.TagResponse;
 
 @Service
 @AllArgsConstructor
@@ -19,17 +16,12 @@ public class FilterService {
 
     private final SessionService sessionService;
     private final MissionService missionService;
-    private final TagService tagService;
-    private final MemberService memberService;
 
     public FilterResponse showAll(LoginMember loginMember) {
         List<SessionResponse> sessionResponses = sessionService.findAllWithMySessionFirst(
             loginMember); // my session으로 대체
         List<MissionResponse> missionResponses = missionService.findAllWithMyMissionFirst(
             loginMember);
-        List<TagResponse> tagResponses = tagService.findTagsIncludedInStudylogs();
-        List<MemberResponse> memberResponses = memberService.findAllOrderByNickNameAsc();
-        return new FilterResponse(sessionResponses, missionResponses, tagResponses,
-            memberResponses);
+        return new FilterResponse(sessionResponses, missionResponses);
     }
 }
--- backend/src/main/java/wooteco/prolog/studylog/application/dto/FilterResponse.java
+++ backend/src/main/java/wooteco/prolog/studylog/application/dto/FilterResponse.java
@@ -4,7 +4,6 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import wooteco.prolog.member.application.dto.MemberResponse;
 import wooteco.prolog.session.application.dto.MissionResponse;
 import wooteco.prolog.session.application.dto.SessionResponse;
 
@@ -15,6 +14,4 @@ public class FilterResponse {
 
     private List<SessionResponse> sessions;
     private List<MissionResponse> missions;
-    private List<TagResponse> tags;
-    private List<MemberResponse> members;
 }
