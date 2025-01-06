ef6458cf1cb53a158c9d14647754fda501597def
woowahan-neo <jaeyeonling@woowahan.com>
2025-01-07 02:15:19 +0900

    feat: add secrets manager access role to ec2

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -40,6 +40,30 @@ resource "aws_iam_role_policy_attachment" "s3_access_attachment" {
     policy_arn = aws_iam_policy.s3_access_policy.arn
 }
 
+resource "aws_iam_policy" "secrets_manager_access_policy" {
+    name = "${var.project_name}-secrets_manager_access-policy"
+
+    policy = jsonencode({
+        Version = "2012-10-17",
+        Statement = [
+            {
+                Action = [
+                    "secretsmanager:GetSecretValue"
+                ],
+                Effect   = "Allow",
+                Resource = "*"
+            }
+        ]
+    })
+
+    // Note: Tag not required
+}
+
+resource "aws_iam_role_policy_attachment" "secrets_manager_access_attachment" {
+    role       = aws_iam_role.ec2_role.name
+    policy_arn = aws_iam_policy.secrets_manager_access_policy.arn
+}
+
 resource "aws_iam_role" "code_deploy_role" {
     name = "${var.project_name}-code-deploy-role"
 
