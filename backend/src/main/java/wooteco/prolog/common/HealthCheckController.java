8b5c17f9da7dd53e6e369a743ca2b3ca1428c2f5
woowahan-neo <jaeyeonling@woowahan.com>
2025-01-07 11:28:50 +0900

    feat: add health check controller

---
+++ backend/src/main/java/wooteco/prolog/common/HealthCheckController.java
@@ -0,0 +1,13 @@
+package wooteco.prolog.common;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
