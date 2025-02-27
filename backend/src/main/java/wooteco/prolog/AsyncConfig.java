45a1c846e89c067e2cb6870b0ac23abcf1359a9b
woowahan-neo <jaeyeonling@woowahan.com>
2025-02-28 00:57:10 +0900

    feat: enable async

---
+++ backend/src/main/java/wooteco/prolog/AsyncConfig.java
@@ -0,0 +1,9 @@
+package wooteco.prolog;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
